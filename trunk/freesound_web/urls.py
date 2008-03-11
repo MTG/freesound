0593300e4c1cbd2ea7150866aa9653ed2cbedaee
bdejong <bdejong@8b7dadd3-f59f-4475-adbd-84da9656de16>
2008-03-11 14:35:33 +0000

    more directory structure
    git-svn-id: https://iua-share.upf.edu/svn/nightingale/trunk@7 8b7dadd3-f59f-4475-adbd-84da9656de16

---
+++ trunk/freesound_web/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^freesound_web/', include('freesound_web.foo.urls')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)

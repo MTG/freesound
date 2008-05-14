2e95fca3b91b88843527f24cc195e4509e5c3710
bdejong <bdejong@8b7dadd3-f59f-4475-adbd-84da9656de16>
2008-05-14 17:32:35 +0000

    split everything into classes, much cleaner now, also optimized the spectral-drawing, TODO: optparse for parsing command line parameters
    git-svn-id: https://iua-share.upf.edu/svn/nightingale/trunk@59 8b7dadd3-f59f-4475-adbd-84da9656de16

--- sandbox/wav2png/wav2png.py
+++ sandbox/wav2png/wav2png.py
@@ -52,13 +52,20 @@ class AudioProcessor(object):
         energy = spectrum.sum()
         
         db_spectrum = ((20*(numpy.log10(spectrum + 1e-30))).clip(-spec_range, 0.0) + spec_range)/spec_range
-    
+        
         if energy < 1e-20:
             return (0, db_spectrum)
         else:
-            sc = (spectrum * self.spectrum_range).sum() / (energy * (length - 1))
+            sc = (spectrum * self.spectrum_range).sum() / (energy * (length - 1)) * self.samplerate*0.5
+            
+            if sc < 100:
+                sc = 100
+                
+            if sc > 22050:
+                sc = 22050
+            
             # arbitrary scaling to look the colors look good :)
-            return (math.log(sc*512 + 1) / math.log(512 + 1), db_spectrum)
+            return (math.log(sc) - math.log(100)) / (math.log(22050) - math.log(100)), db_spectrum
 
 
     def peaks(self, start_seek, end_seek):
@@ -80,6 +87,13 @@ class AudioProcessor(object):
     
         if block_size > end_seek - start_seek:
             block_size = end_seek - start_seek
+            
+        if block_size <= 1:
+            samples = self.read(start_seek, 1)
+            return samples[0], samples[0]
+        elif block_size == 2:
+            samples = self.read(start_seek, 2)
+            return samples[0], samples[1]
         
         for i in range(start_seek, end_seek, block_size):
             samples = self.read(i, block_size)
@@ -99,112 +113,191 @@ class AudioProcessor(object):
                 min_index = local_min_index
     
         return (min_value, max_value) if min_index < max_index else (max_value, min_value)
-
-
-class ColorCreator(object):
-    def __init__(self):
-        self.num_colors = 1000
-        self.colors = []
-
-        for i in range(self.num_colors):
-            self.colors.append( self.color_from_value(i/float(self.num_colors)) )
-        
-        self.palette = []
-        colors = [  (0, 0, 0),
-                    (58/4 * 128.0/256.0, 68/4 * 128.0/256.0,65/4 * 128.0/256.0, 12),
-                    (80*230.0/256.0,100*230.0/256.0,153*230.0/256.0),
-                    (90,180,100),
-                    (224,224,44),
-                    (255,60,30)]
     	
-        for i in range(0,256):
-            index = (i * (len(colors) - 1))/255.0
-            index_int = int(index)
-            alpha = index - float(index_int)
-            
-            if alpha > 0:
-                r = (1.0 - alpha) * colors[index_int][0] + alpha * colors[index_int + 1][0]
-                g = (1.0 - alpha) * colors[index_int][1] + alpha * colors[index_int + 1][1]
-                b = (1.0 - alpha) * colors[index_int][2] + alpha * colors[index_int + 1][2]
-            else:
-                r = (1.0 - alpha) * colors[index_int][0]
-                g = (1.0 - alpha) * colors[index_int][1]
-                b = (1.0 - alpha) * colors[index_int][2]
-            	   
-            self.palette.extend((int(r), int(g), int(b))) # grayscale wedge
-            
-    def get_palette(self):
-        return self.palette
+    
+def interpolate_colors(colors, flat=False, num_colors=256):
+    """ given a list of colors, create a larger list of colors interpolating
+    the first one. If flatten is True a list of numers will be returned. If
+    False, a list of (r,g,b) tuples. num_colors is the number of colors wanted
+    in the final list """
+    
+    palette = []
+    
+    for i in range(num_colors):
+        index = (i * (len(colors) - 1))/(num_colors - 1.0)
+        index_int = int(index)
+        alpha = index - float(index_int)
+        
+        if alpha > 0:
+            r = (1.0 - alpha) * colors[index_int][0] + alpha * colors[index_int + 1][0]
+            g = (1.0 - alpha) * colors[index_int][1] + alpha * colors[index_int + 1][1]
+            b = (1.0 - alpha) * colors[index_int][2] + alpha * colors[index_int + 1][2]
+        else:
+            r = (1.0 - alpha) * colors[index_int][0]
+            g = (1.0 - alpha) * colors[index_int][1]
+            b = (1.0 - alpha) * colors[index_int][2]
+        
+        if flat:
+            palette.extend((int(r), int(g), int(b)))
+        else:
+            palette.append((int(r), int(g), int(b)))
+        
+    return palette
+    
 
-    def color_lookup(self, value):
-        return self.colors[int( value*(self.num_colors - 1) )]
+class WaveformImage(object):
+    def __init__(self, image_width, image_height):
+        self.image = Image.new("RGB", (image_width, image_height))
+        
+        self.image_width = image_width
+        self.image_height = image_height
+        
+        self.draw = ImageDraw.Draw(self.image)
+        self.previous_x, self.previous_y = None, None
+
+        # draw a zer "zero" line
+        self.draw.line([0, self.image_height/2, self.image_width, self.image_height/2], ImageColor.getrgb("#ff0000") )
+        
+        self.color_lookup = interpolate_colors( [self.color_from_value(value/29.0) for value in range(0,30)])
+        
+        self.pix = self.image.load()
+        
 
     def color_from_value(self, value):
+        """ given a value between 0 and 1, return an (r,g,b) tuple """
+
         return ImageColor.getrgb("hsl(%d,%d%%,%d%%)" % (int( (1.0 - value) * 360 ), 80, 50))
+        
+        
+    def draw_peaks(self, x, peaks, spectral_centroid):
+        """ draw 2 peaks at x using the spectral_centroid for color """
+
+        y1 = self.image_height * 0.5 - peaks[0] * self.image_height * 0.5
+        y2 = self.image_height * 0.5 - peaks[1] * self.image_height * 0.5
+        
+        line_color = self.color_lookup[int(spectral_centroid*255.0)]
+        
+        if self.previous_y != None:
+            self.draw.line([self.previous_x, self.previous_y, x, y1, x, y2], line_color)
+        else:
+            self.draw.line([x, y1, x, y2], line_color)
+    
+        self.previous_x, self.previous_y = x, y2
+        
+        self.draw_anti_aliased_pixels(x, y1, y2, line_color)
+    
+    
+    def draw_anti_aliased_pixels(self, x, y1, y2, color):
+        """ vertical anti-aliasing at y1 and y2 """
+
+        y_max = max(y1, y2)
+        y_max_int = int(y_max)
+        alpha = y_max - y_max_int
+        
+        if alpha > 0.0 and alpha < 1.0 and y_max_int + 1 < self.image_height:
+            current_pix = self.pix[x, y_max_int + 1]
+                
+            r = int((1-alpha)*current_pix[0] + alpha*color[0])
+            g = int((1-alpha)*current_pix[1] + alpha*color[1])
+            b = int((1-alpha)*current_pix[2] + alpha*color[2])
+            
+            self.pix[x, y_max_int + 1] = (r,g,b)
+            
+        y_min = min(y1, y2)
+        y_min_int = int(y_min)
+        alpha = 1.0 - (y_min - y_min_int)
+        
+        if alpha > 0.0 and alpha < 1.0 and y_min_int - 1 >= 0:
+            current_pix = self.pix[x, y_min_int - 1]
+                
+            r = int((1-alpha)*current_pix[0] + alpha*color[0])
+            g = int((1-alpha)*current_pix[1] + alpha*color[1])
+            b = int((1-alpha)*current_pix[2] + alpha*color[2])
+            
+            self.pix[x, y_min_int - 1] = (r,g,b)
+            
+            
+    def save(self, filename):
+        self.image.save(filename)
+        
+        
+class SpectrogramImage(object):
+    def __init__(self, image_width, image_height, fft_size):
+        self.image = Image.new("P", (image_height, image_width))
+        
+        self.image_width = image_width
+        self.image_height = image_height
+        self.fft_size = fft_size
+        
+        colors = [  (0, 0, 0),
+                    (58/4,68/4,65/4),
+                    (80/2,100/2,153/2),
+                    (90,180,100),
+                    (224,224,44),
+                    (255,60,30),
+                    (255,255,255) ]
+        
+        self.image.putpalette(interpolate_colors(colors, True))
+        #self.pix = self.image.load()
+
+        # generate the lookup which translates y-coordinate to fft-bin
+        self.y_to_bin = []
+        f_min = 100.0
+        f_max = 22050.0
+        y_min = math.log(f_min)/math.log(10.0)
+        y_max = math.log(f_max)/math.log(10.0)
+        for y in range(self.image_height):
+            freq = math.pow(10.0, y_min + y / (image_height - 1.0) *(y_max - y_min))
+            bin = freq / 22050.0 * (self.fft_size/2 + 1)
+
+            if bin < self.fft_size/2:
+                alpha = bin - int(bin)
+                
+                self.y_to_bin.append((int(bin), alpha * 255))
+           
+        self.pixels = []
+            
+    def draw_spectrum(self, x, spectrum):
+        for (index, alpha) in self.y_to_bin:
+            self.pixels.append( int( ((255.0-alpha) * spectrum[index] + alpha * spectrum[index + 1] )) )
+            
+        for y in range(len(self.y_to_bin), self.image_height):
+            self.pixels.append(0)
+
+    def save(self, filename):
+        self.image.putdata(self.pixels)
+        self.image.transpose(Image.ROTATE_90).save(filename)
+
 
 def create_png():
     filename = sys.argv[1]
     
     fft_size = 2048
     
-    image_width = 500
-    image_height = 200
+    image_width = 2400
+    image_height = 400
     
     audio_file = audiolab.sndfile(filename, 'read')
     samples_per_pixel = audio_file.get_nframes() / float(image_width)
+    processor = AudioProcessor(audio_file, fft_size, numpy.hanning)
     
-    processor = AudioProcessor(audio_file, fft_size, numpy.hanning) 
-    color_creator = ColorCreator()
+    waveform = WaveformImage(image_width, image_height)
+    spectrogram = SpectrogramImage(image_width, image_height, fft_size)
     
-    im_waveform = Image.new("RGB", (image_width, image_height))
-    draw = ImageDraw.Draw(im_waveform)
-    previous_x = 0
-    previous_y = image_height / 2
-    zero_line_color = ImageColor.getrgb("#ff0000")
-    draw.line([0, image_height/2, image_width, image_height/2], zero_line_color )
-    
-    im_spectrogram = Image.new("P", (image_width, image_height))
-    im_spectrogram.putpalette(color_creator.get_palette())
-    y_to_bin = []
-    f_min = 100.0
-    f_max = 22050.0
-    y_min = math.log(f_min)/math.log(10.0)
-    y_max = math.log(f_max)/math.log(10.0)
-    for y in range(image_height):
-        y_inv = (image_height - 1.0 - y)/(image_height - 1.0) # between 1 and 0
-        freq = math.pow(10.0, y_min + y_inv *(y_max - y_min))
-        bin = freq / (audio_file.get_samplerate()/2.0) * (fft_size/2 + 1)
-        y_to_bin.append(bin)
-    pix = im_spectrogram.load()
-
     for x in range(image_width):
-        
         seek_point = int(x * samples_per_pixel)
         next_seek_point = int( (x + 1) * samples_per_pixel)
+        
         (spectral_centroid, db_spectrum) = processor.spectral_centroid(seek_point)
         peaks = processor.peaks(seek_point, next_seek_point)
-    
-        y1 = image_height * 0.5 - peaks[0] * image_height * 0.5
-        y2 = image_height * 0.5 - peaks[1] * image_height * 0.5
         
-        draw.line([previous_x, previous_y, x, y1, x, y2], color_creator.color_lookup(spectral_centroid))
-    
-        previous_x, previous_y = x, y2
+        waveform.draw_peaks(x, peaks, spectral_centroid)
+        spectrogram.draw_spectrum(x, db_spectrum)
         
-        for y in range(image_height):
-            
-            bin = y_to_bin[y]
-            
-            if bin < db_spectrum.shape[0] - 1:
-                int_bin = int(bin)
-                alpha = bin - float(int_bin)
-                db = (1.0 - alpha) * db_spectrum[int_bin] + alpha * db_spectrum[int_bin + 1]
-                #col = color_creator.greyscale_lookup(db)
+    
+    waveform.save(filename + '_w.png')
+    spectrogram.save(filename + '_s.png')
 
-                pix[x,y] = int(255*db)
-    
-    im_waveform.save(filename + '_w.png')
-    im_spectrogram.save(filename + '_s.png')
 
 if __name__ == '__main__':
     if len(sys.argv) == 3 and sys.argv[2] == "profile":

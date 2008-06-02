9581fe3a8c6167ab35406b71db5fa521ba6cd86a
bdejong <bdejong@8b7dadd3-f59f-4475-adbd-84da9656de16>
2008-06-02 15:47:34 +0000

    switched to BSD
    git-svn-id: https://iua-share.upf.edu/svn/nightingale/trunk@85 8b7dadd3-f59f-4475-adbd-84da9656de16

--- sandbox/solr/solr.py
+++ sandbox/solr/solr.py
@@ -25,7 +25,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# Author: bram de Jong <bram.dejong@domain.com> where domain in gmail 
+# Author: bram de Jong <bram.dejong@domain.com> where domain is gmail 
  
 from datetime import datetime, date
 from time import strptime

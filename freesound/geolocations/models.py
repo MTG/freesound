7a8e3e5a04b16ffa99d92e31dbc6744580c04bd9
bdejong <bdejong@8b7dadd3-f59f-4475-adbd-84da9656de16>
2008-07-02 17:32:06 +0000

    new admin classes
    git-svn-id: https://iua-share.upf.edu/svn/nightingale/trunk@88 8b7dadd3-f59f-4475-adbd-84da9656de16

--- freesound/geolocations/models.py
+++ freesound/geolocations/models.py
@@ -1,19 +1,29 @@
 # -*- coding: utf-8 -*-
-
+from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-class GeoLocation(models.Model):
+class GeoTag(models.Model):
     user = models.ForeignKey(User)
 
-    lat = models.FloatField()
-    lon = models.FloatField()
-    zoom = models.IntegerField()
-
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField(db_index=True)
-    content_object = generic.GenericForeignKey()
+    lat = models.FloatField(db_index=True)
+    lon = models.FloatField(db_index=True)
+    zoom = models.IntegerField(db_index=True)
 
     created = models.DateTimeField()
+    
+    def __unicode__(self):
+        return u"(%f,%f)" % (self.lat, self.lon)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('geotag', (smart_unicode(self.id),))
+
+
+class GeoTagAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',) 
+    list_display = ('user', 'lat', 'lon', 'created')
+
+admin.site.register(GeoTag, GeoTagAdmin)

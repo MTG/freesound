65712e84b2c8bd162fb6d28d1907455388b35ef2
bdejong <bdejong@8b7dadd3-f59f-4475-adbd-84da9656de16>
2008-07-02 17:29:50 +0000

    admin classes added
    git-svn-id: https://iua-share.upf.edu/svn/nightingale/trunk@86 8b7dadd3-f59f-4475-adbd-84da9656de16

--- freesound/users/models.py
+++ freesound/users/models.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
-
+from django.contrib import admin
 from django.contrib.auth.models import User
 from django.db import models
+from general.models import SocialModel
 
-class Profile(models.Model):
+class Profile(SocialModel):
     user = models.ForeignKey(User)
     # add many many more things here :)
     
-    whitelisted = models.BooleanField(default=False)
-    newsletter = models.BooleanField(default=True)
+    home_page = models.URLField(null=True, blank=True, default=None)
+    about = models.TextField(null=True, blank=True, default=None)
+    newsletter = models.BooleanField(default=True, db_index=True)
+    
+    whitelisted = models.BooleanField(default=False, db_index=True)
+    
+    def __unicode__(self):
+        return self.user.username
+    
+    @models.permalink
+    def get_absolute_url(self):
+        return ('account', (smart_unicode(self.user.username),))
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',) 
+    list_display = ('user', 'home_page', 'whitelisted')
+    ordering = ('user__unsername',)
+
+admin.site.register(Profile, ProfileAdmin)

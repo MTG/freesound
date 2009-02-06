bd7f04afd4d5a8639dcce96db411298b17d5f82c
Bram de Jong <bram.dejong+github@gmail.com>
2009-02-06 19:24:36 +0100

    now takes wildcarts

--- freesound/utils/audioprocessing/convert_to_mp3.py
+++ freesound/utils/audioprocessing/convert_to_mp3.py
@@ -3,4 +3,5 @@
 import sys
 from processing import convert_to_mp3, audio_info, AudioProcessingException
 
-convert_to_mp3(sys.argv[1], sys.argv[2])
+for filename in sys.argv[1:]:
+    convert_to_mp3(filename, filename[:-3] + "mp3")

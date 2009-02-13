872905ce1e34cd2cb0925fce0a37acf595311d91
Bram de Jong <bram.dejong+github@gmail.com>
2009-02-13 15:50:55 +0100

    fixed slow delete statements

--- sandbox/db_conversion/users.py
+++ sandbox/db_conversion/users.py
@@ -29,16 +29,33 @@ while True:
         user_email = smart_character_decoding(user_email)
         
         username = username.replace(u"&amp;", u"&").replace(u"&lt;", u"<").replace(u"&gt;", u">").replace("&quot;","\"")
+        
+        if len(user_email) > 75:
+            # skip users with crazy email addresses
+            continue
+        
         output_file.write(u"\t".join([unicode(user_id), unicode(user_active), username, user_password, unicode(user_regdate), unicode(user_lastvisit), user_email, u"", u"", "0", "0"]) + "\n")
 
 print """
 alter table auth_user drop constraint auth_user_username_key;
 copy auth_user (id, is_active, username, password, date_joined, last_login, email, first_name, last_name, is_staff, is_superuser) from '%s';
 select setval('auth_user_id_seq',(select max(id)+1 from auth_user));
-delete from auth_user where username in (select username from auth_user group by username having count(*) > 1);
-delete from auth_user where email in (select email from auth_user group by email having count(*) > 1) and last_login = '1970-01-01 01:00:00+01';
-delete from auth_user where lower(username) in (select lower(username) from auth_user group by lower(username) having count(*) > 1) and last_login='1970-01-01 01:00:00+01';
-alter table auth_user add constraint auth_user_username_key unique(username);
+vacuum analyze auth_user;
+
+create table TEMP_TABLE as select select username from auth_user group by username having count(*) > 1;
+delete from auth_user where username in (select username from TEMP_TABLE);
+drop table TEMP_TABLE;
+
+create table TEMP_TABLE as select email from auth_user group by email having count(*) > 1;
+delete from auth_user where email in (select email from TEMP_TABLE) and last_login = '1970-01-01 01:00:00+01';
+drop table TEMP_TABLE;
+
+create table TEMP_TABLE as select lower(username) as username from auth_user group by lower(username) having count(*) > 1;
+delete from auth_user where lower(username) in (select username from TEMP_TABLE) and last_login='1970-01-01 01:00:00+01';
+drop table TEMP_TABLE;
+
 update auth_user set is_staff=true, is_superuser=true where username='Bram';
+
+alter table auth_user add constraint auth_user_username_key unique(username);
 vacuum analyze auth_user;
 """ % output_filename        

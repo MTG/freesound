fe0f2d02561c643ccc91822cb3ce73a0a2e07ab6
Bram de Jong <bram.dejong+github@gmail.com>
2009-05-18 14:54:35 +0200

    some URL refactoing

--- freesound/urls.py
+++ freesound/urls.py
@@ -9,10 +9,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'sounds.views.front_page', name='front-page'),
     
-    (r'^account/', include('accounts.urls')),
-    
-    url(r'^search/$', 'sounds.views.search', name='sounds-search'),
-    
     url(r'^people/$', 'accounts.views.accounts', name="accounts"),
     url(r'^people/(?P<username>[^//]+)/$', 'accounts.views.account', name="account"),
     url(r'^people/(?P<username>[^//]+)/sounds/$', 'sounds.views.for_user', name="sounds-for-user"),
@@ -21,34 +17,32 @@ urlpatterns = patterns('',
     url(r'^people/(?P<username>[^//]+)/sounds/(?P<sound_id>\d+)/sources/$', 'sounds.views.sources', name="sound-sources"),
     url(r'^people/(?P<username>[^//]+)/sounds/(?P<sound_id>\d+)/geotag/$', 'sounds.views.geotag', name="sound-geotag"),
     url(r'^people/(?P<username>[^//]+)/sounds/(?P<sound_id>\d+)/similar/$', 'sounds.views.similar', name="sound-similar"),
-
     url(r'^people/(?P<username>[^//]+)/packs/$', 'sounds.views.packs_for_user', name="packs-for-user"),
     url(r'^people/(?P<username>[^//]+)/packs/(?P<pack_id>\d+)/$', 'sounds.views.pack', name="pack"),
 
     url(r'^browse/$', 'sounds.views.sounds', name="sounds"),
-
     url(r'^browse/tags/(?P<multiple_tags>[\w//-]*)/$', 'tags.views.tags', name="tags"),
-    
     url(r'^browse/packs/$', 'sounds.views.packs', name="packs"),
-    
     url(r'^browse/random/$', 'sounds.views.random', name="sounds-random"),
     url(r'^browse/remixed/$', 'sounds.views.remixed', name="sounds-remixed"),
     url(r'^browse/geotagged/$', 'geotags.views.geotags', name="geotags"),
     
-    url(r'^contact/$', 'support.views.contact', name="contact"),
+    url(r'^contact/', 'support.views.contact', name="contact"),
+
+    url(r'^search/$', 'sounds.views.search', name='sounds-search'),
+    
+    (r'^help/', include('wiki.urls')),
+    (r'^forum/', include('forum.urls')),
+    (r'^geotag/', include('geotags.urls')),
+    (r'^account/', include('accounts.urls')),
+    (r'^helpdesk/', include('helpdesk.urls')),
     
     url(r'^blog/$', "django.views.generic.simple.redirect_to", kwargs={'url': "http://blog.freesound.org/"}, name="blog"),
-    
-    url(r'^help/$', "django.views.generic.simple.redirect_to", kwargs={'url': "/help/main/"}, name="wiki"),
-    url(r'^help/(?P<name>[//\w_-]+)/edit/$', 'wiki.views.editpage', name="wiki-page-edit"),
-    url(r'^help/(?P<name>[//\w_-]+)/$', 'wiki.views.page', name="wiki-page"),
 
-    (r'^forum/', include('forum.urls')),
-
+    # admin views
     url(r'^admin/orderedmove/(?P<direction>up|down)/(?P<model_type_id>\d+)/(?P<model_id>\d+)/$', 'general.views.admin_move_ordered_model', name="admin-move"),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/(.*)', admin.site.root),
-    (r'helpdesk/', include('helpdesk.urls')),
 )
 
 if settings.DEBUG:
---
+++ freesound/wiki/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls.defaults import *
+import views as wiki
+
+urlpatterns = patterns('',
+    url(r'^$', "django.views.generic.simple.redirect_to", kwargs={'url': "/help/main/"}, name="wiki"),
+    url(r'^(?P<name>[//\w_-]+)/edit/$', wiki.editpage, name="wiki-page-edit"),
+    url(r'^(?P<name>[//\w_-]+)/$', wiki.page, name="wiki-page"),
+)

41f12b428de1489cc3b2b7c67f6d949617a8c677
Bram de Jong <bram.dejong+github@gmail.com>
2009-05-19 17:34:44 +0200

    updated the localsettings example file

--- freesound/local_settings.example.py
+++ freesound/local_settings.example.py
@@ -1,38 +1,46 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 DEBUG = True
 
 ADMINS = (
-    ('Your Name', 'Your Email'),
+    ('Bram de Jong', 'support@freesound.org'),
 )
 
-DATABASE_PASSWORD = 'password'
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/absolute/path/to/nightingale/trunk/freesound/media/'
+DATABASE_PASSWORD = 'x'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://127.0.0.1:8000/media/'
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = 'http://127.0.0.1:8000/media/admin_media/'
+ADMIN_MEDIA_PREFIX = '/media/admin_media/'
+
+# data path, where all files should go to
+DATA_PATH = '/Users/bram/Development/nightingale/freesound/media/data/'
+
+# data URL, hosted via lighttpd or something similar
+DATA_URL = MEDIA_URL + 'data/'
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'pgfwcaswp$ss(d@yn)!k)x#9tbgy!d!y59mwj4s4min^ft+hx)'
+SECRET_KEY = '....something unique and long here....'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    '/absolute/path/to/nightingale/trunk/freesound/templates'
-)
-
-EMAIL_HOST_USER = '' # in live this should be the address of a mailbox on iua-mail
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 2525
 
 SESSION_COOKIE_DOMAIN = None # leave this until you know what you are doing
+
+# place where files will be uploaded to via HTTP (and FTP)
+FILES_UPLOAD_DIRECTORY = "/Users/bram/Development/nightingale/freesound/uploads/uploads/"
+FILES_UPLOAD_OK_DIRECTORY = "/Users/bram/Development/nightingale/freesound/uploads/uploads_ok/"
+
+PROXIES = {} #{'http': 'http://proxy.upf.edu:8080'}
+
+RECAPTCHA_PRIVATE_KEY = 'your recaptcha private key'
+RECAPTCHA_PUBLIC_KEY = 'your recaptcha public key'
+
+GOOGLE_API_KEY = 'your google maps api key'

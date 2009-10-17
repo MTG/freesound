06670ec8f601cc23448e279baa63fe855184d7d1
Bram de Jong <bram.dejong@samplesumo.com>
2009-10-17 13:20:04 +0200

    fixed the DelayedQueryExecuter. if we did: [for x in query] [for x in query] the query would be evaluated two times. that's no longer happening

--- freesound/utils/sql.py
+++ freesound/utils/sql.py
@@ -1,12 +1,21 @@
 from django.db import connection
 
 class DelayedQueryExecuter:
+    """ The delayed executed executes a query, but waits for the first time
+    the results are actually needed, i.e. via iteration """
     def __init__(self, query):
         self.query = query
-        
+        self.cache = None
+    
     def __iter__(self):
-        cursor = connection.cursor()
-        cursor.execute(self.query)
-        collumn_names = [desc[0] for desc in cursor.description]
-        for row in cursor.fetchall():
-            yield dict(zip( collumn_names, row))
+        if self.cache is None:
+            cursor = connection.cursor() #@UndefinedVariable
+            cursor.execute(self.query)
+            
+            column_names = [desc[0] for desc in cursor.description]
+            
+            # cursor.fetchall fetches all results in one go (i.e. not a generator) so this is just as fast
+            self.cache = (dict(zip(column_names, row)) for row in cursor.fetchall())
+
+        for row in self.cache:
+            yield row

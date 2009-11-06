3aafb7a9cc995123883a7dfe9b8ed15baa22f8c4
Bram de Jong <bram.dejong@samplesumo.com>
2009-11-06 15:30:41 +0100

    wrongly used apply instead of map which resulted in all kinds of strangeness when using variables

--- freesound/utils/cache.py
+++ freesound/utils/cache.py
@@ -4,6 +4,6 @@ from django.utils.http import urlquote
 from functional import compose
 
 def invalidate_template_cache(fragment_name, *variables):
-    args = md5_constructor(u':'.join(apply(compose(urlquote, unicode), variables)))
+    args = md5_constructor(u':'.join(map(compose(urlquote, unicode), variables)))
     cache_key = 'template.cache.%s.%s' % (fragment_name, args.hexdigest())
     cache.delete(cache_key)

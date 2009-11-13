b355eb73509964f7937bcba9c90b066b8e8ede8b
Bram de Jong <bram.dejong@samplesumo.com>
2009-11-13 14:10:04 +0100

    local_settings

--- sandbox/db_conversion/comments.py
+++ sandbox/db_conversion/comments.py
@@ -1,14 +1,26 @@
-import MySQLdb as my
-import codecs, sys
-from django.template.defaultfilters import slugify
-from text_utils import prepare_for_insert, smart_character_decoding
 from HTMLParser import HTMLParseError
+from local_settings import *
+from text_utils import prepare_for_insert, smart_character_decoding
+import MySQLdb as my
+import codecs
+import psycopg2
 
 output_filename = '/tmp/importfile.dat'
 output_file = codecs.open(output_filename, 'wt', 'utf-8')
 
-my_conn = my.connect(host="localhost", user="freesound", passwd=sys.argv[1], db="freesound", unix_socket="/var/mysql/mysql.sock", use_unicode=False)
+my_conn = my.connect(**MYSQL_CONNECT)
 my_curs = my_conn.cursor()
+
+ppsql_conn = psycopg2.connect(POSTGRES_CONNECT)
+ppsql_cur = ppsql_conn.cursor()
+print "getting all valid sound ids"
+ppsql_cur.execute("SELECT id FROM sounds_sound")
+valid_sound_ids = dict((row[0],1) for row in ppsql_cur.fetchall())
+print "done"
+print "getting all valid user ids"
+ppsql_cur.execute("SELECT id FROM auth_user")
+valid_user_ids = dict((row[0],1) for row in ppsql_cur.fetchall())
+print "done"
 
 start = 0
 granularity = 1000
@@ -34,6 +46,9 @@ while True:
     
     for row in rows:
         id, object_id, user_id, created, comment = row
+        
+        if object_id not in valid_sound_ids or user_id not in valid_user_ids:
+            continue
         
         try:
             comment = prepare_for_insert(smart_character_decoding(comment))

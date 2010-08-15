d1b4bbecb76b830f3b7089d88d43839b68ad457b
Bram de Jong <bram.dejong@samplesumo.com>
2010-08-15 20:40:28 +0200

    added sound delete, for_user, remixes and sources views

--- freesound/sounds/views.py
+++ freesound/sounds/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.core.cache import cache
 from django.core.paginator import Paginator, InvalidPage
+from django.core.servers.basehttp import FileWrapper
+from django.core.urlresolvers import reverse
 from django.db.models import Count, Max
 from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
@@ -17,9 +19,19 @@ from sounds.forms import SoundDescriptionForm, PackForm, GeotaggingForm, \
     LicenseForm, FlagForm
 from sounds.models import Sound, Pack, Download
 from utils.cache import invalidate_template_cache
+from utils.encryption import encrypt, decrypt
+from utils.functional import combine_dicts
 from utils.mail import send_mail_template
+from utils.pagination import paginate
 from utils.text import slugify
 import os
+
+def get_random_sound():
+    cache_key = "random_sound"
+    random_sound = cache.get(cache_key)
+    if not random_sound:
+        random_sound = Sound.objects.random()
+        cache.set(cache_key, random_sound, 60*60*24)
 
 def front_page(request):
     rss_url = settings.FREESOUND_RSS
@@ -28,18 +40,13 @@ def front_page(request):
     latest_forum_posts = Post.objects.select_related('author', 'thread', 'thread__forum').all().order_by("-created")[0:10]
     latest_additions = Sound.objects.latest_additions(5)
     
-    cache_key = "random_sound"
-    random_sound = cache.get(cache_key)
-    if not random_sound:
-        random_sound = Sound.objects.random()
-        cache.set(cache_key, random_sound, 60*60*24)
+    random_sound = get_random_sound()
     
     return render_to_response('index.html', locals(), context_instance=RequestContext(request)) 
  
 
 def sounds(request):
     pass
-
 
 def sound(request, username, sound_id):
     try:
@@ -51,8 +58,6 @@ def sound(request, username, sound_id):
 
     content_type = ContentType.objects.get_for_model(Sound)
 
-    paginator = Paginator(Comment.objects.select_related("user").filter(content_type=content_type, object_id=sound_id), settings.SOUND_COMMENTS_PER_PAGE)
-
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
@@ -62,20 +67,10 @@ def sound(request, username, sound_id):
     else:
         form = CommentForm()
 
-    try:
-        current_page = int(request.GET.get("page", 1))
-    except ValueError:
-        current_page = 1
+    qs = Comment.objects.select_related("user").filter(content_type=content_type, object_id=sound_id)
 
-    try:
-        page = paginator.page(current_page)
-    except InvalidPage:
-        page = paginator.page(1)
-        current_page = 1
+    return render_to_response('sounds/sound.html', combine_dicts(locals(), paginate(request, qs, settings.SOUND_COMMENTS_PER_PAGE)), context_instance=RequestContext(request))
 
-    return render_to_response('sounds/sound.html', locals(), context_instance=RequestContext(request))
-
-from django.core.servers.basehttp import FileWrapper
 
 @login_required
 def sound_download(request, username, sound_id):
@@ -181,12 +176,14 @@ def sound_edit(request, username, sound_id):
     
 def remixes(request, username, sound_id):
     sound = get_object_or_404(Sound, user__username__iexact=username, id=sound_id, moderation_state="OK", processing_state="OK")
-    pass
+    qs = sound.remixes.filter(moderation_state="OK", processing_state="OK")
+    return render_to_response('sounds/remixes.html', combine_dicts(locals(), paginate(request, qs, settings.SOUNDS_PER_PAGE)), context_instance=RequestContext(request))
 
 
 def sources(request, username, sound_id):
     sound = get_object_or_404(Sound, user__username__iexact=username, id=sound_id, moderation_state="OK", processing_state="OK")
-    pass
+    qs = sound.sources.filter(moderation_state="OK", processing_state="OK")
+    return render_to_response('sounds/sources.html', combine_dicts(locals(), paginate(request, qs, settings.SOUNDS_PER_PAGE)), context_instance=RequestContext(request))
 
 
 def geotag(request, username, sound_id):
@@ -202,21 +199,8 @@ def similar(request, username, sound_id):
 
 def pack(request, username, pack_id):
     pack = get_object_or_404(Pack, user__username__iexact=username, id=pack_id)
-
-    paginator = Paginator(Sound.objects.select_related('user').filter(pack=pack, moderation_state="OK", processing_state="OK"), settings.SOUNDS_PER_PAGE)
-
-    try:
-        current_page = int(request.GET.get("page", 1))
-    except ValueError:
-        current_page = 1
-
-    try:
-        page = paginator.page(current_page)
-    except InvalidPage:
-        page = paginator.page(1)
-        current_page = 1
-
-    return render_to_response('sounds/pack.html', locals(), context_instance=RequestContext(request))
+    qs = Sound.objects.select_related('user').filter(pack=pack, moderation_state="OK", processing_state="OK")
+    return render_to_response('sounds/pack.html', combine_dicts(locals(), paginate(request, qs, settings.SOUNDS_PER_PAGE)), context_instance=RequestContext(request))
 
 
 def remixed(request):
@@ -233,30 +217,48 @@ def packs(request):
 
 def packs_for_user(request, username):
     user = get_object_or_404(User, username__iexact=username)
-
     order = request.GET.get("order", "name")
-    
     if order not in ["name", "-last_update", "-created", "-num_sounds"]:
         order = "name"
-    
-    paginator = Paginator(Pack.objects.filter(user=user, sound__moderation_state="OK", sound__processing_state="OK").annotate(num_sounds=Count('sound'), last_update=Max('sound__created')).filter(num_sounds__gt=0).order_by(order), settings.PACKS_PER_PAGE)
-
-    try:
-        current_page = int(request.GET.get("page", 1))
-    except ValueError:
-        current_page = 1
-
-    try:
-        page = paginator.page(current_page)
-    except InvalidPage:
-        page = paginator.page(1)
-        current_page = 1
-
-    return render_to_response('sounds/packs.html', locals(), context_instance=RequestContext(request))
+    qs = Pack.objects.filter(user=user, sound__moderation_state="OK", sound__processing_state="OK").annotate(num_sounds=Count('sound'), last_update=Max('sound__created')).filter(num_sounds__gt=0).order_by(order)
+    return render_to_response('sounds/packs.html', combine_dicts(paginate(request, qs, settings.PACKS_PER_PAGE), locals()), context_instance=RequestContext(request))
 
 
 def for_user(request, username):
-    pass
+    user = get_object_or_404(User, username__iexact=username)
+    qs = Sound.public.filter(user=user)
+    return render_to_response('sounds/for_user.html', combine_dicts(paginate(request, qs, settings.SOUNDS_PER_PAGE), locals()), context_instance=RequestContext(request))
+    
+
+@login_required
+def delete(request, username, sound_id):
+    sound = get_object_or_404(Sound, user__username__iexact=username, id=sound_id, moderation_state="OK", processing_state="OK")
+    
+    if not (request.user.has_perm('sound.can_change') or sound.user == request.user):
+        raise PermissionDenied
+
+    import time
+    
+    encrypted_string = request.GET.get("sound", None)
+     
+    waited_too_long = False
+    
+    if encrypted_string != None:
+        try:
+            sound_id, now = decrypt(encrypted_string).split("\t")
+            sound_id = int(sound_id)
+            link_generated_time = float(now)
+            if abs(time.time() - link_generated_time) < 10:
+                sound.delete()
+                return HttpResponseRedirect(reverse("accounts-home"))
+            else:
+                waited_too_long = True
+        except:
+            pass
+    
+    encrypted_link = encrypt(u"%d\t%f" % (sound.id, time.time()))
+            
+    return render_to_response('sounds/delete.html', locals(), context_instance=RequestContext(request))    
 
 
 def flag(request, username, sound_id):

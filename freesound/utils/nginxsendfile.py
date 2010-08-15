d1b43614b5130e33c17461b8eb185ee91568086a
Bram de Jong <bram.dejong@samplesumo.com>
2010-08-15 15:55:38 +0200

    minor changes to x-accell-redirect example

--- freesound/utils/nginxsendfile.py
+++ freesound/utils/nginxsendfile.py
@@ -2,17 +2,15 @@ from django.http import HttpResponse,Http404
 from django.core.servers.basehttp import FileWrapper
 from django.conf import settings
 
-import mimetypes
 import os
 
-def basic_sendfile(fname,download_name=None):
+def basic_sendfile(fname, download_name=None):
     if not os.path.exists(fname):
         raise Http404
 
     wrapper = FileWrapper(open(fname,"r"))
 
-    content_type = mimetypes.guess_type(fname)[0] or 'application/octet-stream'
-    response = HttpResponse(wrapper, content_type=content_type)
+    response = HttpResponse(wrapper, content_type='application/octet-stream')
     response['Content-Length'] = os.path.getsize(fname)
 
     if download_name:
@@ -20,12 +18,11 @@ def basic_sendfile(fname,download_name=None):
 
     return response
 
-def x_sendfile(fname,download_name=None):
+def x_sendfile(fname, download_name=None):
     if not os.path.exists(fname):
         raise Http404
 
-    content_type = mimetypes.guess_type(fname)[0] or 'application/octet-stream'
-    response = HttpResponse('', content_type=content_type)
+    response = HttpResponse('', content_type='application/octet-stream')
     response['Content-Length'] = os.path.getsize(fname)
     response['X-Accel-Redirect'] = fname
 

ba22b00affa7d91acac5972d5702eb21f0834515
Vincent Akkermans <vincent.akkermans@upf.edu>
2010-08-18 10:37:00 +0200

    changed unnecessary CrsfExemptHandler

--- freesound/api/urls.py
+++ freesound/api/urls.py
@@ -6,21 +6,20 @@ from key_authentication import KeyAuthentication
 
 auth = KeyAuthentication()
 
-class CsrfExemptResource(Resource):
-    """A Custom Resource that is csrf exempt"""
+class AR(Resource):
+    ''' Uses the standard authentication mechanism '''
     def __init__(self, handler, authentication=auth):
-        super(CsrfExemptResource, self).__init__(handler, authentication)
-        self.csrf_exempt = getattr(self.handler, 'csrf_exempt', True)
+        super(AR, self).__init__(handler, authentication)
 
 urlpatterns = patterns('',
     # sounds
-    url(r'^sounds/search/?$',                                        CsrfExemptResource(SoundSearchHandler), name='api-search'),
-    url(r'^sounds/(?P<sound_id>\d+)/?$',                             CsrfExemptResource(SoundHandler),       name='single-sound'),
-    url(r'^sounds/(?P<sound_id>\d+)/(?P<file_or_preview>\w+)/?$',    CsrfExemptResource(SoundServeHandler),  name='sound-serve'),
+    url(r'^sounds/search/?$',                                        AR(SoundSearchHandler), name='api-search'),
+    url(r'^sounds/(?P<sound_id>\d+)/?$',                             AR(SoundHandler),       name='single-sound'),
+    url(r'^sounds/(?P<sound_id>\d+)/(?P<file_or_preview>\w+)/?$',    AR(SoundServeHandler),  name='sound-serve'),
     # users
-    url(r'^people/(?P<username>[\w_-]+)/?$',                         CsrfExemptResource(UserHandler),        name='single-user'),
-    url(r'^people/(?P<username>[\w_-]+)/sounds/?$',                  CsrfExemptResource(UserSoundsHandler),  name='user-sounds'),
-    url(r'^people/(?P<username>[\w_-]+)/packs/?$',                   CsrfExemptResource(UserPacksHandler),   name='user-packs'),
+    url(r'^people/(?P<username>[\w_-]+)/?$',                         AR(UserHandler),        name='single-user'),
+    url(r'^people/(?P<username>[\w_-]+)/sounds/?$',                  AR(UserSoundsHandler),  name='user-sounds'),
+    url(r'^people/(?P<username>[\w_-]+)/packs/?$',                   AR(UserPacksHandler),   name='user-packs'),
     
    # url(r'^update_solr/?$',                                             CsrfExemptResource(UpdateSolrHandler)),
    # website

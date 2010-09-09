86b68a107cd6917792e135156597b0377cb7747e
Bram de Jong <bram.dejong@samplesumo.com>
2010-09-09 21:31:17 +0200

    moved many things outside of the try-catch

--- freesound/utils/audioprocessing/freesound_audio_processing.py
+++ freesound/utils/audioprocessing/freesound_audio_processing.py
@@ -77,12 +77,13 @@ def process(sound):
         return False
     
     # create preview
+    mp3_path = os.path.join(settings.DATA_PATH, paths["preview_path"])
     try:
-        mp3_path = os.path.join(settings.DATA_PATH, paths["preview_path"])
-        try:
-            os.makedirs(os.path.dirname(mp3_path))
-        except OSError:
-            pass
+        os.makedirs(os.path.dirname(mp3_path))
+    except OSError:
+        pass
+    
+    try:
         audioprocessing.convert_to_mp3(tmp_wavefile, mp3_path)
         success("created mp3: " + mp3_path)
     except Exception, e:
@@ -90,9 +91,10 @@ def process(sound):
         failure("conversion to mp3 (preview) has failed", e)
         return False
     
+    # create waveform images M
+    waveform_path_m = os.path.join(settings.DATA_PATH, paths["waveform_path_m"])
+    spectral_path_m = os.path.join(settings.DATA_PATH, paths["spectral_path_m"])
     try:
-        waveform_path_m = os.path.join(settings.DATA_PATH, paths["waveform_path_m"])
-        spectral_path_m = os.path.join(settings.DATA_PATH, paths["spectral_path_m"])
         audioprocessing.create_wave_images(tmp_wavefile, waveform_path_m, spectral_path_m, 120, 71, 2048)
         success("created png, medium size: " + waveform_path_m)
     except Exception, e:
@@ -100,9 +102,10 @@ def process(sound):
         failure("creation of images (M) has failed", e)
         return False
 
+    # create waveform images L
+    waveform_path_l = os.path.join(settings.DATA_PATH, paths["waveform_path_l"])
+    spectral_path_l = os.path.join(settings.DATA_PATH, paths["spectral_path_l"])
     try:
-        waveform_path_l = os.path.join(settings.DATA_PATH, paths["waveform_path_l"])
-        spectral_path_l = os.path.join(settings.DATA_PATH, paths["spectral_path_l"])
         audioprocessing.create_wave_images(tmp_wavefile, waveform_path_l, spectral_path_l, 900, 201, 2048)
         success("created png, large size: " + waveform_path_l)
     except Exception, e:
@@ -114,17 +117,19 @@ def process(sound):
     new_original_path = os.path.join(settings.DATA_PATH, paths["sound_path"])
     if sound.original_path != new_original_path:
         try:
-            try:
-                os.makedirs(os.path.dirname(new_original_path))
-            except OSError:
-                pass
+            os.makedirs(os.path.dirname(new_original_path))
+        except OSError:
+            pass
+
+        try:
             #shutil.move(sound.original_path, new_original_path)
             shutil.copy(sound.original_path, new_original_path)
-            sound.original_path = new_original_path
-            sound.save()
             success("moved original file from %s to %s" % (sound.original_path, new_original_path))
         except IOError, e:
             failure("failed to move file from %s to %s" % (sound.original_path, new_original_path), e) 
+
+        sound.original_path = new_original_path
+        sound.save()
         
     cleanup([tmp_wavefile])
     sound.processing_state = "OK"

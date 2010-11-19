06a2ac5048400691d691126cbc452c112120803c
Vincent Akkermans <vincent.akkermans@upf.edu>
2010-11-19 16:28:48 +0100

    more flexible reporting

--- utilities/reporting/fs1/generate_report.py
+++ utilities/reporting/fs1/generate_report.py
@@ -27,6 +27,7 @@ def generate_dates(start, end, delta_months, delta_days):
 
 MONTHS = [x for x in generate_dates(THEN, NOW, 1, 0)]
 WEEKS  = [x for x in generate_dates(THEN, NOW, 0, 7)]
+DAYS   = [x for x in generate_dates(THEN, NOW, 0, 1)]
 
 
 def execute_query(query, dates):
@@ -53,7 +54,7 @@ def get_downloads_accum(dates):
     return execute_query(sql, dates)
 
 def get_uploads_accum(dates):
-    sql = "select count(*) from audio_file where FROM_date(dateAdded) < '{y}-{m}-{d}';"
+    sql = "select count(*) from audio_file where date(dateAdded) < '{y}-{m}-{d}';"
     return execute_query(sql, dates)
 
 
@@ -61,19 +62,50 @@ def get_uploads_accum(dates):
 
 
 if __name__ == '__main__':
+
+    queries = {'users_accum':
+               "select count(*) from phpbb_users where FROM_UNIXTIME(user_regdate) < '{y}-{m}-{d}';",
+               'downloads_monthly':
+               "select count(*) from audio_file_downloads where year(date) = {y} AND month(date) = {m};",
+               'downloads_daily':
+               "select count(*) from audio_file_downloads where date(date) = '{y}-{m}-{d}';",
+               'uploads_monthly':
+               "select count(*) from audio_file where year(dateAdded) = {y} AND month(dateAdded) = {m};",
+               'uploads_daily':
+               "select count(*) from audio_file where date(dateAdded) = '{y}-{m}-{d}';",
+               }
+
     data = {}
-    queries = [#('users_accum_monthly',      lambda: get_users_accum(MONTHS)),
-               #('users_accum_weekly',       lambda: get_users_accum(WEEKS)),
-               #('downloads_accum_monthly',  lambda: get_downloads_accum(MONTHS[0:4])),
-               #('downloads_accum_weekly',   lambda: get_downloads_accum(WEEKS)),
-               #('uploads_accum_monthly',    lambda: get_downloads_accum(MONTHS)),
-               ('uploads_accum_weekly',     lambda: get_downloads_accum(WEEKS)),
+    execs    = [('users_accum_monthly', # will be the index in the json result file
+                'users_accum',         # will be used to look up the sql query
+                MONTHS),               # will be used as input for the dates
+
+               ('users_accum_weekly',
+                'users_accum',
+                WEEKS),
+
+               ('downloads_monthly',
+                'downloads_monthly',
+                MONTHS),
+
+               ('downloads_daily',
+                'downloads_daily',
+                DAYS),
+
+               ('uploads_monthly',
+                'uploads_monthly',
+                DAYS),
+
+               ('uploads_daily',
+                'uploads_daily',
+                DAYS),
               ]
-    for name, func in queries:
-        print 'Executing query: %s' % name
-        data[name] = func()
+    for result_name, query_name, dates in execs:
+        print 'Executing query: %s' % result_name
+        data[result_name] = execute_query(queries[query_name], dates)
         print '.'
-    report_file = os.path.join(os.path.dirname(__file__), 'report_freesound1_%d%02d%02d.json' % (NOW.year, NOW.month, NOW.day))
+    report_file = os.path.join(os.path.dirname(__file__), 'report_freesound1_%d%02d%02d_to_%d%02d%02d.json' \
+                               % (THEN.year, THEN.month, THEN.day, NOW.year, NOW.month, NOW.day))
     fp = file(report_file, 'w')
     json.dump(data, fp)
     fp.close()

5120d1b0a4d8951dadf6ab3ac50429c0bc8ca287
Bram de Jong <bram.dejong@samplesumo.com>
2010-12-21 23:47:19 +0100

    downloads need to be unique for (sound/user/pack) combo

--- freesound/sounds/models.py
+++ freesound/sounds/models.py
@@ -327,4 +327,5 @@ class Download(models.Model):
     created = models.DateTimeField(db_index=True, auto_now_add=True)
     
     class Meta:
+        unique_together = ('user', 'sound', 'pack')
         ordering = ("-created",)

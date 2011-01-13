7cbc5530643ea953186d8ba827c50a5345e40d25
groma <gerard.roma@upf.edu>
2011-01-13 13:16:14 +0100

    get last date from the database for development

---
+++ freesound/utils/dbtime.py
@@ -0,0 +1,21 @@
+from sounds.models import Download
+from sounds.models import Sound
+from django.core.cache import cache
+from django.conf import settings
+from datetime import datetime
+
+class DBTime():
+    last_time = None
+
+    @staticmethod
+    def get_last_time():
+	if not settings.DEBUG:
+	    return datetime.now() 
+	if DBTime.last_time is None:
+            cache_key = "last_download_time"
+            last_time = cache.get(cache_key)
+            if not last_time:
+                last_time = Download.objects.order_by('-created')[0].created
+                cache.set(cache_key, DBTime.last_time, 60*60*24)
+            DBTime.last_time = last_time
+	return DBTime.last_time

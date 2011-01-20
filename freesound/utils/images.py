eaec3507ef5fbebfc644e73cb37c8db0aa7c62c7
Marius Miron <miron.marius@gmail.com>
2011-01-20 15:38:19 +0100

    Ticket #41 Resized the image and filled with white background when needed

--- freesound/utils/images.py
+++ freesound/utils/images.py
@@ -1,18 +1,38 @@
 from PIL import Image
 
+
 class ImageProcessingError(Exception):
     pass
 
 def extract_square(input_filename, output_filename, size):
     im = Image.open(input_filename)
     
-    if im.size[0] < size or im.size[1] < size:
-        #raise ImageProcessingError, "image too small"
-        #fill out to exactly maxSize (square image)          
+    if im.mode not in ('L', 'RGB'):
+        im = im.convert('RGB')
+        
+    #fill out and resize the image
+    if im.size[0] < size and im.size[1] < size:
+        if im.size[0] < im.size[1]:
+            ratio = im.size[1] / im.size[0]
+            im = im.resize((size / ratio, size), Image.ANTIALIAS)            
+        else: 
+            ratio = im.size[0] / im.size[1] 
+            im = im.resize((size,size / ratio), Image.ANTIALIAS)
+        #fill out          
         background = Image.new("RGBA", (size,size), (255, 255, 255, 0)) # use white for empty space  
         background.paste(im, ((size - im.size[0]) / 2, (size - im.size[1]) / 2))  
         background.save(output_filename)
-
+        return
+        
+    #if one side of the image is smaller and one is bigger
+    elif im.size[0] > size and im.size[1] < size:
+            ratio = im.size[0] / im.size[1]
+            im = im.resize((size * ratio,size), Image.ANTIALIAS)  
+                     
+    elif im.size[0] < size and im.size[1] > size:  
+            ratio = im.size[1] / im.size[0]
+            im = im.resize((size, size * ratio), Image.ANTIALIAS)
+           
     if im.size[0] > im.size[1]:
         # --------
         # |      |

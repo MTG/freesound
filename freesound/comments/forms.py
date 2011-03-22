ed3e08837146a31e57bcde6975a8ea2dc8b0559d
Bram de Jong <bram.dejong@samplesumo.com>
2011-03-22 19:55:14 +0100

    added comment spam protection

--- freesound/comments/forms.py
+++ freesound/comments/forms.py
@@ -1,5 +1,18 @@
 from django import forms
 from utils.forms import HtmlCleaningCharField
+from utils.spam import is_spam
 
 class CommentForm(forms.Form):
     comment = HtmlCleaningCharField(widget=forms.Textarea)
+    
+    def __init__(self, request, *args, **kwargs):
+        self.request = request
+        super(CommentForm, self).__init__(*args, **kwargs)
+    
+    def clean_comment(self):
+        comment = self.cleaned_data['comment']
+
+        if is_spam(self.request, comment):
+            raise forms.ValidationError("Your comment was considered spam, please edit and repost. If it keeps failing please contact the admins.")
+        
+        return comment

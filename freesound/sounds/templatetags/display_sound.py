53bb3c2b5295630c9612a381fb5e4606971d3498
groma <gerard.roma@upf.edu>
2011-04-13 17:50:14 +0200

    support for more compact sound list views

--- freesound/sounds/templatetags/display_sound.py
+++ freesound/sounds/templatetags/display_sound.py
@@ -1,14 +1,19 @@
+from __future__ import absolute_import
+from tags.models import TaggedItem as ti 
+#avoid namespace clash with 'tags' templatetag
+
 from django import template
 from sounds.models import Sound
 
 register = template.Library()
-
 @register.inclusion_tag('sounds/display_sound.html', takes_context=True)
+
 def display_sound(context, sound):
     if isinstance(sound, Sound):
         return {
                 'sound_id': sound.id,
                 'sound': [sound],
+                'sound_tags':ti.objects.select_related().filter(object_id=sound.id, content_type=18).all(),
                 'media_url': context['media_url'],
                 'sounds_url': context['sounds_url']
                 }        
@@ -16,9 +21,14 @@ def display_sound(context, sound):
         return {
                 'sound_id': sound,
                 'sound': Sound.objects.select_related('user').filter(id=sound), # need to use filter here because we don't want the query to be evaluated already!
+                'sound_tags':ti.objects.select_related().filter(object_id=sound, content_type=18).all(),
                 'media_url': context['media_url'],
                 'sounds_url': context['sounds_url']
                 }
     else:
         print "sound is", sound, type(sound)
         raise template.TemplateSyntaxError, "the display_sound tag needs either a sound id or an actual sound object"
+        
+
+
+

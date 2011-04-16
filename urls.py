ca63ec491574ac3d4289514c294a619a049908a9
Bram de Jong <bram.dejong@samplesumo.com>
2011-04-16 17:59:51 +0200

    fixed avatars...

--- urls.py
+++ urls.py
@@ -75,5 +75,6 @@ from django.conf import settings
 if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^%s/(?P<path>.*)$' % settings.MEDIA_URL.strip('/'), 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        (r'^%s/(?P<path>.*)$' % settings.DATA_URL.strip('/'), 'django.views.static.serve', {'document_root': settings.DATA_PATH, 'show_indexes': True}),
         (r'^fsmedia/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     )

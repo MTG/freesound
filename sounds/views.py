df6b2f18f7a60d735d0672cd1d066a93f9f05874
john <john@john-server.chello.ie>
2011-04-17 15:59:44 +0100

    Adding functionaity to allow emails to be sent to a user when someone else leaves a comment on one of their sounds

--- sounds/views.py
+++ sounds/views.py
@@ -110,10 +110,23 @@ def sound(request, username, sound_id):
     if request.method == "POST":
         form = CommentForm(request, request.POST)
         if form.is_valid():
-            sound.comments.add(Comment(content_object=sound, user=request.user, comment=form.cleaned_data["comment"]))
+            comment_text=form.cleaned_data["comment"]
+            sound.comments.add(Comment(content_object=sound, user=request.user, comment=comment_text))
 
             sound.num_comments = sound.num_comments + 1
-            sound.save() 
+            sound.save()
+            try:
+                # send the user an email to notify him of the new comment!
+                print "Gonna send a mail to this user: %s" % request.user.email 
+                send_mail_template(
+                    u'You have a new comment.', 'sounds/email_new_comment.txt', 
+                    {'sound': sound, 'user': request.user, 'comment': comment_text}, 
+                    None, sound.user.email
+                )
+            except Exception, e:
+                # if the email sending fails, ignore...
+                print ("Problem sending email to '%s' about new comment: %s" \
+                    % (request.user.email, e))
             
             return HttpResponseRedirect(sound.get_absolute_url())
     else:

989d075cb7ba8218c143a424161dbbdb6d08224a
Bram de Jong <bram.dejong@samplesumo.com>
2011-04-17 19:38:09 +0200

    renaming

--- sounds/management/commands/process_reschedule_late.py
+++ sounds/management/commands/process_reschedule_late.py
@@ -3,11 +3,11 @@ from sounds.models import Sound
 from datetime import datetime
 
 class Command(BaseCommand):
-    args = '<num_days>'
-    help = 'Take all sounds that have been sitting the processing queue marked as "being processed" and reschedule them'
+    args = '<num_hours>'
+    help = 'Take all sounds that have been sitting (for num_hours) the processing queue marked as "being processed" and reschedule them'
 
     def handle(self, *args, **options):
-        num_days = int(args[0])
-        for sound in Sound.objects.filter(processing_state="PR", processing_date__lt=datetime.now()-datetime.timedelta(num_days)):
+        num_hours = int(args[0])
+        for sound in Sound.objects.filter(processing_state="PR", processing_date__lt=datetime.now()-datetime.timedelta(hours=num_hours)):
             sound.processing_state = "PE"
             sound.save()

28a2eb29ef6fff641cc7072356dd59c541cb59e7
Bram de Jong <bram.dejong@samplesumo.com>
2011-04-17 13:08:04 +0200

    new local settings

--- local_settings.example.py
+++ local_settings.example.py
@@ -1,57 +1,32 @@
 # -*- coding: utf-8 -*-
 
 DEBUG = True
+DISPLAY_DEBUG_TOOLBAR = True
 
 ADMINS = (
-    ('Bram de Jong', 'support@freesound.org'),
+    ('Your Email Here', 'abc@gmail.com'),
 )
 
-DATABASE_ENGINE = 'sqlite'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'freesound'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_USER = 'freesound'             # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 DATABASE_PASSWORD = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://localhost:8080/fsmedia/'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/admin_media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = ''
 
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 2525
-#EMAIL_HOST_USER = ''
-#EMAIL_HOST_PASSWORD = ''
 
-SESSION_COOKIE_DOMAIN = None # leave this until you know what you are doing
-
-PROXIES = {}
-#PROXIES = {'http': 'http://proxy.upf.edu:8080'}
+PROXIES = {} #'http': 'http://proxy.upf.edu:8080'}
 
 RECAPTCHA_PRIVATE_KEY = ''
 RECAPTCHA_PUBLIC_KEY = ''
 AKISMET_KEY = ''
 
-
 GOOGLE_API_KEY = ''
+GOOGLE_ANALYTICS_KEY = ''
 
 SOLR_URL = "http://localhost:8983/solr/"
-
-DISPLAY_DEBUG_TOOLBAR = False # change this in the local_settings
-
-STEREOFY_PATH = "/home/fsweb/freesound/sandbox/legacy/stereofy/stereofy"
-
-FILES_UPLOAD_DIRECTORY_MOUNT = '/mnt/freesound/'
-
-SAMBA_FLAG = False #change this if you want to use the samba mount on the local disk
-
-if SAMBA_FLAG:
-    FILES_UPLOAD_DIRECTORY = FILES_UPLOAD_DIRECTORY_MOUNT
--- settings.py
+++ settings.py
@@ -164,6 +164,8 @@ PACKS_SENDFILE_URL = SENDFILE_SECRET_URL + "packs/"
 
 STEREOFY_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '_sandbox/stereofy/stereofy'))
 
+SESSION_COOKIE_DOMAIN = None # leave this until you know what you are doing
+
 # leave at bottom starting here!
 from local_settings import * #@UnusedWildImport
 

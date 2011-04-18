9846b8b22200f642e357bce81aae3a8f7c5c26c3
Bram de Jong <bram.dejong@samplesumo.com>
2011-04-19 01:02:59 +0200

    remove all files except compilation script

--- _sandbox/flashPlayer/src/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mxmlc -target-player=10.0 LargePlayer.as
-mxmlc -target-player=10.0 MediumPlayer.as
-
-mv LargePlayer.swf ../../../media/swf/LargePlayer.swf
-mv MediumPlayer.swf ../../../media/swf/MediumPlayer.swf

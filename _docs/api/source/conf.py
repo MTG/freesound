46bf49d22cfb8b0f398e593c283da08b840ae401
Vincent Akkermans <vincent.akkermans@gmail.com>
2011-04-27 16:33:08 +0200

    changed copyright to university, at least its a legal entity

--- _docs/api/source/conf.py
+++ _docs/api/source/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Freesound API'
-copyright = u'2010, Bram de Jong, Vincent Akkermans'
+copyright = u'2010, Universitat Pompeu Fabra'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

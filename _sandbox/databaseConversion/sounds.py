41ead1a7046dfea0513030bb206dc05856cde09f
Vincent Akkermans <vincent.akkermans@gmail.com>
2011-05-11 18:37:48 +0200

    adding similarity_state to conversion script

--- _sandbox/databaseConversion/sounds.py
+++ _sandbox/databaseConversion/sounds.py
@@ -29,59 +29,60 @@ while True:
     my_curs.execute("SELECT ID, originalFilename, userID, duration, bitrate, bitdepth, filesize, dateAdded, samplerate, channels, packID, moderated, badDescription, md5, newFilename, extension FROM audio_file ORDER BY dateAdded limit %d, %d" % (start, granularity))
     rows = my_curs.fetchall()
     start += len(rows)
-    
+
     if len(rows) == 0:
         break
-    
+
     for row in rows:
         id, original_filename, user_id, duration, bitrate, bitdepth, filesize, created, samplerate, channels, pack_id, moderation_state, moderation_bad_description, md5, base_filename_slug, type = row
-        
+
         if user_id not in valid_user_ids:
             continue
-        
+
         original_filename = smart_character_decoding(original_filename)
-        
+
         if id in missing_files:
             continue
-        
+
         if md5 in md5s:
             continue
         else:
             md5s[md5] = 1
-        
+
         my_curs.execute("SELECT text FROM audio_file_text_description where audioFileId = %d" % id)
-        
+
         descriptions = []
         for d in my_curs.fetchall():
             descriptions.append(smart_character_decoding(d[0]))
 
         description = prepare_for_insert( u"\n".join(descriptions))
-        
+
         original_path = None
         moderation_date = created
         processing_date = created
+        similarity_state = "PE"
         processing_state = "PE"
         license_id = 1
         processing_log = None
-        
+
         type = type.lower()
-        
+
         if moderation_state in [0,3]:
             moderation_state = "PE"
         elif moderation_state == 1:
             moderation_state = "OK"
         elif moderation_state in [2,4]:
             moderation_state = "DE"
-            
+
         moderation_bad_description = "f" if moderation_bad_description == 0 else "t"
-        
+
         geotag = None
         num_comments = 0
         num_downloads = 0
         avg_rating = 0
         num_ratings = 0
         moderation_note = None
-        
+
         all_vars = [id,
             user_id,
             created,
@@ -106,12 +107,17 @@ while True:
             processing_state,
             processing_date,
             processing_log,
-            geotag, num_comments, num_downloads, avg_rating, num_ratings]
-        
+            geotag,
+            num_comments,
+            num_downloads,
+            avg_rating,
+            num_ratings,
+            similarity_state]
+
         output_file.write(u"\t".join(map(unicode, all_vars)) + "\n")
 
 print """
-copy sounds_sound (id, user_id, created, original_path, base_filename_slug, description, license_id, original_filename, pack_id, type, duration, bitrate, bitdepth, samplerate, filesize, channels, md5, moderation_state, moderation_date, moderation_note, has_bad_description, processing_state, processing_date, processing_log, geotag_id, num_comments, num_downloads, avg_rating, num_ratings) from '%s' null as 'None';
+copy sounds_sound (id, user_id, created, original_path, base_filename_slug, description, license_id, original_filename, pack_id, type, duration, bitrate, bitdepth, samplerate, filesize, channels, md5, moderation_state, moderation_date, moderation_note, has_bad_description, processing_state, processing_date, processing_log, geotag_id, num_comments, num_downloads, avg_rating, num_ratings, similarity_state) from '%s' null as 'None';
 select setval('sounds_sound_id_seq',(select max(id)+1 from sounds_sound));
 update sounds_sound set original_path = '/mnt/freesound-data/' || id/1000 || '/sounds/' || base_filename_slug || '.' || type;
 vacuum analyze sounds_sound;

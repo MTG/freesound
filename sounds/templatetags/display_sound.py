a246dd613526a9d98316c48a4195b584de45ea50
Vincent Akkermans <vincent.akkermans@gmail.com>
2011-05-23 18:48:48 +0200

    removed useless cruft from things

--- sounds/templatetags/display_sound.py
+++ sounds/templatetags/display_sound.py
@@ -8,8 +8,8 @@ from django import template
 register = template.Library()
 @register.inclusion_tag('sounds/display_sound.html', takes_context=True)
 
-def display_sound(context, sound, player_class):
-    
+def display_sound(context, sound):
+
     if isinstance(sound, Sound):
         sound_id = sound.id
         sound_obj = sound
@@ -20,9 +20,7 @@ def display_sound(context, sound, player_class):
         except Sound.DoesNotExist:
             sound_obj = False
 
-
-    return { 'player_class': player_class,
-             'sound_id':     sound_id,
+    return { 'sound_id':     sound_id,
              'sound':        sound_obj,
              'sound_tags':   ti.objects.select_related() \
                                 .filter(object_id=sound_id,

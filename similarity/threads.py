0b5d25bfffd3362e8be4d6f1f28803a899ad9639
Vincent Akkermans <vincent.akkermans@gmail.com>
2011-05-24 13:00:54 +0200

    sorry, debugging remotely

--- similarity/threads.py
+++ similarity/threads.py
@@ -50,7 +50,7 @@ class SimilarityThread(threading.Thread):
             self.reply(res)
 
         elif msg['type'] == 'AddSound':
-            logger.debug(msg)
+            self.logger.debug(msg)
             sound_id = getattr(msg, 'sound_id', False)
             yaml  = getattr(msg, 'yaml', False)
             if not sound_id or not yaml:

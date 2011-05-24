c2048123b58f73772cd0a545ba6577a68f4abea4
Vincent Akkermans <vincent.akkermans@gmail.com>
2011-05-24 12:41:46 +0200

    oops, localhost should not be used

--- similarity/settings.py
+++ similarity/settings.py
@@ -4,5 +4,5 @@ INDEX_DIR                   = '/home/fsweb/freesound-similarity/'
 PRESET_DIR                  = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'presets/')
 PRESETS                     = ['lowlevel', 'music', 'rhythm', 'timbre']
 SIMILARITY_MINIMUM_POINTS   = 10000
-REQREP_ADDRESS              = "tcp://127.0.0.1:9070"
+REQREP_ADDRESS              = "tcp://cuernavaca.s.upf.edu:9070"
 NUM_THREADS                 = 5

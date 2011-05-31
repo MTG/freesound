a83f739b85067ac9a9e99b6c47c137269c2c6243
Vincent Akkermans <vincent.akkermans@gmail.com>
2011-05-31 11:38:39 +0200

    stupid import error

--- similarity/logger.py
+++ similarity/logger.py
@@ -1,11 +1,12 @@
 import logging
+from logging.handlers import RotatingFileHandler
 from settings import LOGFILE, LOGFILE_LEVEL
 
 logger      = logging.getLogger('similarity')
 
-handler     = logging.handlers.RotatingFileHandler(LOGFILE,
-                                                   maxBytes=2*1024*1024,
-                                                   backupCount=5)
+handler     = RotatingFileHandler(LOGFILE,
+                                  maxBytes=2*1024*1024,
+                                  backupCount=5)
 handler.setLevel(LOGFILE_LEVEL)
 
 formatter   = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')

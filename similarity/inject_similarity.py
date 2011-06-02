451d344236de26fe736a18cb424a992415cc7054
Vincent Akkermans <vincent.akkermans@gmail.com>
2011-06-02 12:02:34 +0200

    cleaning dataset after minimum points so the whole thing fits in memory

--- similarity/inject_similarity.py
+++ similarity/inject_similarity.py
@@ -5,35 +5,41 @@ a Gaia dataset with all the files it finds.
 '''
 
 import os, sys, gaia2
-from settings import INDEX_DIR
+from settings import INDEX_DIR, SIMILARITY_MINIMUM_POINTS
 from gaia_wrapper import GaiaWrapper
 
 counter = 0
+dataset = False
 
 def prepare_dataset(dir, target):
+    global dataset
     dataset = gaia2.DataSet()
-    os.path.walk(dir, walk_func, dataset)
+    os.path.walk(dir, walk_func, False)
     dataset.save(target)
 
 
-def walk_func(dataset, dirname, names):
+def walk_func(arg, dirname, names):
+    """arg is unused but required for os.path.walk"""
     global counter
     for name in names:
         path = os.path.join(dirname, name)
         basename, ext = os.path.splitext(name)
         if os.path.isfile(path) and ext == '.yaml':
-            handle_resource(path, basename, dataset)
+            handle_resource(path, basename)
             # progress counter
             counter += 1
             if counter % 100 == 0:
                 print 'Processed %s yamls' % counter
 
 
-def handle_resource(path, name, dataset):
+def handle_resource(path, name):
+    global dataset
     p = gaia2.Point()
     p.load(path)
     p.setName(name.split('_')[0])
     dataset.addPoint(p)
+    if dataset.size() == SIMILARITY_MINIMUM_POINTS:
+        dataset = GaiaWrapper.prepare_original_dataset_helper(dataset)
 
 
 if __name__ == '__main__':

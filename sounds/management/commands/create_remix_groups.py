25c78468254fccdf25b37e41df211cc612257e41
Vincent Akkermans <vincent.akkermans@gmail.com>
2011-06-07 17:55:03 +0200

    not using simplejson anywhere anymore

--- sounds/management/commands/create_remix_groups.py
+++ sounds/management/commands/create_remix_groups.py
@@ -9,7 +9,7 @@ from django.db import connection
 from networkx import nx
 from pprint import pprint as pp
 from sounds.models import RemixGroup
-import simplejson as json
+import json
 
 # TODO: 1) test me!!! if sound not found in more than 1 groups we should be OK
 #       2) save to RemixGroup model
@@ -20,23 +20,23 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Calculate eccentricity so the arcs don't get clipped
-        # N.B. this is not the canonical way to calculate eccentricity but protovis's way  
+        # N.B. this is not the canonical way to calculate eccentricity but protovis's way
         def __calculateEccentricity(sounds_length):
             eccentricity = 0
             if sounds_length > 3:
                 a = (sounds_length-2) * 80
                 b = 200
                 eccentricity = (1 - b/a) * (1 - b/a)
-                
+
             return str(eccentricity)
-        
+
         # 1) Get all the sounds that have remixes
         cursor = connection.cursor()
         cursor.execute("""
-                        SELECT 
+                        SELECT
                             src.from_sound_id AS from,
                             src.to_sound_id AS to,
-                            snd.created AS created  
+                            snd.created AS created
                         FROM
                             sounds_sound_sources src,
                             sounds_sound snd
@@ -45,28 +45,28 @@ class Command(BaseCommand):
                         ORDER BY
                             snd.created ASC
                         """)
-        
+
         # 2) Create directed graph
         dg = nx.DiGraph()
         for row in cursor:
             dg.add_edge(row[0], row[1])
-        
+
         # 3) Add date to nodes for sorting (FIXME: how can we avoid this query???)
         for node in dg.nodes():
             cursor.execute("SELECT snd.created, snd.original_filename, au.username " \
                            "FROM sounds_sound snd, auth_user au WHERE au.id=snd.user_id AND snd.id = %s", [node])
             temp = cursor.fetchone()
-            dg.add_node(node, {'date':temp[0], 
+            dg.add_node(node, {'date':temp[0],
                                'nodeName': temp[1],
                                'username': temp[2]})
-            
+
         # print dg.nodes(data=True)
         # 4) Find weakly connected components (single direction)
         subgraphs = nx.weakly_connected_component_subgraphs(dg)
         node_list = []
-        # 5) delete all remixgroup objects to recalculate 
+        # 5) delete all remixgroup objects to recalculate
         RemixGroup.objects.all().delete()
-        # 6) Loop through all connected graphs in the dataset 
+        # 6) Loop through all connected graphs in the dataset
         #    and create the groups
         for sg_idx,sg in enumerate(subgraphs):
             remixgroup = RemixGroup()
@@ -74,26 +74,26 @@ class Command(BaseCommand):
             # add to list the subgraphs(connected components) with the extra data
             node_list = sg.nodes(data=True)
             # pp(node_list)
-            
+
             # sort by date (holds all subgraph nodes sorted by date)
             # we need this since we go forward in time (source older than remix)
             node_list.sort(key = lambda x: x[1]['date']) # I think ['date'] is not necessary
             # print ' ========== SORTED NODE_LIST ========= '
             # pp(node_list)
-            
+
             # dict with key=sound_id, value=index, nodeName=original_filname
             # in the previous sorted by date list
             # FIXME: no need for all this data, can be simple dict, key=value
             container = dict((val[0],{'index': idx, 'nodeName': val[1]['nodeName']}) for (idx,val) in enumerate(node_list))
             # print ' ========== CONTAINER ========= '
             # pp(container)
-            
+
             links = []
             remixgroup.save()   # need to save to have primary key before ManyToMany
             # FIXME: no idea why nx.weakly_connected_components(sg) return list in list...
             remixgroup.sounds = set(nx.weakly_connected_components(sg)[0])
             # FIXME: seems like double work here, maybe convert container to list and sort?
-            nodes = [{'id': val[0], 
+            nodes = [{'id': val[0],
                       'username': val[1]['username'],
                       'nodeName': val[1]['nodeName'],
                       'group': 1} for (idx,val) in enumerate(node_list)]
@@ -102,8 +102,8 @@ class Command(BaseCommand):
                 if len(line.split()) > 1:
                     for i,l in enumerate(line.strip().split(" ")):
                         # index 0 is the source, which we already know
-                        if i > 0:                
-                            link = {'target': container[int(line.split(" ")[0])]['index'], 
+                        if i > 0:
+                            link = {'target': container[int(line.split(" ")[0])]['index'],
                                     'source': container[int(l)]['index']}
                             links.append(link)
                             #print link
@@ -111,12 +111,12 @@ class Command(BaseCommand):
                                        "\"eccentricity\":" + __calculateEccentricity(len(node_list)) + ","\
                                        "\"length\":" + str(len(node_list)) + "," \
                                        "\"nodes\": " + json.dumps(nodes) + "," \
-                                       "\"links\": " + json.dumps(links) + "}" 
+                                       "\"links\": " + json.dumps(links) + "}"
             remixgroup.save()
-            
+
             """
             print ' ========== NODES ========='
             pp(nodes)
             print ' ========== LINKS ========='
             pp(links)
-            """    
+            """

812289a5d8c8e9301d6361032b66c8c32dd36f37
Vincent Akkermans <vincent.akkermans@gmail.com>
2011-06-08 16:25:20 +0200

    remixes link now points to the remix group page

--- sounds/models.py
+++ sounds/models.py
@@ -423,8 +423,11 @@ class Download(models.Model):
 
     class Meta:
         ordering = ("-created",)
-        
+
 
 class RemixGroup(models.Model):
     protovis_data = models.TextField(null=True, blank=True, default=None)
-    sounds = models.ManyToManyField(Sound, symmetrical=False, related_name='sounds', blank=True)
+    sounds = models.ManyToManyField(Sound,
+                                    symmetrical=False,
+                                    related_name='remix_groups',
+                                    blank=True)

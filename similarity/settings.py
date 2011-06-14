2522630e58fb108265b2dd2afc92e4b82d32b6b4
Vincent Akkermans <vincent.akkermans@gmail.com>
2011-06-14 18:10:09 +0200

    only lowlevel and music presets, default is lowlevel now

--- similarity/settings.py
+++ similarity/settings.py
@@ -2,7 +2,7 @@ import os, logging
 
 INDEX_DIR                   = '/home/fsweb/freesound-similarity/'
 PRESET_DIR                  = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'presets/')
-PRESETS                     = ['lowlevel', 'music', 'mfcc', 'stationary', 'metaverse', 'gerard']
+PRESETS                     = ['lowlevel', 'music']
 SIMILARITY_MINIMUM_POINTS   = 2000
 REQREP_ADDRESS              = "tcp://10.55.0.104:9070" #cuernavaca.s.upf.edu
 NUM_THREADS                 = 5

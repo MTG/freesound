ee467b49131df1a5cbaa0253c253205909479f1e
Vincent Akkermans <vincent.akkermans@gmail.com>
2011-06-14 12:38:33 +0200

    adding mfcc similarity preset

--- similarity/settings.py
+++ similarity/settings.py
@@ -2,7 +2,7 @@ import os, logging
 
 INDEX_DIR                   = '/home/fsweb/freesound-similarity/'
 PRESET_DIR                  = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'presets/')
-PRESETS                     = ['lowlevel', 'music'] # ['lowlevel', 'music', 'rhythm', 'timbre']
+PRESETS                     = ['lowlevel', 'music', 'mfcc'] # ['lowlevel', 'music', 'rhythm', 'timbre']
 SIMILARITY_MINIMUM_POINTS   = 2000
 REQREP_ADDRESS              = "tcp://10.55.0.104:9070" #cuernavaca.s.upf.edu
 NUM_THREADS                 = 5

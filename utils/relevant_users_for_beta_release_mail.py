778a16cdc01479522e74ebbadf5f5e6b52bda4f9
Vincent Akkermans <vincent.akkermans@gmail.com>
2011-06-27 12:07:59 +0200

    update relevant_users script to use django settings

--- utils/relevant_users_for_beta_release_mail.py
+++ utils/relevant_users_for_beta_release_mail.py
@@ -1,31 +1,38 @@
-import psycopg2
-import time
+#!/usr/bin/env python
+import psycopg2, time, json, os, sys
 from datetime import date
-import json
+
+# load the freesound settings so the script works in every deployment
+sys.path.append('/'.join(os.path.dirname(os.path.abspath(__file__)).split('/')[:-1]))
+import settings
+from django.core.management import setup_environ
+setup_environ(settings)
 
 def datetimeToSqlDate(date) :
     return str(date.year) + "-" + str(date.month) + "-" + str(date.day)
 
+
 def run():
-    
+
     tic = time.clock()
-        
+
     # Connect to database
-    DB_NAME = ""
-    DB_USER = ""
-    
+    DB_NAME = settings.DATABASE_NAME
+    DB_USER = settings.DATABASE_USER
+    DB_PASS = settings.DATABASE_PASSWORD
+
     print "Connecting to " + DB_NAME + " database..." ,
-    conn = psycopg2.connect("dbname=" + DB_NAME + " user=" + DB_USER)
+    conn = psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASS)
     cur = conn.cursor()
     print "done!\n\n"
-    
+
     # PARAMETERS
     date_threshold = date(2010, 6, 20) # Date of last visit must be greater thn date threshold
     uploaded_sounds_threshold = 100 # Minimum number of uploaded sounds
-    
+
     cur.execute("""SELECT user_id, COUNT(DISTINCT id) FROM sounds_sound GROUP BY user_id ORDER BY COUNT(DISTINCT id) DESC""")
     users = cur.fetchall()
-    
+
     data = []
     count = 0
     for user in users:
@@ -37,22 +44,22 @@ def run():
                 data.append({'id': user_data[0][0], 'name': user_data[0][1], 'email': user_data[0][2], 'uploaded_sounds': user[1]})
                 #print str(user_data[0][0]) + " " + user_data[0][1] + ": " + user_data[0][2] + " (" +  str(user[1]) + " sounds)"
                 count += 1
-     
+
     with open('beta_release_mails.json', mode='w') as f:
         json.dump(data, f, indent = 4)
-        
-    
+
+
     print data
     print "\nTotal number of users: " + str(count)
-    
-    
-    
+
+
+
     print "\n"
     print "Closing connection to " + DB_NAME + " database..." ,
     cur.close()
     conn.close()
     print "done!\n\n"
-    
+
     toc = time.clock()
     print '(performed in ' + str(toc-tic) + ' seconds)\n\n'
 

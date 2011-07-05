774fedd6e6e519f5901b739baea5a3ee93d0154f
groma <gerard.roma@upf.edu>
2011-07-05 18:36:55 +0200

    fix for wrong url match in random sound, extra parens in merged facebook link

--- sounds/views.py
+++ sounds/views.py
@@ -81,7 +81,7 @@ def random(request):
     if sound_id is None:
         raise Http404
     sound_obj = Sound.objects.get(pk=sound_id)
-    return HttpResponseRedirect(reverse("sound",sound_id,kwargs={'random_browsing':True}))
+    return HttpResponseRedirect(reverse("sound",args=[sound_obj.user.username,sound_id])+"?random_browsing=true")
 
 
 def packs(request):
@@ -145,9 +145,8 @@ def sound(request, username, sound_id):
         form = CommentForm(request)
 
     qs = Comment.objects.select_related("user").filter(content_type=content_type, object_id=sound_id)
-
     display_random_link = request.GET.get('random_browsing')
-    facebook_like_link = urllib.quote_plus('http://%s%s' % (Site.objects.get_current().domain, reverse('sound', args=[sound.user.username, sound.id]))
+    facebook_like_link = urllib.quote_plus('http://%s%s' % (Site.objects.get_current().domain, reverse('sound', args=[sound.user.username, sound.id])))
     return render_to_response('sounds/sound.html', combine_dicts(locals(), paginate(request, qs, settings.SOUND_COMMENTS_PER_PAGE)), context_instance=RequestContext(request))
 
 # N.B. login is required but adapted to not return the user to the download link.

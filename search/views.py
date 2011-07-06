09be9191e45b24b6b566f6636abc5cbe93e41af8
Frederic Font <frederic.font@upf.edu>
2011-07-06 18:59:04 +0200

    Re #254 Implemented advanced search functionality. It is a first version that allows selecting in wich fields to search (pack names, filenames, tags, sound ids...), specifying min and max duration, selecting the license to search and also to if the sounds are geotagged. it NEEDS to be refactored (specially the design), but the functionality is there

--- search/views.py
+++ search/views.py
@@ -21,15 +21,44 @@ def search_prepare_sort(sort, options):
         sort = ["num_downloads desc"]
     return sort
 
-def search_prepare_query(search_query, filter_query, sort, current_page, sounds_per_page):
+DEFAULT_SEARCH_WEIGHTS = {
+                        'id' : 4,
+                        'tag' : 3,
+                        'description' : 3,
+                        'username' : 2,
+                        'pack_tokenized' : 2,
+                        'original_filename' : 2
+                        }
+
+def search_prepare_query(search_query, 
+                         filter_query, 
+                         sort, 
+                         current_page, 
+                         sounds_per_page, 
+                         id_weight = DEFAULT_SEARCH_WEIGHTS['id'],
+                         tag_weight = DEFAULT_SEARCH_WEIGHTS['tag'],
+                         description_weight = DEFAULT_SEARCH_WEIGHTS['description'],
+                         username_weight = DEFAULT_SEARCH_WEIGHTS['username'],
+                         pack_tokenized_weight = DEFAULT_SEARCH_WEIGHTS['pack_tokenized'],
+                         original_filename_weight = DEFAULT_SEARCH_WEIGHTS['original_filename']):
     query = SolrQuery()
+    
+    field_weights = []
+    if id_weight != 0 :
+        field_weights.append(("id", id_weight))
+    if tag_weight != 0 :
+        field_weights.append(("tag", tag_weight))
+    if description_weight != 0 :
+        field_weights.append(("description", description_weight))
+    if username_weight != 0 :
+        field_weights.append(("username", username_weight))
+    if pack_tokenized_weight != 0 :
+        field_weights.append(("pack_tokenized", pack_tokenized_weight))
+    if original_filename_weight != 0 :
+        field_weights.append(("original_filename", original_filename_weight))    
+    
     query.set_dismax_query(search_query,
-                           query_fields=[("id", 4),
-                                         ("tag",3),
-                                         ("description",3),
-                                         ("username",2),
-                                         ("pack_tokenized",2),
-                                         ("original_filename",2)])
+                           query_fields=field_weights)
     query.set_query_options(start=(current_page - 1) * sounds_per_page, rows=sounds_per_page, field_list=["id"], filter_query=filter_query, sort=sort)
     query.add_facet_fields("samplerate", "pack", "username", "tag", "bitrate", "bitdepth", "type", "channels")
     query.set_facet_options_default(limit=5, sort=True, mincount=1, count_missing=False)
@@ -44,6 +73,51 @@ def search(request):
     current_page = int(request.GET.get("page", 1))
     sort = request.GET.get("s", forms.SEARCH_DEFAULT_SORT)
     sort_options = forms.SEARCH_SORT_OPTIONS_WEB
+    
+    # Set default values    
+    id_weight = DEFAULT_SEARCH_WEIGHTS['id']
+    tag_weight = DEFAULT_SEARCH_WEIGHTS['tag']
+    description_weight = DEFAULT_SEARCH_WEIGHTS['description']
+    username_weight = DEFAULT_SEARCH_WEIGHTS['username']
+    pack_tokenized_weight = DEFAULT_SEARCH_WEIGHTS['pack_tokenized']
+    original_filename_weight = DEFAULT_SEARCH_WEIGHTS['original_filename']
+
+    # Parse advanced search options
+    advanced = request.GET.get("advanced", "")
+
+    # if advanced search
+    if advanced == "1" :
+        a_tag = request.GET.get("a_tag", "")
+        a_filename = request.GET.get("a_filename", "")
+        a_description = request.GET.get("a_description", "")
+        a_packname = request.GET.get("a_packname", "")
+        a_soundid = request.GET.get("a_soundid", "")
+        a_username = request.GET.get("a_username", "")
+        
+        # If none is selected use all (so other filter can be appleid)
+        if a_tag or a_filename or a_description or a_packname or a_soundid or a_username != "" :
+        
+            # Initialize all weights to 0
+            id_weight = 0
+            tag_weight = 0
+            description_weight = 0
+            username_weight = 0
+            pack_tokenized_weight = 0
+            original_filename_weight = 0
+            
+            # Set the weights of selected checkboxes
+            if a_soundid != "" :
+                id_weight = DEFAULT_SEARCH_WEIGHTS['id']
+            if a_tag != "" :
+                tag_weight = DEFAULT_SEARCH_WEIGHTS['tag']
+            if a_description != "" :
+                description_weight = DEFAULT_SEARCH_WEIGHTS['description']
+            if a_username != "" :
+                username_weight = DEFAULT_SEARCH_WEIGHTS['username']
+            if a_packname != "" :
+                pack_tokenized_weight = DEFAULT_SEARCH_WEIGHTS['pack_tokenized']
+            if a_filename != "" :
+                original_filename_weight = DEFAULT_SEARCH_WEIGHTS['original_filename']
 
     # Allow to return ALL sounds when search has no q parameter
     #if search_query.strip() != "": 
@@ -51,7 +125,18 @@ def search(request):
 
     solr = Solr(settings.SOLR_URL)
 
-    query = search_prepare_query(search_query, filter_query, sort, current_page, settings.SOUNDS_PER_PAGE)
+    query = search_prepare_query(search_query, 
+                                 filter_query, 
+                                 sort, 
+                                 current_page, 
+                                 settings.SOUNDS_PER_PAGE,
+                                 id_weight,
+                                 tag_weight,
+                                 description_weight,
+                                 username_weight,
+                                 pack_tokenized_weight,
+                                 original_filename_weight
+                                 )
 
     try:
         results = SolrResponseInterpreter(solr.select(unicode(query)))
@@ -74,7 +159,6 @@ def search(request):
         return render_to_response('search/search.html', locals(), context_instance=RequestContext(request))
     else:
         return render_to_response('search/search_ajax.html', locals(), context_instance = RequestContext(request))
-    
     
 def get_pack_tags(pack_obj):
     query = SolrQuery()

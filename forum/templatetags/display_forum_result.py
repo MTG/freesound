2c2698f93be2ed7ad19ef3bf5be7095abe7579d8
stelios <stetogias@gmail.com>
2011-07-12 18:07:44 +0200

    highlighted results were not matched to 'normal' results - see comment in code

--- forum/templatetags/display_forum_result.py
+++ forum/templatetags/display_forum_result.py
@@ -11,17 +11,21 @@ register = template.Library()
 @register.inclusion_tag('forum/display_forum_result.html', takes_context=True)
 def display_forum_result(context, post, highlight):
     results = []
-    for thd, hl in zip(post, highlight):
-        print thd
-        print highlight[hl]
-        post = highlight[hl]['post_body']   
+    for thd in post:
+        # highlight is dict, so not ordered. below we much results to highlighted results
+        # TODO: find an efficient way to do this...
+        if str(thd['doclist']['docs'][0]["id"]) in highlight:
+            post = highlight[str(thd['doclist']['docs'][0]["id"])]['post_body'][0]
+        else:
+            post = thd['doclist']['docs'][0]["post_body"]
+
         results.append({
                         'thread_id': thd['doclist']['docs'][0]["thread_id"],
                         'thread_title': thd['doclist']['docs'][0]['thread_title'],
                         'forum_name': thd['doclist']['docs'][0]['forum_name'],
                         'forum_name_slug': thd['doclist']['docs'][0]['forum_name_slug'],
                         'post_id': thd['doclist']['docs'][0]["id"],
-                        'post': post[0],
+                        'post': post,
                         'thread_info': ' - '.join(['User: ' + thd['doclist']['docs'][0]['thread_author'], 
                                                    'Posts: ' + str(thd['doclist']['docs'][0]['num_posts']), 
                                                    'Date: ' + str(thd['doclist']['docs'][0]['thread_created'])]),

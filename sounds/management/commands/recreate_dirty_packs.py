e1e1e8a0be5bee5b9eaa0f805a63fb43b120e793
groma <gerard.roma@upf.edu>
2011-07-19 15:51:26 +0200

    fixes #289, cronjobs failing on tabasco

--- sounds/management/commands/recreate_dirty_packs.py
+++ sounds/management/commands/recreate_dirty_packs.py
@@ -4,7 +4,6 @@ from sounds.models import Pack
 class Command(BaseCommand):
     args = ''
     help = "Find packs that need refreshing and create them"
-
-    def execute(self):
+    def handle(self, *args, **options):
         for pack in Pack.objects.filter(is_dirty=True):
             pack.create_zip()

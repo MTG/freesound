0a408605451308c4c8b8e8bb48769eaf330704d4
stelios <stetogias@gmail.com>
2011-07-21 12:13:13 +0200

    now using highlighted results in all group results - fixed blockquote bug - added css classes

--- forum/templatetags/display_forum_result.py
+++ forum/templatetags/display_forum_result.py
@@ -15,39 +15,35 @@ def display_forum_result(context, post, highlight):
     for thd in post:
         # highlight is dict, so not ordered. below we much results to highlighted results
         # TODO: find an efficient way to do this... maybe try except keyerror will be faster
-        post = []
-        first_doc = thd['doclist']['docs'][0]
-        # highlighted result
-        if str(thd['doclist']['docs'][0]['id']) in highlight:
-            post.append({'post_id': first_doc['id'],
-                         'post_body': highlight[str(first_doc['id'])]['post_body'][0],
-                         'post_info': ' - '.join(['Post by: ' + first_doc['post_author'],
-                                                  'Date: ' + str(first_doc['post_created'])])})
-        else:
-            post.append({'post_id': thd['doclist']['docs'][0]['id'], 
-                         'post_body': thd['doclist']['docs'][0]['post_body'],
-                         'post_info': ' - '.join(['Post by: ' + first_doc['post_author'],
-                                                  'Date: ' + str(first_doc['post_created'])])})
-        
-        # up to 3 posts per group can be returned    
-        if thd['doclist']['numFound'] > 1:
-            for idx,val in enumerate(thd['doclist']['docs']):
-                if idx > 0:
-                    post.append({'post_id': val['id'], 
-                                 'post_body': val['post_body'],
-                                 'post_info': ' - '.join(['Post by: ' + val['post_author'],
-                                                          'Date: ' + str(val['post_created'])])})
+        posts = []
+        first_doc = thd['doclist']['docs']
+        for p in first_doc:
+            # highlighted result
+            if str(p['id']) in highlight:
+                print '================ HIGHILIGHT ===================='
+                print str(p['id'])
+                posts.append({'post_id': p['id'],
+                             'post_body': highlight[str(p['id'])]['post_body'][0],
+                             'post_info': ' - '.join(['Post by: ' + p['post_author'],
+                                                      'Date: ' + str(p['post_created'])])})
+            else:
+                print '================ NORMAL ===================='
+                print str(p['id'])
+                posts.append({'post_id': p['id'], 
+                             'post_body': p['post_body'],
+                             'post_info': ' - '.join(['Post by: ' + p['post_author'],
+                                                      'Date: ' + str(p['post_created'])])})
 
         results.append({
-                        'thread_id': first_doc['thread_id'],
-                        'thread_title': first_doc['thread_title'],
-                        'forum_name': first_doc['forum_name'],
-                        'forum_name_slug': first_doc['forum_name_slug'],
-                        'post_id': first_doc['id'],
-                        'post': post,
-                        'thread_info': ' - '.join(['Thread by: ' + first_doc['thread_author'], 
-                                                   'Posts: ' + str(first_doc['num_posts']), 
-                                                   'Date: ' + str(first_doc['thread_created'])]),
+                        'thread_id': first_doc[0]['thread_id'],
+                        'thread_title': first_doc[0]['thread_title'],
+                        'forum_name': first_doc[0]['forum_name'],
+                        'forum_name_slug': first_doc[0]['forum_name_slug'],
+                        'post_id': first_doc[0]['id'],
+                        'posts': posts,
+                        'thread_info': ' - '.join(['Thread by: ' + first_doc[0]['thread_author'], 
+                                                   'Posts: ' + str(first_doc[0]['num_posts']), 
+                                                   'Date: ' + str(first_doc[0]['thread_created'])]),
                         }) 
     
     return { 'results': results }

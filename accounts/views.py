c90dfd04323c6196e79aab9baa10bb14aa2aed9b
groma <gerard.roma@upf.edu>
2011-08-29 18:31:05 +0200

    test #362

--- accounts/views.py
+++ accounts/views.py
@@ -494,14 +494,14 @@ def attribution(request):
 
 
 def downloaded_sounds(request, username):
-    user = User.objects.get(username=username)
+    user=get_object_or_404(User, username=username)
     # Retrieve all sounds downloaded by the user (for the moment we are not diplaying downloaded packs...)
     qs = Download.objects.filter(user=user.id, sound__isnull=False)
     num_results = len(qs)
     return render_to_response('accounts/downloaded_sounds.html', combine_dicts(paginate(request, qs, settings.SOUNDS_PER_PAGE), locals()), context_instance=RequestContext(request))
 
 def downloaded_packs(request, username):
-    user = User.objects.get(username=username)
+    user=get_object_or_404(User, username=username)
     # Retrieve all sounds downloaded by the user (for the moment we are not diplaying downloaded packs...)
     qs = Download.objects.filter(user=user.id, pack__isnull=False)
     num_results = len(qs)

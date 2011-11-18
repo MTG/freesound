1f06140fd7646e64b8ae3311d9fecc5e9ca7cdc1
Frederic Font <frederic.font@upf.edu>
2011-11-18 17:03:50 +0100

    Re #437 Automatically setting preferFlash = False for embeds

--- media/html_player/player.js
+++ media/html_player/player.js
@@ -3,7 +3,11 @@ soundManager.url = '/media/html_player/swf/';
 //flash 9 doesn't have weird artifacts at the beginning of sounds.
 soundManager.flashVersion = 9;
 soundManager.debugMode = false;
-soundManager.preferFlash = false;
+soundManager.preferFlash = true;
+// If the player is used in an embed, it uses HTML5 so it is lighter (althogh playbar position is not updated as fast as with flash)
+if (typeof isEmbed!="undefined"){
+    soundManager.preferFlash = false;
+}
 //if you have a stricter test than 'maybe' SM will switch back to flash.
 //soundManager.html5Test = /^maybe$/i
 
@@ -378,6 +382,6 @@ $(function() {
 
     soundManager.onready(function()
     {
-        makePlayer('.player');
+        makePlayer('.player');	
     });
 });

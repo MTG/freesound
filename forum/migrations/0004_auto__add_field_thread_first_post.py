d13007592b7606ae8e9576e0e7d5941e0ff1f889
Frederic Font <frederic.font@upf.edu>
2012-02-02 19:32:54 +0100

    Re #479 Implemented hiding non moderated posts and threads whose first post is non moderated in the forums pages (as well as in the front page). Setting first posts with http:// or https:// to moderation_state="NM" (NEEDS MODERATION). Added first_post field to the Thread model (plus south migration). Created a management command (set_first_post_in_threads) to fill first_post field of previously existing Threads (for newly created threads this is done in the new_thread view)

---
+++ forum/migrations/0004_auto__add_field_thread_first_post.py
@@ -0,0 +1,100 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Thread.first_post'
+        db.add_column('forum_thread', 'first_post', self.gf('django.db.models.fields.related.OneToOneField')(related_name='first_in_thread', null=True, default=None, to=orm['forum.Post'], blank=True, unique=True), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Thread.first_post'
+        db.delete_column('forum_thread', 'first_post_id')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'forum.forum': {
+            'Meta': {'ordering': "['order']", 'object_name': 'Forum'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_post': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'latest_in_forum'", 'null': 'True', 'default': 'None', 'to': "orm['forum.Post']", 'blank': 'True', 'unique': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'name_slug': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
+            'num_posts': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'num_threads': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {})
+        },
+        'forum.post': {
+            'Meta': {'ordering': "('created',)", 'object_name': 'Post'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'body': ('django.db.models.fields.TextField', [], {}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'moderation_state': ('django.db.models.fields.CharField', [], {'default': "'OK'", 'max_length': '2', 'db_index': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'thread': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['forum.Thread']"})
+        },
+        'forum.subscription': {
+            'Meta': {'unique_together': "(('subscriber', 'thread'),)", 'object_name': 'Subscription'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'db_index': 'True'}),
+            'subscriber': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'thread': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['forum.Thread']"})
+        },
+        'forum.thread': {
+            'Meta': {'ordering': "('-status', '-last_post__created')", 'object_name': 'Thread'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'first_post': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'first_in_thread'", 'null': 'True', 'default': 'None', 'to': "orm['forum.Post']", 'blank': 'True', 'unique': 'True'}),
+            'forum': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['forum.Forum']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_post': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'latest_in_thread'", 'null': 'True', 'default': 'None', 'to': "orm['forum.Post']", 'blank': 'True', 'unique': 'True'}),
+            'num_posts': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'status': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1', 'db_index': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        }
+    }
+
+    complete_apps = ['forum']

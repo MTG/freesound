5cb89dc5bbcc0473826c07072b01e1708bf6f2cb
groma <gerard.roma@upf.edu>
2012-05-09 13:22:46 +0200

    errors in previous commit

--- sounds/models.py
+++ sounds/models.py
@@ -362,18 +362,17 @@ def on_delete_sound(sender,instance, **kwargs):
             instance.geotag.delete()
     except:
         pass
+    if sound.pack:
+        sound.pack.create_license_file()
     
     delete_sound_from_solr(instance)
     delete_object_files(instance, web_logger)
     # N.B. be watchful of errors that might be thrown if the sound is not in the similarity index
     Similarity.delete(instance.id)
+
 post_delete.connect(on_delete_sound, sender=Sound)
 
-def recreate_pack(sender,instance,**kwargs):
-    if instance.moderation_state=="OK" and instance.pack:
-        instance.pack.process()
     
-post_save.connect(recreate_pack, sender=Sound)
 
 class Pack(SocialModel):
     user = models.ForeignKey(User)

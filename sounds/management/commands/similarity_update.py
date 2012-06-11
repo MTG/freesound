21e13fb6b34f850a9ac7e75ca7a39b374da354e6
Frederic Font <frederic.font@upf.edu>
2012-06-11 13:57:48 +0200

    Updated similarity update command

--- sounds/management/commands/similarity_update.py
+++ sounds/management/commands/similarity_update.py
@@ -1,12 +1,25 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 from sounds.models import Sound
 from similarity.client import Similarity
+from optparse import make_option
 
-class Command(NoArgsCommand):
-    help = "Take all sounds that haven't been added to the similarity service yet and add them."
+class Command(BaseCommand):
+    help = "Take all sounds that haven't been added to the similarity service yet and add them. Use option --force to force reindex ALL sounds"
+    option_list = BaseCommand.option_list + (
+    make_option('-f','--force',
+        dest='force',
+        action='store_true',
+        default=False,
+        help='Reindex all sounds regardless of their similarity state'),
+    )
 
-    def handle(self, **options):
-        to_be_added = Sound.objects.filter(analysis_state='OK', similarity_state='PE', moderation_state='OK')
+    def handle(self,  *args, **options):
+
+        if options['force']:
+            to_be_added = Sound.objects.filter(analysis_state='OK', moderation_state='OK')
+        else:
+            to_be_added = Sound.objects.filter(analysis_state='OK', similarity_state='PE', moderation_state='OK')
+
         for sound in to_be_added:
             try:
                 Similarity.add(sound.id, sound.locations('analysis.statistics.path'))
@@ -17,3 +30,4 @@ class Command(NoArgsCommand):
                 #sound.similarity_state = 'FA'
                 sound.set_similarity_state('FA')
             #sound.save()
+

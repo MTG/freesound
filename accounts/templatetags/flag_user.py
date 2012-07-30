7541a9621d5f17d031a504ccd064506160bffd92
Frederic Font <frederic.font@upf.edu>
2012-07-30 13:09:54 +0200

    Fixed bug in spam reporting for non registered users

--- accounts/templatetags/flag_user.py
+++ accounts/templatetags/flag_user.py
@@ -8,10 +8,18 @@ register = template.Library()
 @register.inclusion_tag("accounts/flag_user.html", takes_context=True)
 def flag_user(context, flag_type, username, content_id, text = None, user_sounds = None):
 
-    flagged = UserFlag.objects.filter(user__username = username, reporting_user = context['request'].user, object_id = content_id).values('reporting_user').distinct()
+    no_show = False
     link_text = "Report spam"
-    if text:
-        link_text = text
 
+    print context['request'].user
+    print context['request'].user.is_authenticated()
 
-    return {'user_sounds':user_sounds,'done_text':"Marked as spam", 'flagged':len(flagged),'flag_type':flag_type,'username':username, 'content_obj_id':content_id, 'media_url': context['media_url'], 'link_text':link_text}
+    if not context['request'].user.is_authenticated():
+        no_show = True
+        flagged = []
+    else:
+        flagged = UserFlag.objects.filter(user__username = username, reporting_user = context['request'].user, object_id = content_id).values('reporting_user').distinct()
+        if text:
+            link_text = text
+
+    return {'user_sounds':user_sounds,'done_text':"Marked as spam", 'flagged':len(flagged),'flag_type':flag_type,'username':username, 'content_obj_id':content_id, 'media_url': context['media_url'], 'link_text':link_text, 'no_show':no_show}

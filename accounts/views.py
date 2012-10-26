8b1a6da611a9e6bf1830c60cc9701ba48fda0bb2
Frederic Font <frederic.font@upf.edu>
2012-10-26 13:26:10 +0200

    Added ty/except to compute crc (temporary)

--- accounts/views.py
+++ accounts/views.py
@@ -501,7 +501,11 @@ def describe_sounds(request):
                                      'File <a href="%s">%s</a> has been described and is awaiting moderation.' % \
                                      (sound.get_absolute_url(), forms[i]['sound'].name))
             # compute crc
-            sound.compute_crc()
+            # TEMPORARY
+            try:
+                sound.compute_crc()
+            except:
+                pass
         # remove the files we described from the session and redirect to this page
         request.session['describe_sounds'] = request.session['describe_sounds'][len(sounds_to_describe):]
         # Process the sound

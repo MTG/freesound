fbc81631a9f946dccde91569ef302c22b75d6c77
Frederic Font <frederic.font@upf.edu>
2012-10-30 13:39:37 +0100

    Added sound id to worker processing logs

--- utils/audioprocessing/freesound_audio_processing.py
+++ utils/audioprocessing/freesound_audio_processing.py
@@ -10,7 +10,7 @@ logger = logging.getLogger("processing")
 def process(sound):
 
     def write_log(message):
-        logger.info("[%d] %s" % (os.getpid(),message))
+        logger.info("[%d] %i: %s" % (os.getpid(),sound.id,message))
         sys.stdout.write(str(message)+'\n')
         sys.stdout.flush()
 

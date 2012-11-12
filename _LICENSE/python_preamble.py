37f25cd5100131118024738f002fed40d5f7c6b0
Frederic Font <frederic.font@upf.edu>
2012-11-12 11:51:45 +0100

    Updated license preambles to match new MTG guidelines

--- _LICENSE/python_preamble.py
+++ _LICENSE/python_preamble.py
@@ -1,13 +1,12 @@
-# [one line description of file/program]
-# Copyright (C) 2008 MUSIC TECHNOLOGY GROUP (MTG)
-#                    UNIVERSITAT POMPEU FABRA
 #
-# This program is free software: you can redistribute it and/or modify
+# Freesound is (C) MUSIC TECHNOLOGY GROUP, UNIVERSITAT POMPEU FABRA 2008-2012
+#
+# Freesound is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# Freesound is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.

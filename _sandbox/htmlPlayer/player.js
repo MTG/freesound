7b725b74a45ecd24bf9fbcc71d381ec14dd4acde
Frederic Font <frederic.font@upf.edu>
2012-11-12 13:56:33 +0100

    Added preamble to js files

--- _sandbox/htmlPlayer/player.js
+++ _sandbox/htmlPlayer/player.js
@@ -1,24 +1,23 @@
 /*
-* player.js -- sound playback using soundManager and only HTML elements
-* Copyright (C) 2008 MUSIC TECHNOLOGY GROUP (MTG)
-*                    UNIVERSITAT POMPEU FABRA
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-* Authors:
-*   Bram de Jong <bram.dejong at domain.com where domain in gmail>
-*/
+ *
+ * Freesound is (c) MUSIC TECHNOLOGY GROUP, UNIVERSITAT POMPEU FABRA
+ *
+ * Freesound is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Freesound is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Bram de Jong <bram.dejong at domain.com where domain in gmail>
+ */
 
 soundManager.debugMode = false;
   

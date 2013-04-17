b8791fbbcee8e5b98ccaeed571115296a47ee3ed
darad <dabiri.dara@gmail.com>
2013-04-17 13:50:53 +0200

    using .get(,"") instead of default-if-statement in click_log function. Merging the three if statements in to one.

--- sounds/views.py
+++ sounds/views.py
@@ -682,19 +682,14 @@ def pack_downloaders(request, username, pack_id):
 
 def click_log(request,click_type=None, sound_id="", pack_id="" ):
     
-    searchtime_session_key = ""
+    searchtime_session_key = request.session.get("searchtime_session_key", "")
     authenticated_session_key = ""
-    if "searchtime_session_key" in request.session:
-        searchtime_session_key = request.session["searchtime_session_key"]
     if request.user.is_authenticated():
         authenticated_session_key = request.session.session_key
+    if click_type in ['soundpreview', 'sounddownload']:
+        entity_id = sound_id
+    else:
+        entity_id = pack_id
 
-    if click_type == 'soundpreview': 
-        logger_click.info("CLICK.preview : %s : %s : %s"
-                          % (authenticated_session_key, searchtime_session_key, sound_id))
-    elif click_type == 'sounddownload':
-        logger_click.info("CLICK.sounddownload : %s : %s : %s"
-                          % (authenticated_session_key, searchtime_session_key, sound_id))
-    elif click_type == 'packdownload':
-        logger_click.info("CLICK.packdownload : %s : %s : %s"
-                          % (authenticated_session_key, searchtime_session_key, pack_id))
+    logger_click.info("%s : %s : %s : %s"
+                          % (click_type, authenticated_session_key, searchtime_session_key, entity_id))

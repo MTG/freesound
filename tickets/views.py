231082777dfbe967d8b3b4313efd2602e34cb659
Frederic Font <frederic.font@upf.edu>
2013-04-18 16:08:38 +0200

    #503, adding number of days in queue in the page. This should finally work now

--- tickets/views.py
+++ tickets/views.py
@@ -255,14 +255,15 @@ GROUP BY sender_id""" % TICKET_STATUS_NEW)
     for user in user_objects:
         # Pick the oldest non moderated ticket of each user and compute how many seconds it has been in the queue
         oldest_new_ticket = Ticket.objects.filter(sender__id=user.id,status=TICKET_STATUS_NEW,content__isnull=False).order_by("created")[0]
-        seconds_in_queue = (datetime.datetime.today()-oldest_new_ticket.created).seconds
-        users_aux.append({'user':user, 'seconds_in_queue':seconds_in_queue, 'new_sounds':user_ids_plus_new_count[user.id]})
+        seconds_in_queue = (datetime.datetime.now() - oldest_new_ticket.created).total_seconds()
+        days_in_queue = seconds_in_queue / (3600 * 24)
+        users_aux.append({'user':user, 'days_in_queue':days_in_queue, 'new_sounds':user_ids_plus_new_count[user.id]})
 
     # Sort users according to their oldest ticket (older = first)
-    users_aux.sort(key=lambda item:item['seconds_in_queue'], reverse=True)
+    users_aux.sort(key=lambda item:item['days_in_queue'], reverse=True)
     new_sounds_users = []
     for user in users_aux:
-        new_sounds_users.append((user['user'],user['new_sounds']))
+        new_sounds_users.append((user['user'],user['new_sounds'],int(user['days_in_queue'])))
 
     return new_sounds_users
 

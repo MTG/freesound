7e85ad2ea89c62aef443683e77636ca79ba0e0c9
Frederic Font <frederic.font@upf.edu>
2013-04-19 12:54:57 +0200

    #523 Added basic structure of client/server for tag recommendation with fake method.

---
+++ tagrecommendation/client/__init__.py
@@ -0,0 +1,47 @@
+#
+# Freesound is (c) MUSIC TECHNOLOGY GROUP, UNIVERSITAT POMPEU FABRA
+#
+# Freesound is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Freesound is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#     See AUTHORS file.
+#
+
+from tagrecommendation.tagrecommendation_settings import TAGRECOMMENDATION_ADDRESS, TAGRECOMMENDATION_PORT
+import json
+import urllib2
+
+_BASE_URL                     = 'http://%s:%i/tagrecommendation/' % (TAGRECOMMENDATION_ADDRESS, TAGRECOMMENDATION_PORT)
+_URL_RECOMMEND_TAGS           = 'recommend_tags/'
+
+
+def _get_url_as_json(url):
+    f = urllib2.urlopen(url.replace(" ","%20"))
+    resp = f.read()
+    return json.loads(resp)
+
+
+def _result_or_exception(result):
+    if not result['error']:
+        return result['result']
+    else:
+        raise Exception(result['result'])
+
+
+class TagRecommendation():
+
+    @classmethod
+    def recommend_tags(cls, input_tags):
+        url = _BASE_URL + _URL_RECOMMEND_TAGS + '?' + 'input_tags=' + ",".join(input_tags)
+        return _result_or_exception(_get_url_as_json(url))
---
+++ tagrecommendation/tagrecommendation_server.py
@@ -0,0 +1,91 @@
+#
+# Freesound is (c) MUSIC TECHNOLOGY GROUP, UNIVERSITAT POMPEU FABRA
+#
+# Freesound is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Freesound is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#     See AUTHORS file.
+#
+
+from twisted.web import server, resource
+from twisted.internet import reactor
+from tagrecommendation_settings import *
+import logging
+import graypy
+from logging.handlers import RotatingFileHandler
+import json
+
+
+def server_interface(resource):
+    return {
+        'recommend_tags':resource.recommend_tags, # input_tags (tags separated by commas)
+    }
+
+
+class TagRecommendationServer(resource.Resource):
+    def __init__(self):
+        resource.Resource.__init__(self)
+        self.methods = server_interface(self)
+        self.isLeaf = False
+
+    def error(self,message):
+        return json.dumps({'Error':message})
+
+    def getChild(self, name, request):
+        return self
+
+    def render_GET(self, request):
+        return self.methods[request.prepath[1]](**request.args)
+
+    def recommend_tags(self, input_tags):
+        try:
+            logger.debug('Getting recommendation for input tags %s' % input_tags)
+            input_tags = input_tags[0].split(",")
+            input_tags += ["new_test_tag"]
+            result = {'error': False, 'result': input_tags}
+
+        except Exception, e:
+            logger.debug('Errors occurred while recommending tags to %s' % input_tags)
+            result = {'error': True, 'result': str(e)}
+
+        return json.dumps(result)
+
+
+if __name__ == '__main__':
+    # Set up logging
+    logger = logging.getLogger('tagrecommendation')
+    logger.setLevel(logging.DEBUG)
+    handler = RotatingFileHandler(LOGFILE, maxBytes=2*1024*1024, backupCount=5)
+    handler.setLevel(logging.DEBUG)
+    std_handler = logging.StreamHandler()
+    std_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    std_handler.setFormatter(formatter)
+    logger.addHandler(std_handler)
+    handler_graypy = graypy.GELFHandler('10.55.0.48', 12201)
+    logger.addHandler(handler_graypy)
+
+    # Start service
+    logger.info('Configuring tag recommendation service...')
+    root = resource.Resource()
+    root.putChild("tagrecommendation", TagRecommendationServer())
+    site = server.Site(root)
+    reactor.listenTCP(LISTEN_PORT, site)
+    logger.info('Started tag recommendation service, listening to port ' + str(LISTEN_PORT) + "...")
+    reactor.run()
+    logger.info('Service stopped.')
+
+

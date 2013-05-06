5da7999152cd2053f41bd61c47e540faec643bd8
Frederic Font <frederic.font@upf.edu>
2013-05-06 14:51:51 +0200

    Fixed small bug in Freesound API when returning sounds in similarity handler

--- api/handlers.py
+++ api/handlers.py
@@ -610,9 +610,12 @@ class SoundSimilarityHandler(BaseHandler):
 
         sounds = []
         for similar_sound in similar_sounds :
-            sound = prepare_collection_sound(Sound.objects.select_related('user').get(id=similar_sound[0]), custom_fields = request.GET.get('fields', False))
-            sound['distance'] = similar_sound[1]
-            sounds.append( sound )
+            try:
+                sound = prepare_collection_sound(Sound.objects.select_related('user').get(id=similar_sound[0]), custom_fields = request.GET.get('fields', False))
+                sound['distance'] = similar_sound[1]
+                sounds.append(sound)
+            except Exception, e:
+                pass
 
         result = {'sounds': sounds, 'num_results': len(similar_sounds)}
         add_request_id(request,result)

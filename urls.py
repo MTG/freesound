2580ed8ebe16a6c15374980e0e2146b9e7515f6f
darad <dabiri.dara@gmail.com>
2013-05-24 13:16:14 +0200

    removing clickthrough functionality from urls.py that was mistakingly merged with master.

--- urls.py
+++ urls.py
@@ -32,7 +32,6 @@ import tags.views
 import forum.views
 import comments.views
 import bookmarks.views
-import clickthrough.views
 from django.views.generic.simple import redirect_to
 
 admin.autodiscover()
@@ -123,7 +122,6 @@ urlpatterns = patterns('',
     url(r'^tagsViewSingle', tags.views.old_tag_link_redirect, name="old-tag-page"),
     url(r'^forum/viewtopic', forum.views.old_topic_link_redirect, name="old-topic-page"),
     
-    url(r'^clickthrough/query_to_download_deltatime',clickthrough.views.query_to_download_deltatime,name="query-to-download-dealtatime"),
 
     # dead season redirect (THIS IS TEMPORAL)
     url(r'^deadseason/$', redirect_to, {'url': 'http://www.freesound.org/people/Slave2theLight/bookmarks/category/4730/'}),

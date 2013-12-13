b1e97a13b92563658944ffa9750b4d11e758e394
Frederic Font <frederic.font@upf.edu>
2013-12-13 18:01:24 +0100

    Improved similarity_update command management (uses faster yaml)

--- sounds/management/commands/similarity_update.py
+++ sounds/management/commands/similarity_update.py
@@ -24,6 +24,7 @@ from similarity.client import Similarity
 from optparse import make_option
 import yaml
 
+
 class Command(BaseCommand):
     help = "Take all sounds that haven't been added to the similarity service yet and add them. Use option --force to " \
            "force reindex ALL sounds. Pas a number argument to limit the number of sounds that will be reindexed " \
@@ -65,7 +66,7 @@ class Command(BaseCommand):
             # Check if sound analyzed using the desired extractor
             if freesound_extractor_version:
                 try:
-                    data = yaml.load(open(sound.locations('analysis.statistics.path')))
+                    data = yaml.load(open(sound.locations('analysis.statistics.path')), Loader=yaml.cyaml.CLoader)
                 except:
                     print 'Sound with id %i was not indexed (no yaml file found when checking for extractor version)' % sound.id
                     continue

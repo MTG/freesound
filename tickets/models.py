0360b0d2fab5b28c0bf38b8273729c89553420fb
Nuno Hespanhol <nuno.hespanhol@gmail.com>
2014-02-04 10:56:39 +0100

    Fixed #558
    Call to action for users on pending sound pages, when they should reply
    back on a sound ticket.

--- tickets/models.py
+++ tickets/models.py
@@ -78,7 +78,7 @@ class Ticket(models.Model):
         Get the last n comments that are not 'moderator only' from the self ticket
         """
         ticket_comments = self.messages.all().filter(moderator_only=False)
-        return list(ticket_comments)[-n:] # converting from Django QuerySet to Python list in order to use negative indexing
+        return list(ticket_comments)[-n:] # converting from Django QuerySet to python list in order to use negative indexing
 
     def send_notification_emails(self, notification_type, sender_moderator):
         ticket = self

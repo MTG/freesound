0ab1b02d2418b3005b2ca24eb4a41576494ff437
Frederic Font <frederic.font@upf.edu>
2014-02-28 17:26:39 +0100

    #488 Fixing bug in get_authentication_details_form_request

--- apiv2/utils.py
+++ apiv2/utils.py
@@ -522,7 +522,7 @@ def get_authentication_details_form_request(request):
         if auth_method_name == "OAuth2":
             user = request.user
             developer = request.auth.client.user
-            client_id = request.client_id
+            client_id = request.auth.client_id
         elif auth_method_name == "Token":
             user = None
             developer = request.auth.user

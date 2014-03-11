69d689e6dbcb3b9b093e188dc5cbc09a2d00b321
Alastair Porter <alastair.porter@upf.edu>
2014-03-11 17:54:12 +0100

    Rewrite geotags queries to use INNER JOIN
    Fixes #584. Because Django insisted on using an OUTER JOIN, we now
    do the queries manually. Only one fast query is needed for geo
    queries. Some additional changes to make sure every query only looks
    at sounds with processing and moderation states are OK

--- geotags/views.py
+++ geotags/views.py
@@ -27,28 +27,53 @@ from django.views.decorators.cache import cache_page
 from django.contrib.auth.models import User
 import json
 
+"""
+SQL queries in this file are done manually in order to force an INNER JOIN,
+since Django insists on doing an OUTER LEFT JOIN, which is too slow.
+The equivalent Django query is also provided for each raw query.
+"""
+
+geoquery = """SELECT sounds_sound.id,
+                     geotags_geotag.lat,
+                     geotags_geotag.lon
+                FROM sounds_sound
+                     %(join)s
+          INNER JOIN geotags_geotag
+                  ON (sounds_sound.geotag_id = geotags_geotag.id)
+               WHERE sounds_sound.processing_state = 'OK'
+                 AND sounds_sound.moderation_state = 'OK'
+                     %(where)s
+            ORDER BY sounds_sound.created DESC
+                     %(end)s"""
 
 def generate_json(sound_queryset):
-    sounds_data = []
+    # When using Django Sound queries:
+    #sounds_data = [[s.id, s.geotag.lat, s.geotag.lon] for s in sound_queryset]
 
-    for sound in sound_queryset:
-        sounds_data.append([sound.id, sound.geotag.lat, sound.geotag.lon])
+    sounds_data = [[s.id, s.lat, s.lon] for s in sound_queryset]
 
     return HttpResponse(json.dumps(sounds_data))
 
 @cache_page(60 * 15)
 def geotags_json(request, tag=None):
     if tag:
-        sounds = Sound.objects.select_related('geotag').filter(tags__tag__name=tag).exclude(geotag=None)
+        #sounds = Sound.objects.select_related('geotag').filter(tags__tag__name=tag).exclude(geotag=None)
+        join = """INNER JOIN "tags_taggeditem"
+                           ON ("sounds_sound"."id" = "tags_taggeditem"."object_id")
+                   INNER JOIN "tags_tag"
+                           ON ("tags_taggeditem"."tag_id" = "tags_tag"."id")"""
+        where = """AND "tags_tag"."name" = %s"""
+        q = geoquery % {"join": join, "where": where, "end": ""}
+        sounds = Sound.objects.raw(q, [tag])
     else:
-        sounds = Sound.objects.select_related('geotag').all().exclude(geotag=None)
+        #sounds = Sound.public.select_related('geotag').all().exclude(geotag=None)
+        q = geoquery % {"join": "", "where": "", "end": ""}
+        sounds = Sound.objects.raw(q)
 
     return generate_json(sounds)
 
 def geotags_box_json(request):    
-    
     box = request.GET.get("box","-180,-90,180,90")
-    print "geotags_box_json ", box
     try:
         min_lat, min_lon, max_lat, max_lon = box.split(",")  
         qs = Sound.objects.select_related("geotag").exclude(geotag=None).filter(moderation_state="OK", processing_state="OK")        
@@ -61,22 +86,36 @@ def geotags_box_json(request):
         elif min_lat > max_lat and min_lon > max_lon:
             sounds = qs.exclude(geotag__lat__range=(max_lat,min_lat)).exclude(geotag__lon__range=(max_lon,min_lon))        
         return generate_json(sounds)
-    except:
+    except ValueError:
         raise Http404
     
 @cache_page(60 * 15)
 def geotags_for_user_json(request, username):
-    sounds = Sound.objects.select_related('user', 'geotag').filter(user__username__iexact=username).exclude(geotag=None)
+    #sounds = Sound.public.select_related('geotag').filter(user__username__iexact=username).exclude(geotag=None)
+    join = """INNER JOIN "auth_user"
+                      ON ("sounds_sound"."user_id" = "auth_user"."id")"""
+    where = """AND UPPER("auth_user"."username"::text) = UPPER(%s)"""
+    q = geoquery % {"join": join, "where": where, "end": ""}
+    sounds = Sound.objects.raw(q, [username])
     return generate_json(sounds)
 
 #@cache_page(60 * 15)
 def geotags_for_user_latest_json(request, username):
-    sounds = Sound.public.filter(user__username__iexact=username).exclude(geotag=None)[0:10]
+    #sounds = Sound.public.filter(user__username__iexact=username).exclude(geotag=None)[0:10]
+    join = """INNER JOIN "auth_user"
+                      ON ("sounds_sound"."user_id" = "auth_user"."id")"""
+    where = """AND UPPER("auth_user"."username"::text) = UPPER(%s)"""
+    end = "LIMIT 10"
+    q = geoquery % {"join": join, "where": where, "end": end}
+    sounds = Sound.objects.raw(q, [username])
     return generate_json(sounds)
 
 #@cache_page(60 * 15)
 def geotags_for_pack_json(request, pack_id):
-    sounds = Sound.public.select_related('license', 'pack', 'geotag', 'user', 'user__profile').filter(pack__id=pack_id).exclude(geotag=None)
+    #sounds = Sound.public.select_related('geotag').filter(pack__id=pack_id).exclude(geotag=None)
+    where = "AND pack_id = %s"
+    q = geoquery % {"join": "", "where": where, "end": ""}
+    sounds = Sound.objects.raw(q, [pack_id])
     return generate_json(sounds)
 
 def geotags(request, tag=None):

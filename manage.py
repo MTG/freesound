3d7c23c38aa9fc557780abf20d082b60007a2bd9
Frederic Font <frederic.font@upf.edu>
2014-12-10 11:30:53 +0100

    Removing uneeded comment in manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,6 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "freesound.settings")
-    #sys.path.append('/Users/frederic/Developer/Freesound/freesound/freesound')
 
     from django.core.management import execute_from_command_line
 

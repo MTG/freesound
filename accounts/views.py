5e508ae8beffe6addaf602e8a89e99811a885bf8
Frederic Font <frederic.font@upf.edu>
2014-12-19 08:48:42 +0100

    Another small fix in following stuff

--- accounts/views.py
+++ accounts/views.py
@@ -808,7 +808,7 @@ def account(request, username):
     # true if the logged user is following the user of the current viewed profile page
     show_unfollow_button = False
     if request.user.is_authenticated():
-        show_unfollow_button = follow_utils.is_user_following_user(request.user, User.objects.get(username=username))
+        show_unfollow_button = follow_utils.is_user_following_user(request.user, user))
 
     home = False
     has_bookmarks = Bookmark.objects.filter(user=user).exists()

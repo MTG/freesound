0b900cf8ef71add102ecacde73ff3fb0cc928504
Frederic Font <frederic.font@gmail.com>
2015-05-07 12:39:49 +0200

    Added logger.example.py file

---
+++ freesound/logger.example.py
@@ -0,0 +1,19 @@
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/path/to/django/debug.log',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+

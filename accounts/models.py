b40e306121979c2372a473a67d56e9d324bed96a
ffont <frederic.font@gmail.com>
2015-07-21 15:42:39 +0200

    Tidying up code in accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -21,19 +21,20 @@
 #
 
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 from django.db import models
 from django.db.models.signals import post_save
 from django.utils.encoding import smart_unicode
+from django.conf import settings
 from general.models import SocialModel
 from geotags.models import GeoTag
 from utils.sql import DelayedQueryExecuter
-from django.conf import settings
 from utils.locations import locations_decorator
-import datetime
-import os
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
 from tickets.views import get_num_pending_sounds
+import datetime
+import random
+import os
 
 
 class ResetEmailRequest(models.Model):
@@ -42,37 +43,31 @@ class ResetEmailRequest(models.Model):
     
     
 class ProfileManager(models.Manager):
-    def random_uploader(self):
-        import random
 
+    @staticmethod
+    def random_uploader():
         user_count = User.objects.filter(profile__num_sounds__gte=1).count()
-
         if user_count:
             offset = random.randint(0, user_count - 1)
             return User.objects.filter(profile__num_sounds__gte=1)[offset:offset+1][0]
         else:
             return None
 
+
 class Profile(SocialModel):
     user = models.OneToOneField(User, related_name="profile")
-
     about = models.TextField(null=True, blank=True, default=None)
     home_page = models.URLField(null=True, blank=True, default=None)
     signature = models.TextField(max_length=256, null=True, blank=True)
     geotag = models.ForeignKey(GeoTag, null=True, blank=True, default=None)
     has_avatar = models.BooleanField(default=False)
-
     wants_newsletter = models.BooleanField(default=True, db_index=True)
     is_whitelisted = models.BooleanField(default=False, db_index=True)
-
     num_sounds = models.PositiveIntegerField(editable=False, default=0)
     num_posts = models.PositiveIntegerField(editable=False, default=0)
-
     has_old_license = models.BooleanField(null=False, default=False)
     not_shown_in_online_users_list = models.BooleanField(null=False, default=False)
-
     accepted_tos = models.BooleanField(default=False)
-
     enabled_stream_emails = models.BooleanField(db_index=True, default=False)
     last_stream_email_sent = models.DateTimeField(db_index=True, null=True, default=None)
     last_attempt_of_sending_stream_email = models.DateTimeField(db_index=True, null=True, default=None)
@@ -84,7 +79,7 @@ class Profile(SocialModel):
 
     @models.permalink
     def get_absolute_url(self):
-        return ('account', (smart_unicode(self.user.username),))
+        return 'account', (smart_unicode(self.user.username),)
 
     @locations_decorator(cache=False)
     def locations(self):
@@ -98,72 +93,71 @@ class Profile(SocialModel):
             m_avatar = settings.MEDIA_URL + "images/40x40_avatar.png"
             l_avatar = settings.MEDIA_URL + "images/70x70_avatar.png"
         return dict(
-            avatar = dict(
-                S = dict(
-                    path = os.path.join(settings.AVATARS_PATH, id_folder, "%d_S.jpg" % self.user_id),
-                    url = s_avatar
+            avatar=dict(
+                S=dict(
+                    path=os.path.join(settings.AVATARS_PATH, id_folder, "%d_S.jpg" % self.user_id),
+                    url=s_avatar
                 ),
-                M = dict(
-                    path = os.path.join(settings.AVATARS_PATH, id_folder, "%d_M.jpg" % self.user_id),
-                    url = m_avatar
+                M=dict(
+                    path=os.path.join(settings.AVATARS_PATH, id_folder, "%d_M.jpg" % self.user_id),
+                    url=m_avatar
                 ),
-                L = dict(
-                    path = os.path.join(settings.AVATARS_PATH, id_folder, "%d_L.jpg" % self.user_id),
-                    url = l_avatar
+                L=dict(
+                    path=os.path.join(settings.AVATARS_PATH, id_folder, "%d_L.jpg" % self.user_id),
+                    url=l_avatar
                 )
             )
         )
 
     def get_tagcloud(self):
         return DelayedQueryExecuter("""
-            select
-                tags_tag.name as name,
-                X.c as count
-            from (
-                select
+            SELECT
+                tags_tag.name AS name,
+                X.c AS count
+            FROM (
+                SELECT
                     tag_id,
                     count(*) as c
-                from tags_taggeditem
-                left join sounds_sound on
-                    object_id=sounds_sound.id
-                where
-                    tags_taggeditem.user_id=%d and
-                    sounds_sound.moderation_state='OK' and
+                FROM tags_taggeditem
+                LEFT JOIN sounds_sound ON object_id=sounds_sound.id
+                WHERE
+                    tags_taggeditem.user_id=%d AND
+                    sounds_sound.moderation_state='OK' AND
                     sounds_sound.processing_state='OK'
-                group by tag_id
-                order by c
-                desc limit 10
-            ) as X
-            left join tags_tag on tags_tag.id=X.tag_id
-            order by tags_tag.name;""" % self.user_id)
+                GROUP BY tag_id
+                ORDER BY c
+                DESC LIMIT 10
+            ) AS X
+            LEFT JOIN tags_tag ON tags_tag.id=X.tag_id
+            ORDER BY tags_tag.name;""" % self.user_id)
 
     def can_post_in_forum(self):
-
-        # POSTS PENDING TO MODERATE: Do not allow new posts if there are others pending to moderate
         user_has_posts_pending_to_moderate = self.user.post_set.filter(moderation_state="NM").count() > 0
         if user_has_posts_pending_to_moderate:
-            return False, "We're sorry but you can't post to the forum because you have previous posts still pending to moderate"
+            return False, "We're sorry but you can't post to the forum because you have previous posts still " \
+                          "pending to moderate"
 
-        # THROTTLING
         if self.user.post_set.all().count() >= 1 and self.user.sounds.all().count() == 0:
             today = datetime.datetime.today()
-            reference_date = self.user.post_set.all()[0].created # or since registration date: reference_date = self.user.date_joined
+            reference_date = self.user.post_set.all()[0].created
 
             # Do not allow posts if last post is not older than 5 minutes
             seconds_per_post = 60*5
             if (today - self.user.post_set.all().reverse()[0].created).seconds < seconds_per_post:
-                return False, "We're sorry but you can't post to the forum because your last post was less than 5 minutes ago"
+                return False, "We're sorry but you can't post to the forum because your last post was less than 5 " \
+                              "minutes ago"
 
             # Do not allow posts if user has already posyted N posts that day
-            # (every day users can post as many posts as twice the number of days since the reference date (registration or first post date))
             max_posts_per_day = 5 + pow((today - reference_date).days,2)
-            if self.user.post_set.filter(created__range=(today-datetime.timedelta(days=1),today)).count() > max_posts_per_day:
-                return False, "We're sorry but you can't post to the forum because you exceeded your maximum number of posts per day"
+            if self.user.post_set.filter(created__range=(today-datetime.timedelta(days=1), today)).count() > \
+                    max_posts_per_day:
+                return False, "We're sorry but you can't post to the forum because you exceeded your maximum number " \
+                              "of posts per day"
 
         return True, ""
 
     def is_blocked_for_spam_reports(self):
-        reports_count = UserFlag.objects.filter(user__username = self.user.username).values('reporting_user').distinct().count()
+        reports_count = UserFlag.objects.filter(user__username=self.user.username).values('reporting_user').distinct().count()
         if reports_count < settings.USERFLAG_THRESHOLD_FOR_AUTOMATIC_BLOCKING or self.user.sounds.all().count() > 0:
             return False
         else:

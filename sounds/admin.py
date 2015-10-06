27f393c21e08c3b2ebbf6276f69f153d9bde7e43
ffont <frederic.font@gmail.com>
2015-10-06 15:06:07 +0200

    Code styling in sounds/admin.py

--- sounds/admin.py
+++ sounds/admin.py
@@ -21,7 +21,8 @@
 #
 
 from django.contrib import admin
-from sounds.models import Download, License, Sound, Pack, Flag, DeletedSound
+from sounds.models import License, Sound, Pack, Flag, DeletedSound
+
 
 class LicenseAdmin(admin.ModelAdmin):
     list_display = ('name', 'deed_url', 'legal_code_url', 'change_order')
@@ -31,22 +32,25 @@ admin.site.register(License, LicenseAdmin)
 
 class SoundAdmin(admin.ModelAdmin):
     fieldsets = ((None, {'fields': ('user', )}),
-                 #('Filenames', {'fields': ('original_path', 'base_filename_slug')}),
                  ('Filenames', {'fields': ('base_filename_slug',)}),
                  ('User defined fields', {'fields': ('description', 'license', 'original_filename', 'sources', 'pack')}),
-                 ('File properties', {'fields': ('md5', 'type', 'duration', 'bitrate', 'bitdepth', 'samplerate', 'filesize', 'channels', 'date_recorded')}),
+                 ('File properties', {'fields': ('md5', 'type', 'duration', 'bitrate', 'bitdepth', 'samplerate',
+                                                 'filesize', 'channels', 'date_recorded')}),
                  ('Moderation', {'fields': ('moderation_state', 'moderation_date', 'has_bad_description')}),
-                 ('Processing', {'fields': ('processing_state', 'processing_date', 'processing_log', 'analysis_state', 'similarity_state')}),
+                 ('Processing', {'fields': ('processing_state', 'processing_date', 'processing_log', 'analysis_state',
+                                            'similarity_state')}),
                  )
     raw_id_fields = ('user', 'pack', 'sources')
     list_display = ('id', 'user', 'original_filename', 'license', 'created', 'moderation_state')
     list_filter = ('moderation_state', 'license', 'processing_state')
 admin.site.register(Sound, SoundAdmin)
 
+
 class DeletedSoundAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)
     list_display = ('sound_id', 'user')
 admin.site.register(DeletedSound, DeletedSoundAdmin)
+
 
 class PackAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)

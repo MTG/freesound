76038967a67d59a2332c8b488925f0832057237c
ffont <frederic.font@gmail.com>
2015-12-03 12:20:27 +0100

    Refactored post_dirty_sounds_to_solr and reindex_solr
     post_dirty_sounds_to_solr and reindex_solr now also take care of deleting sounds which are in solr and should not be there.
    
    https://github.com/MTG/freesound/issues/688

--- search/management/commands/post_dirty_sounds_to_solr.py
+++ search/management/commands/post_dirty_sounds_to_solr.py
@@ -20,20 +20,36 @@
 
 from django.core.management.base import BaseCommand
 from sounds.models import Sound
-from utils.search.search_general import add_all_sounds_to_solr
+from utils.search.search_general import add_all_sounds_to_solr, delete_sound_from_solr, check_if_sound_exists_in_solr
 import logging
 logger = logging.getLogger("web")
+
 
 class Command(BaseCommand):
     args = ''
     help = 'Add all sounds with index_dirty flag True to SOLR index'
 
     def handle(self, *args, **options):
-        sound_qs = Sound.objects.select_related("pack", "user", "license") \
-                                .filter(is_index_dirty=True,
-                                        moderation_state='OK',
-                                        processing_state='OK')
+        sounds_dirty = Sound.objects.select_related("pack", "user", "license").filter(is_index_dirty=True)
 
-        logger.info("Starting posting dirty sounds to solr. %i sounds to be added/updated to the solr index" % sound_qs.count())
-        num_correctly_indexed_sounds = add_all_sounds_to_solr(sound_qs, mark_index_clean=True)
-        logger.info("Finished posting dirty sounds to solr. %i sounds have been added/updated" % num_correctly_indexed_sounds)
+        # Index all those which are processed and moderated ok
+        sounds_dirty_to_index = sounds_dirty.filter(moderation_state='OK', processing_state='OK')
+        logger.info("Starting posting dirty sounds to solr. %i sounds to be added/updated to the solr index"
+                    % sounds_dirty_to_index.count())
+        num_correctly_indexed_sounds = add_all_sounds_to_solr(sounds_dirty_to_index, mark_index_clean=True)
+        logger.info("Finished posting dirty sounds to solr. %i sounds have been added/updated"
+                    % num_correctly_indexed_sounds)
+
+        # Remove all those which are not processed or moderated ok and that are still in solr (should not happen)
+        sounds_dirty_to_remove = sounds_dirty.exclude(moderation_state='OK', processing_state='OK')
+        n_deleted_sounds = 0
+        for sound in sounds_dirty_to_remove:
+            if check_if_sound_exists_in_solr(sound):
+                # We need to know if the sound exists in solr so that besides deleting it (which could be accomplished
+                # by simply using delete_sound_from_solr), we know whether we have to change is_index_dirty state. If
+                # we do not change it, then we would try to delete the sound at every attempt.
+                delete_sound_from_solr(sound)
+                n_deleted_sounds += 1
+                sound.is_index_dirty = False
+                sound.save()
+        logger.info("Deleted %i sounds from solr index." % n_deleted_sounds)

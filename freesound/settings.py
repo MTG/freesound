dc9b333c765577fddc7b63b99151690200abc2be
ffont <frederic.font@gmail.com>
2016-04-22 18:13:54 +0200

    Re-enable xml, jsonp and yaml api renderers

--- freesound/settings.py
+++ freesound/settings.py
@@ -223,9 +223,9 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-        #'rest_framework.renderers.YAMLRenderer',
-        #'rest_framework.renderers.JSONPRenderer',
-        #'rest_framework.renderers.XMLRenderer',
+        'rest_framework_yaml.renderers.YAMLRenderer',
+        'rest_framework_jsonp.renderers.JSONPRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',
     ),
     'DEFAULT_THROTTLE_CLASSES': (
         'apiv2.throttling.ClientBasedThrottlingBurst',

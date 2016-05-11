ce3a23a75dc4c32f59bb82c84275576a264b355c
ffont <frederic.font@gmail.com>
2016-05-11 12:59:51 +0200

    Add APIErrorLogsFilter

--- utils/logging_filters.py
+++ utils/logging_filters.py
@@ -71,3 +71,53 @@ class APILogsFilter(logging.Filter):
                 record.api_client_username = 'unknown'
 
         return True
+
+
+class APIErrorLogsFilter(logging.Filter):
+    # Only gets apiv2 error messages
+
+    def filter(self, record):
+        message = record.getMessage().encode('utf8')
+
+        if 'ApiV2' in message:
+            try:
+                record.status = int(message.split(' ')[0][1:])
+            except Exception, e:
+                record.status = -1
+
+            try:
+                record.summary_message = message.split('<')[1].split('>')[0]
+            except:
+                record.summary_message = 'unknown'
+
+            try:
+                record.long_message = message.split('> ')[1].split(' (ApiV2')[0]
+            except:
+                record.long_message = 'unknown'
+
+            request_info = message[message.rfind('(')+1:-1]
+            try:
+                record.ip = request_info.split('Ip:')[1]
+            except:
+                record.ip = 'unknown'
+            try:
+                record.api_client_id = request_info.split('Client:')[1].split(' Ip:')[0]
+            except:
+                record.api_client_id = 'unknown'
+
+            try:
+                record.api_client_username = request_info.split('Dev:')[1].split(' User:')[0]
+            except:
+                record.api_client_username = 'unknown'
+
+            try:
+                record.api_enduser_username = request_info.split('User:')[1].split(' Client:')[0]
+            except:
+                record.api_enduser_username = 'unknown'
+
+            try:
+                record.api_auth_type = request_info.split('Auth:')[1].split(' Dev:')[0]
+            except:
+                record.api_auth_type = 'unknown'
+
+        return True

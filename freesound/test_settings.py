8e3fd8c4155d5d69b771e1cefbc8963ff97665dc
Bram de Jong <bram.dejong@native-instruments.de>
2016-09-09 15:54:03 +0200

    Disabled raven in test settings

--- freesound/test_settings.py
+++ freesound/test_settings.py
@@ -4,8 +4,9 @@ DATABASES['default']['NAME'] = 'freesound'
 DATABASES['default']['USER'] = 'freesound'
 INSTALLED_APPS = INSTALLED_APPS + ('django_nose', )
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-SOUTH_TESTS_MIGRATE = False
 NOSE_ARGS = [
     '--with-xunit',
 ]
 SECRET_KEY = "testsecretwhichhastobeatleast16characterslong"
+
+RAVEN_CONFIG = {}

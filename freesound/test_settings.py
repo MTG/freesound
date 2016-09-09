b419d8d336ec565f84b469596705cc823e5f8a43
Bram de Jong <bram.dejong@native-instruments.de>
2016-09-09 15:45:50 +0200

    Added test settings to test_settings.py

--- freesound/test_settings.py
+++ freesound/test_settings.py
@@ -1,3 +1,11 @@
 from settings import *
 
+DATABASES['default']['NAME'] = 'freesound'
+DATABASES['default']['USER'] = 'freesound'
+INSTALLED_APPS = INSTALLED_APPS + ('django_nose', )
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 SOUTH_TESTS_MIGRATE = False
+NOSE_ARGS = [
+    '--with-xunit',
+]
+SECRET_KEY = "testsecretwhichhastobeatleast16characterslong"

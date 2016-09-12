5af8f37fd9afd1481cc6159487c576c0690a5395
Frederic Font <frederic.font@upf.edu>
2016-09-12 13:34:54 +0200

    Make test settings work with other test environments we use
    Now we read postgres username from environment variable `FS_TEST_PG_USERNAME` and also use `FS_TEST_USE_DJANGO_NOSE` variable as a flag for using or not using django-nose. Travis and Jenkins configurations will need to be updated accordingly.

--- freesound/test_settings.py
+++ freesound/test_settings.py
@@ -1,12 +1,18 @@
 from settings import *
 
-DATABASES['default']['NAME'] = 'freesound'
-DATABASES['default']['USER'] = 'postgres'
-INSTALLED_APPS = INSTALLED_APPS + ('django_nose', )
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-NOSE_ARGS = [
-    '--with-xunit',
-]
-SECRET_KEY = "testsecretwhichhastobeatleast16characterslong"
+postgres_username = os.getenv('FS_TEST_PG_USERNAME', None)
+if postgres_username is not None:
+    DATABASES['default']['NAME'] = 'freesound'
+    DATABASES['default']['USER'] = postgres_username
 
+use_django_nose = os.getenv('FS_TEST_USE_DJANGO_NOSE', None)
+if use_django_nose is not None:
+    INSTALLED_APPS = INSTALLED_APPS + ('django_nose',)
+    TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+    SOUTH_TESTS_MIGRATE = False
+    NOSE_ARGS = [
+        '--with-xunit',
+    ]
+
+SECRET_KEY = "testsecretwhichhastobeatleast16characterslong"
 RAVEN_CONFIG = {}

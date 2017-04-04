6c6faa78b8a3825c790fb761475ff808f32a0dec
Frederic Font <frederic.font@gmail.com>
2017-04-04 11:22:38 +0200

    Make unit test for test_should_suggest_donation more comprehensive
    https://github.com/MTG/freesound/pull/820

--- utils/tests.py
+++ utils/tests.py
@@ -21,15 +21,18 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from django.conf import settings
 from utils.forms import filename_has_valid_extension
-from sounds.models import Sound, Pack, License, Download, DeletedSound
+from sounds.models import Sound, Pack, License, Download
 from donations.models import Donation
-
+import datetime
 import utils.downloads
+
 
 class UtilsTest(TestCase):
 
     fixtures = ['initial_data']
+
     def test_filename_has_valid_extension(self):
         cases = [
             ('filaneme.wav', True),
@@ -44,30 +47,61 @@ class UtilsTest(TestCase):
         for filename, expected_result in cases:
             self.assertEqual(filename_has_valid_extension(filename), expected_result)
 
-
     def test_download_sounds(self):
         user = User.objects.create_user("testuser", password="testpass")
         pack = Pack.objects.create(user=user, name="Test pack")
         for i in range(0, 5):
-            sound = Sound.objects.create(user=user,
-                                     original_filename="Test sound %i" % (i),
-                                     base_filename_slug="test_sound_%i" % (i),
-                                     license=License.objects.all()[0],
-                                     pack=pack,
-                                     md5="fakemd5_%i" % (i))
+            sound = Sound.objects.create(
+                user=user,
+                original_filename="Test sound %i" % i,
+                base_filename_slug="test_sound_%i" % i,
+                license=License.objects.all()[0],
+                pack=pack,
+                md5="fakemd5_%i" % i)
         licenses_url = (reverse('pack-licenses', args=["testuser", pack.id]))
         ret = utils.downloads.download_sounds(licenses_url, pack)
         self.assertEqual(ret.status_code, 200)
 
-    def test_download_popup(self):
+    def test_should_suggest_donation(self):
         user = User.objects.create_user("testuser", password="testpass")
-        sound = Sound.objects.create(user=user, original_filename="Test sound",
-             base_filename_slug="test_sound_10",
-             license=License.objects.all()[0],
-             md5="fakemd5_10")
-        for i in range(0, 5):
-            Download.objects.create(user=user, sound=sound)
-        self.assertEqual(utils.downloads.should_suggest_donation(user, None), True)
 
+        # should_suggest_donation returns False if modal has been shown more than DONATION_MODAL_DISPLAY_TIMES_DAY
+        times_shown_in_last_day = settings.DONATION_MODAL_DISPLAY_TIMES_DAY + 1
+        self.assertEqual(utils.downloads.should_suggest_donation(user, times_shown_in_last_day), False)
+
+        # set times_shown_in_last_day lower than DONATION_MODAL_DISPLAY_TIMES_DAY
+        times_shown_in_last_day = settings.DONATION_MODAL_DISPLAY_TIMES_DAY - 1
+
+        # if user donated recently, modal is not shown (even if times_shown_in_last_day <
+        # DONATION_MODAL_DISPLAY_TIMES_DAY)
+        donation = Donation.objects.create(user=user, amount=1)
+        self.assertEqual(utils.downloads.should_suggest_donation(user, times_shown_in_last_day), False)
+
+        # remove donation object (to simulate user never donated)
+        donation.delete()
+
+        # if user has downloaded less or equal than DONATION_MODAL_DOWNLOADS_IN_PERIOD, do not show the modal
+        sound = Sound.objects.create(
+            user=user,
+            original_filename="Test sound",
+            base_filename_slug="test_sound_10",
+            license=License.objects.all()[0],
+            md5="fakemd5_10")
+        for i in range(0, settings.DONATION_MODAL_DOWNLOADS_IN_PERIOD):
+            Download.objects.create(user=user, sound=sound)
+            self.assertEqual(utils.downloads.should_suggest_donation(user, times_shown_in_last_day), False)
+        Download.objects.create(user=user, sound=sound)  # n downloads > DONATION_MODAL_DOWNLOADS_IN_PERIOD
+        self.assertEqual(utils.downloads.should_suggest_donation(user, times_shown_in_last_day), True)
+
+        # if the download objects are older than DONATION_MODAL_DOWNLOAD_DAYS, don't consider them
+        Download.objects.filter(user=user).update(
+            created=datetime.datetime.now()-datetime.timedelta(days=settings.DONATION_MODAL_DOWNLOAD_DAYS + 1))
+        self.assertEqual(utils.downloads.should_suggest_donation(user, times_shown_in_last_day), False)
+
+        # if user has donations but these are older than DONATION_MODAL_DAYS_AFTER_DONATION, do not consider them
         Donation.objects.create(user=user, amount=1)
-        self.assertEqual(utils.downloads.should_suggest_donation(user, None), False)
+        Donation.objects.filter(user=user).update(
+            created=datetime.datetime.now()-datetime.timedelta(days=settings.DONATION_MODAL_DAYS_AFTER_DONATION + 1))
+        Download.objects.filter(user=user).update(
+            created=datetime.datetime.now())  # Change downloads date again to be recent (modal show be shown)
+        self.assertEqual(utils.downloads.should_suggest_donation(user, times_shown_in_last_day), True)

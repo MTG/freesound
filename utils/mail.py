fc93e9ac382fa30ac4e70f41bbb88657485608a6
Frederic Font <frederic.font@gmail.com>
2017-05-10 10:20:00 +0200

    Remove unused utils.mail.send_mass_html_mail function
    https://github.com/MTG/freesound/pull/763

--- utils/mail.py
+++ utils/mail.py
@@ -76,27 +76,3 @@ def render_mail_template(template, context):
     context["settings"] = settings
     return render_to_string(template, context)
 
-
-def send_mass_html_mail(datatuple, fail_silently=False, user=None, password=None, connection=None):
-    """
-    Given a datatuple of (subject, text_content, html_content, from_email,
-    recipient_list), sends each message to each recipient list. Returns the
-    number of emails sent.
-
-    If from_email is None, the DEFAULT_FROM_EMAIL setting is used.
-    If auth_user and auth_password are set, they're used to log in.
-    If auth_user is None, the EMAIL_HOST_USER setting is used.
-    If auth_password is None, the EMAIL_HOST_PASSWORD setting is used.
-
-    """
-    connection = connection or get_connection(
-        username=user, password=password, fail_silently=fail_silently
-    )
-
-    messages = []
-    for subject, text, html, from_email, recipient in datatuple:
-        message = EmailMultiAlternatives(subject, text, from_email, recipient)
-        message.attach_alternative(html, 'text/html')
-        messages.append(message)
-
-    return connection.send_messages(messages)

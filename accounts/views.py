6508d26d4203c4eb6f01e74757718e4270073db2
Frederic Font <frederic.font@gmail.com>
2017-06-07 11:54:12 +0200

    Use correct authentication form in custom login view
    https://github.com/MTG/freesound/pull/863

--- accounts/views.py
+++ accounts/views.py
@@ -83,8 +83,7 @@ def login(request, template_name, authentication_form):
     # Freesound-specific login view to check if a user has multiple accounts
     # with the same email address. We can switch back to the regular django view
     # once all accounts are adapted
-    from django.contrib.auth import views as auth_views
-    response = auth_views.LoginView.as_view()(request, template_name=template_name, authentication_form=authentication_form)
+    response = LoginView.as_view(template_name=template_name, authentication_form=authentication_form)(request)
     if isinstance(response, HttpResponseRedirect):
         # If there is a redirect it's because the login was successful
         # Now we check if the logged in user has shared email problems

82eb17bd1fa214b8206e37d7684ec4a0fa86eb51
Andres Ferraro <andres.ferraro@upf.edu>
2017-06-22 13:18:32 +0200

    Change warning message from js shown to user when typing

--- media/js/freesound.js
+++ media/js/freesound.js
@@ -157,7 +157,7 @@ function afterDownloadModal(show_modal_url, sound_name){
 function unsecureImageCheck(input) {
     var unique = '#unsecure_img_check' + new Date().valueOf();
     var div = $("<div>", {style: "color: red"});
-    div.append("Sorry, images must be from a secure uri.");
+    div.append("Warning: you are adding an image from a non-secure location. Only images from secure locations (i.e. the URL starts with https://...) will be shown in Freesound.");
     div.insertAfter(input);
     div.hide();
       

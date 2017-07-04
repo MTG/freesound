a45138a410eff3a6486877ed3c4fd309d60c3469
Frederic Font <frederic.font@gmail.com>
2017-07-04 16:49:24 +0200

    Remove unneded `var unique =` line
    https://github.com/MTG/freesound/pull/898

--- media/js/freesound.js
+++ media/js/freesound.js
@@ -155,7 +155,6 @@ function afterDownloadModal(show_modal_url, sound_name){
         });
 }
 function unsecureImageCheck(input) {
-    var unique = '#unsecure_img_check' + new Date().valueOf();
     var div = $("<div>", {class: "unsecure_image_warning"});
     div.append("<b>Warning</b>: We only support images from HTTPS locations. Images from an HTTP location will appear as a link.");
     div.insertBefore(input);

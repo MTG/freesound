c6d8da6faf8070e23e06a617afdf5e9e18083b0a
Frederic Font <frederic.font@gmail.com>
2017-07-07 15:11:09 +0200

    Add migration in atomic transaction to make it faster
    https://github.com/MTG/freesound/pull/907

--- comments/migrations/0003_copy_sound.py
+++ comments/migrations/0003_copy_sound.py
@@ -1,15 +1,17 @@
-from django.db import models, migrations
-from django.contrib.contenttypes.models import ContentType
+from django.db import migrations, transaction
+
 
 def forwards_func(apps, schema_editor):
-    "Migration from old model to the new one."
+    """
+    Migration from old model to the new one."
+    """
     Comment = apps.get_model("comments", "Comment")
-    Sound= apps.get_model("sounds", "Sound")
-    db_alias = schema_editor.connection.alias
-    for comment in Comment.objects.all():
-        s = Sound.objects.get(pk=comment.object_id)
-        comment.sound = s
-        comment.save()
+    Sound = apps.get_model("sounds", "Sound")
+    with transaction.atomic():
+        for comment in Comment.objects.all():
+            s = Sound.objects.get(pk=comment.object_id)
+            comment.sound = s
+            comment.save()
 
 
 class Migration(migrations.Migration):

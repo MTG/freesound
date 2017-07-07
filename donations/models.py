9b36325a71919dc18cd0c4a3415fdc8823c6a636
Frederic Font <frederic.font@gmail.com>
2017-07-07 11:13:59 +0200

    Add method to DonationsModalSettings to return settings from cache
    https://github.com/MTG/freesound/issues/916

--- donations/models.py
+++ donations/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
+from django.core.cache import cache
 
 
 class DonationCampaign(models.Model):
@@ -31,3 +32,23 @@ class DonationsModalSettings(models.Model):
         default=0.25, help_text='probabily of the modal being shown once all previous requirements are met')
     max_times_display_a_day = models.PositiveIntegerField(
         default=10, help_text='max number of times we display the modal during a single day')
+
+    DONATION_MODAL_SETTINGS_CACHE_KEY = 'donation-modal-settings'
+
+    def get_donation_modal_settings(self):
+        """
+        Return the current setting stored in the model.
+        Because this model will be queryied often (every time a user downloads a sound), we want to avoid hitting the
+        DB every time. We store the settings in the cache and return the data from there (if exists).
+        """
+        settings = cache.get(self.DONATION_MODAL_SETTINGS_CACHE_KEY, None)
+        if settings is None:
+            settings = {
+                'enabled': self.enabled,
+                'days_after_donation': self.days_after_donation,
+                'downloads_in_period': self.downloads_in_period,
+                'download_days': self.download_days,
+                'max_times_display_a_day': self.max_times_display_a_day,
+            }
+            cache.set(self.DONATION_MODAL_SETTINGS_CACHE_KEY, settings, timeout=3600)
+        return settings

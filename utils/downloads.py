c2f37100ec18026d70832ba2ecc6aab855d5400a
Frederic Font <frederic.font@gmail.com>
2017-07-07 13:51:52 +0200

    Only show modal to users that have not uploaded any sounds
    https://github.com/MTG/freesound/issues/916

--- utils/downloads.py
+++ utils/downloads.py
@@ -67,6 +67,10 @@ def should_suggest_donation(user, times_shown_in_last_day):
         # If modal has been shown more than settings.DONATION_MODAL_DISPLAY_TIMES_DAY times, don't show it again today
         return False
 
+    if user.profile.num_sounds > 0:
+        # Never show modal to users that have uploaded sounds
+        return False
+
     donation_period = datetime.datetime.now() - datetime.timedelta(days=donation_modal_settings.days_after_donation)
     last_donation = user.donation_set.order_by('created').last()
     if not last_donation or last_donation.created < donation_period:

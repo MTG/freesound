8767e9a2401ac9c8f28a26e6b59b96e06c0b5dcf
Frederic Font <frederic.font@gmail.com>
2017-07-07 11:00:03 +0200

    Create model (and migrations) to store donations modal settings
    https://github.com/MTG/freesound/issues/916

--- donations/admin.py
+++ donations/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from models import DonationCampaign
+from models import DonationCampaign, DonationsModalSettings
 
 admin.site.register(DonationCampaign)
+
+
+class DonationsModalSettingsAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+        num_objects = self.model.objects.count()
+        if num_objects >= 1:
+            return False
+        else:
+            return True
+
+admin.site.register(DonationsModalSettings, DonationsModalSettingsAdmin)
--- donations/models.py
+++ donations/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class DonationCampaign(models.Model):
     goal = models.DecimalField(max_digits=6, decimal_places=2)
     date_start = models.DateField()
@@ -18,3 +19,15 @@ class Donation(models.Model):
     campaign = models.ForeignKey(DonationCampaign, null=True)
     is_anonymous= models.BooleanField(default=True)
     display_amount = models.BooleanField(default=True)
+
+
+class DonationsModalSettings(models.Model):
+    enabled = models.BooleanField(default=False)
+    days_after_donation = models.PositiveIntegerField(
+        default=365, help_text='If user made a donation in the last X days, no modal is shown')
+    downloads_in_period = models.PositiveIntegerField(default=5, help_text='After user has download Z sounds...')
+    download_days = models.PositiveIntegerField(default=7, help_text='...in Y days, we display the modal')
+    display_probability = models.FloatField(
+        default=0.25, help_text='probabily of the modal being shown once all previous requirements are met')
+    max_times_display_a_day = models.PositiveIntegerField(
+        default=10, help_text='max number of times we display the modal during a single day')

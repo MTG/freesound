MTG/freesound

commit 7082fd979635acbdc3911271bba51bee4f139330

Clean up some search functions, update python syntax, remove unused code

utils/search/search_general.py
@@ -32,7 +32,7 @@ logger = logging.getLogger("search")
 
 
 def convert_to_solr_document(sound):
-    #logger.info("creating solr XML from sound %d" % sound.id)
+    # logger.info("creating solr XML from sound %d" % sound.id)
     document = dict()
     document["id"] = sound.id
     document["username"] = sound.user.username
@@ -75,14 +75,6 @@ def convert_to_solr_document(sound):
     return document
 
 
-def add_sound_to_solr(sound):
-    logger.info("adding single sound to solr index")
-    try:
-        Solr(settings.SOLR_URL).add([convert_to_solr_document(sound)])
-    except SolrException, e:
-        logger.error("failed to add sound %d to solr index, reason: %s" % (sound.id, str(e)))
-
-
 def add_sounds_to_solr(sounds):
     logger.info("adding multiple sounds to solr index")
     solr = Solr(settings.SOLR_URL)
@@ -98,7 +90,7 @@ def add_all_sounds_to_solr(sound_queryset, slice_size=4000, mark_index_clean=Fal
     num_sounds = sound_queryset.count()
     num_correctly_indexed_sounds = 0
     for i in range(0, num_sounds, slice_size):
-        print "Adding %i sounds to solr, slice %i"%(slice_size,i)
+        logger.info("Adding %i sounds to solr, slice %i", slice_size, i)
         try:
             sounds_to_update = sound_queryset[i:i+slice_size]
             add_sounds_to_solr(sounds_to_update)
@@ -107,8 +99,9 @@ def add_all_sounds_to_solr(sound_queryset, slice_size=4000, mark_index_clean=Fal
                 sounds.models.Sound.objects.filter(pk__in=[snd.id for snd in sounds_to_update])\
                     .update(is_index_dirty=False)
                 num_correctly_indexed_sounds += len(sounds_to_update)
-        except SolrException, e:
-            logger.error("failed to add sound batch to solr index, reason: %s" % str(e))
+        except SolrException as e:
+            logger.error("failed to add sound batch to solr index, reason: %s", str(e))
+            raise
     return num_correctly_indexed_sounds
 
 
@@ -121,17 +114,14 @@ def get_all_sound_ids_from_solr(limit=False):
     solr_count = None
     PAGE_SIZE = 2000
     current_page = 1
-    try:
-        while (len(solr_ids) < solr_count or solr_count is None) and len(solr_ids) < limit:
-            response = SolrResponseInterpreter(
-                solr.select(unicode(search_prepare_query(
-                    '', '', search_prepare_sort('created asc', SEARCH_SORT_OPTIONS_WEB), current_page, PAGE_SIZE,
-                    include_facets=False))))
-            solr_ids += [element['id'] for element in response.docs]
-            solr_count = response.num_found
-            current_page += 1
-    except Exception, e:
-        raise Exception(e)
+    while (len(solr_ids) < solr_count or solr_count is None) and len(solr_ids) < limit:
+        response = SolrResponseInterpreter(
+            solr.select(unicode(search_prepare_query(
+                '', '', search_prepare_sort('created asc', SEARCH_SORT_OPTIONS_WEB), current_page, PAGE_SIZE,
+                include_facets=False))))
+        solr_ids += [element['id'] for element in response.docs]
+        solr_count = response.num_found
+        current_page += 1
     return sorted(solr_ids)
 
 
@@ -161,12 +151,12 @@ def get_random_sound_from_solr():
             return docs[0]
     except socket_error:
         pass
-    return None
+    return {}
 
 
 def delete_sound_from_solr(sound):
     logger.info("deleting sound with id %d" % sound.id)
     try:
         Solr(settings.SOLR_URL).delete_by_id(sound.id)
-    except Exception, e:
+    except SolrException as e:
         logger.error('could not delete sound with id %s (%s).' % (sound.id, e))

commit 38d912d7236e2744549b4017cbaf92754ed01ab0

Catch socket.error for connection refused when deleting sounds from solr
I introduced a bug in commit 7082fd97 where I removed an `except
Exception` and replaced it with SolrException. This except block was
causing tests to pass when running with an unconfigured solr server,
because it would catch the Connection Refused socket error.

Tests shouldn't rely on blanket exceptions to catch misconfigurations,
so I added mocks for all tests that delete sounds, to just check that
this method was called, instead of calling it.

accounts/tests.py
@@ -650,11 +650,14 @@ class UserDelete(TestCase):
         self.assertEqual(Sound.objects.filter(user__id=user.id).exists(), True)
         self.assertEqual(Sound.objects.filter(user__id=user.id)[0].is_index_dirty, True)
 
-    def test_user_delete_remove_sounds(self):
+    @mock.patch('sounds.models.delete_sound_from_solr')
+    def test_user_delete_remove_sounds(self, delete_sound_solr):
         # This should set user's attribute deleted_user to True and anonymize it,
         # also should remove users Sounds and Packs, and create DeletedSound
         # objects
         user = self.create_user_and_content()
+        user_sounds = Sound.objects.filter(user=user)
+        user_sound_ids = [s.id for s in user_sounds]
         user.profile.delete_user(remove_sounds=True)
         self.assertEqual(User.objects.get(id=user.id).profile.is_deleted_user, True)
         self.assertEqual(user.username, "deleted_user_%s" % user.id)
@@ -670,6 +673,9 @@ class UserDelete(TestCase):
         self.assertEqual(Pack.objects.filter(user__id=user.id).all()[0].is_deleted, True)
         self.assertEqual(Sound.objects.filter(user__id=user.id).exists(), False)
         self.assertEqual(DeletedSound.objects.filter(user__id=user.id).exists(), True)
+
+        calls = [mock.call(i) for i in user_sound_ids]
+        delete_sound_solr.assert_has_calls(calls, any_order=True)
 
 
 class UserEmailsUniqueTestCase(TestCase):

search/management/commands/post_dirty_sounds_to_solr.py
@@ -48,7 +48,7 @@ class Command(BaseCommand):
                 # We need to know if the sound exists in solr so that besides deleting it (which could be accomplished
                 # by simply using delete_sound_from_solr), we know whether we have to change is_index_dirty state. If
                 # we do not change it, then we would try to delete the sound at every attempt.
-                delete_sound_from_solr(sound)
+                delete_sound_from_solr(sound.id)
                 n_deleted_sounds += 1
                 sound.is_index_dirty = False
                 sound.save()

search/management/commands/reindex_solr.py
@@ -36,4 +36,4 @@ class Command(BaseCommand):
         # Get all sounds that should not be in solr and remove them if they are
         sound_qs = Sound.objects.exclude(processing_state="OK", moderation_state="OK")
         for sound in sound_qs:
-            delete_sound_from_solr(sound)  # Will only do something if sound in fact exists in solr
+            delete_sound_from_solr(sound.id)  # Will only do something if sound in fact exists in solr

sounds/models.py
@@ -706,7 +706,7 @@ class Sound(SocialModel):
             audio_logger.info("Send sound with id %s to queue 'analyze'" % self.id)
 
     def delete_from_indexes(self):
-        delete_sound_from_solr(self)
+        delete_sound_from_solr(self.id)
         delete_sound_from_gaia(self)
 
     def invalidate_template_caches(self):

sounds/tests.py
@@ -235,7 +235,8 @@ class ChangeSoundOwnerTestCase(TestCase):
 
     fixtures = ['initial_data']
 
-    def test_change_sound_owner(self):
+    @mock.patch('sounds.models.delete_sound_from_solr')
+    def test_change_sound_owner(self, delete_sound_solr):
         # Prepare some content
         userA, packsA, soundsA = create_user_and_sounds(num_sounds=4, num_packs=1, tags="tag1 tag2 tag3 tag4 tag5")
         userB, _, _ = create_user_and_sounds(num_sounds=0, num_packs=0,
@@ -255,8 +256,9 @@ class ChangeSoundOwnerTestCase(TestCase):
         target_sound_id = target_sound.id
         target_sound_pack = target_sound.pack
         target_sound_tags = [ti.id for ti in target_sound.tags.all()]
+        remaining_sound_ids = [s.id for s in soundsA[1:]]  # Other sounds that the user owns
 
-        # Change owenership of sound
+        # Change ownership of sound
         target_sound.change_owner(userB)
 
         # Perform checks
@@ -274,13 +276,16 @@ class ChangeSoundOwnerTestCase(TestCase):
         userA.delete()  # Completely delete form db (instead of user.profile.delete_user())
         sound = Sound.objects.get(id=target_sound_id)
         self.assertItemsEqual([ti.id for ti in sound.tags.all()], target_sound_tags)
+        calls = [mock.call(i) for i in remaining_sound_ids]
+        delete_sound_solr.assert_has_calls(calls)  # All other sounds by the user were deleted
 
 
 class ProfileNumSoundsTestCase(TestCase):
 
     fixtures = ['initial_data']
 
-    def test_moderation_and_processing_state_changes(self):
+    @mock.patch('sounds.models.delete_sound_from_solr')
+    def test_moderation_and_processing_state_changes(self, delete_sound_solr):
         user, packs, sounds = create_user_and_sounds()
         sound = sounds[0]
         self.assertEqual(user.profile.num_sounds, 0)  # Sound not yet moderated or processed
@@ -294,14 +299,18 @@ class ProfileNumSoundsTestCase(TestCase):
         self.assertEqual(user.profile.num_sounds, 1)  # Sound reprocessed second time and again set as ok
         sound.change_processing_state("FA")
         self.assertEqual(user.profile.num_sounds, 0)  # Sound failed processing
+        delete_sound_solr.assert_called_once_with(sound.id)
         sound.change_processing_state("OK")
         self.assertEqual(user.profile.num_sounds, 1)  # Sound processed again as ok
         sound.change_moderation_state("DE")
         self.assertEqual(user.profile.num_sounds, 0)  # Sound unmoderated
+        self.assertEqual(delete_sound_solr.call_count, 2) # Sound deleted once when going to FA, once when DE
 
-    def test_sound_delete(self):
+    @mock.patch('sounds.models.delete_sound_from_solr')
+    def test_sound_delete(self, delete_sound_solr):
         user, packs, sounds = create_user_and_sounds()
         sound = sounds[0]
+        sound_id = sound.id
         sound.change_processing_state("OK")
         sound.change_moderation_state("OK")
         sound.add_comment(user, "some comment")
@@ -310,14 +319,17 @@ class ProfileNumSoundsTestCase(TestCase):
         sound.delete()
         self.assertEqual(user.profile.num_sounds, 0)
         self.assertEqual(Comment.objects.count(), 0)
+        delete_sound_solr.assert_called_once_with(sound_id)
 
-    def test_deletedsound_creation(self):
+    @mock.patch('sounds.models.delete_sound_from_solr')
+    def test_deletedsound_creation(self, delete_sound_solr):
         user, packs, sounds = create_user_and_sounds()
         sound = sounds[0]
         sound.change_processing_state("OK")
         sound.change_moderation_state("OK")
         sound_id = sound.id
         sound.delete()
+        delete_sound_solr.assert_called_once_with(sound_id)
 
         self.assertEqual(DeletedSound.objects.filter(sound_id=sound_id).exists(), True)
         ds = DeletedSound.objects.get(sound_id=sound_id)
@@ -348,7 +360,8 @@ class PackNumSoundsTestCase(TestCase):
 
     fixtures = ['initial_data']
 
-    def test_create_and_delete_sounds(self):
+    @mock.patch('sounds.models.delete_sound_from_solr')
+    def test_create_and_delete_sounds(self, delete_sound_solr):
         N_SOUNDS = 5
         user, packs, sounds = create_user_and_sounds(num_sounds=N_SOUNDS, num_packs=1)
         pack = packs[0]
@@ -358,7 +371,10 @@ class PackNumSoundsTestCase(TestCase):
             sound.change_moderation_state("OK")
             self.assertEqual(Pack.objects.get(id=pack.id).num_sounds, count + 1)  # Check pack has all sounds
 
-        sounds[0].delete()
+        sound_to_delete = sounds[0]
+        sound_to_delete_id = sound_to_delete.id
+        sound_to_delete.delete()
+        delete_sound_solr.assert_called_once_with(sound_to_delete_id)
         self.assertEqual(Pack.objects.get(id=pack.id).num_sounds, N_SOUNDS - 1)  # Check num_sounds on delete sound
 
     def test_edit_sound(self):
@@ -427,7 +443,8 @@ class SoundViewsTestCase(TestCase):
 
     fixtures = ['initial_data']
 
-    def test_delete_sound_view(self):
+    @mock.patch('sounds.models.delete_sound_from_solr')
+    def test_delete_sound_view(self, delete_sound_solr):
         user, packs, sounds = create_user_and_sounds(num_sounds=1, num_packs=1)
         sound = sounds[0]
         sound_id = sound.id
@@ -442,7 +459,7 @@ class SoundViewsTestCase(TestCase):
         self.assertEqual(resp.status_code, 403)
         self.assertEqual(Sound.objects.filter(id=sound_id).count(), 1)
 
-        # Try delete with expried encrypted link (should not delete sound)
+        # Try delete with expired encrypted link (should not delete sound)
         encrypted_link = encrypt(u"%d\t%f" % (sound.id, time.time() - 15))
         resp = self.client.post(reverse('sound-delete',
             args=[sound.user.username, sound.id]), {"encrypted_link": encrypted_link})
@@ -455,6 +472,7 @@ class SoundViewsTestCase(TestCase):
             args=[sound.user.username, sound.id]), {"encrypted_link": encrypted_link})
         self.assertEqual(Sound.objects.filter(id=sound_id).count(), 0)
         self.assertRedirects(resp, reverse('accounts-home'))
+        delete_sound_solr.assert_called_once_with(sound.id)
 
     def test_embed_iframe(self):
         user, packs, sounds = create_user_and_sounds(num_sounds=1, num_packs=1)

tickets/tests.py
@@ -138,7 +138,8 @@ class TicketsTest(TestCase):
                 'noreply@freesound.org',
                  assigned_ticket.sender.email)
 
-    def test_delete_ticket(self):
+    @mock.patch('sounds.models.delete_sound_from_solr')
+    def test_delete_ticket(self, delete_sound_solr):
         test_user = User.objects.get(username='test_user')
         test_moderator = User.objects.get(username='test_moderator')
         sound = self._create_test_sound(moderation_state='PE', processing_state='OK',
@@ -153,3 +154,4 @@ class TicketsTest(TestCase):
                                     'ticket': ticket.id
                                 })
         self.assertEqual(resp.status_code, 200)
+        delete_sound_solr.assert_called_once_with(sound.id)

utils/search/search_general.py
@@ -18,16 +18,19 @@
 #     See AUTHORS file.
 #
 
-from utils.search.solr import Solr, SolrQuery, SolrResponseInterpreter, SolrException
-from django.conf import settings
-from search.views import search_prepare_sort, search_prepare_query
-from search.forms import SEARCH_SORT_OPTIONS_WEB
-from socket import error as socket_error
-from utils.text import remove_control_chars
-import time
 import logging
 import math
+import socket
+import time
+
+from django.conf import settings
+
 import sounds
+from search.forms import SEARCH_SORT_OPTIONS_WEB
+from search.views import search_prepare_sort, search_prepare_query
+from utils.search.solr import Solr, SolrQuery, SolrResponseInterpreter, SolrException
+from utils.text import remove_control_chars
+
 logger = logging.getLogger("search")
 
 
@@ -149,14 +152,14 @@ def get_random_sound_from_solr():
         docs = response.docs
         if docs:
             return docs[0]
-    except socket_error:
+    except socket.error:
         pass
     return {}
 
 
-def delete_sound_from_solr(sound):
-    logger.info("deleting sound with id %d" % sound.id)
+def delete_sound_from_solr(sound_id):
+    logger.info("deleting sound with id %d" % sound_id)
     try:
-        Solr(settings.SOLR_URL).delete_by_id(sound.id)
-    except SolrException as e:
-        logger.error('could not delete sound with id %s (%s).' % (sound.id, e))
+        Solr(settings.SOLR_URL).delete_by_id(sound_id)
+    except (SolrException, socket.error) as e:
+        logger.error('could not delete sound with id %s (%s).' % (sound_id, e))
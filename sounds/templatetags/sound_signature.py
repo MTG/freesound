2490ce74e2d2b0d5d8a518496a2e20faef8f778e
Andres Ferraro <andres.ferraro@upf.edu>
2017-10-30 17:39:49 +0100

    Add placeholder filter for sound_signature
    issue #868

---
+++ sounds/templatetags/sound_signature.py
@@ -0,0 +1,18 @@
+import urlparse
+from django.urls import reverse
+from django.contrib.sites.models import Site
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='sound_signature_replace')
+def sound_signature_replace(value, sound):
+    domain = "https://%s" % Site.objects.get_current().domain
+    abs_url = urlparse.urljoin(domain, reverse('sound', args=[sound.user.username, sound.id]))
+
+    replace = [("$$SOUND_ID$$", str(sound.id)),
+            ("$$SOUND_URL$", abs_url)]
+    for placeholder, v in replace:
+        value = value.replace(placeholder, v)
+    return value

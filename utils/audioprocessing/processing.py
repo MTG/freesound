024b51c4e7a26f7404518b78fa30015d7d9e00fb
Andres Ferraro <andres.ferraro@upf.edu>
2017-10-30 16:03:01 +0100

    Change default conversion parameter of ffmpeg for 2 channels
    issue  #1004

--- utils/audioprocessing/processing.py
+++ utils/audioprocessing/processing.py
@@ -601,7 +601,7 @@ def convert_using_ffmpeg(input_filename, output_filename):
     if not os.path.exists(input_filename):
         raise AudioProcessingException("file %s does not exist" % input_filename)
 
-    command = ["ffmpeg", "-y", "-i", input_filename, "-ac","1","-acodec", "pcm_s16le", "-ar", "44100", output_filename]
+    command = ["ffmpeg", "-y", "-i", input_filename, "-ac","2","-acodec", "pcm_s16le", "-ar", "44100", output_filename]
 
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     signal.signal(signal.SIGALRM,alarm_handler)

b0e14e1a3b62bf60476894cc05f8d505dc08fc85
Alastair Porter <alastair.porter@upf.edu>
2017-10-31 17:40:33 +0100

    Style/pep8 cleanups for geotags

--- geotags/admin.py
+++ geotags/admin.py
@@ -21,11 +21,14 @@
 #
 
 from django.contrib import admin
+
 from geotags.models import GeoTag
+
 
 class GeoTagAdmin(admin.ModelAdmin):
     search_fields = ('=user__username',)
     raw_id_fields = ('user',) 
     list_display = ('user', 'lat', 'lon', 'created')
+
 
 admin.site.register(GeoTag, GeoTagAdmin)

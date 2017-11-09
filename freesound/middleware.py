MTG/freesound

commit 6965c6a254d7b4a5fc8d8e05c1a19d61bf1ff409

Remove unneeded PermissionDenied custom middleware

freesound/middleware.py
@@ -18,28 +18,13 @@
 #     See AUTHORS file.
 #
 
-from django.shortcuts import render
-from freesound_exceptions import PermissionDenied
-from utils.onlineusers import cache_online_users
-from django.contrib.auth.models import User
-from accounts.models import Profile
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 from django.conf import settings
 from django.core.cache import cache
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 from sounds.models import Sound
-
-
-class PermissionDeniedHandler(object):
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        return self.get_response(request)
-
-    def process_exception(self, request, exception):
-        if isinstance(exception, PermissionDenied):
-            return render(request, 'permissiondenied.html')
+from utils.onlineusers import cache_online_users
 
 
 class OnlineUsersHandler(object):
@@ -50,6 +35,7 @@ class OnlineUsersHandler(object):
         cache_online_users(request)
         response = self.get_response(request)
         return response
+
 
 class BulkChangeLicenseHandler(object):
     def __init__(self, get_response):
@@ -62,10 +48,10 @@ class BulkChangeLicenseHandler(object):
         # don't run it for media URLs
         # N.B. probably better just to check for login in the URL
         if request.user.is_authenticated \
-            and not 'bulklicensechange' in request.get_full_path() \
-            and not 'logout' in request.get_full_path() \
-            and not 'tosacceptance' in request.get_full_path() \
-            and not request.get_full_path().startswith(settings.MEDIA_URL):
+                and 'bulklicensechange' not in request.get_full_path() \
+                and 'logout' not in request.get_full_path() \
+                and 'tosacceptance' not in request.get_full_path() \
+                and request.get_full_path().startswith(settings.MEDIA_URL):
 
             user = request.user
             cache_key = "has-old-license-%s" % user.id
@@ -77,11 +63,9 @@ class BulkChangeLicenseHandler(object):
                 cache.set(cache_key, [has_old_license, has_sounds], 2592000) # 30 days cache
                 if has_old_license and has_sounds:
                     return HttpResponseRedirect(reverse("bulk-license-change"))
-
-            else :
+            else:
                 has_old_license = cache_info[0]
                 has_sounds = cache_info[1]
-                #print "CACHE LICENSE: has_old_license=" + str(has_old_license) + " has_sounds=" + str(has_sounds)
                 if has_old_license and has_sounds:
                     return HttpResponseRedirect(reverse("bulk-license-change"))
         response = self.get_response(request)
@@ -110,13 +94,13 @@ class TosAcceptanceHandler(object):
     def __call__(self, request):
 
         if request.user.is_authenticated \
-            and not 'tosacceptance' in request.get_full_path() \
-            and not 'logout' in request.get_full_path() \
-            and not 'tos_api' in request.get_full_path() \
-            and not 'tos_web' in request.get_full_path() \
-            and not 'contact' in request.get_full_path() \
-            and not 'bulklicensechange' in request.get_full_path() \
-            and not request.get_full_path().startswith(settings.MEDIA_URL):
+                and 'tosacceptance' not in request.get_full_path() \
+                and 'logout' not in request.get_full_path() \
+                and 'tos_api' not in request.get_full_path() \
+                and 'tos_web' not in request.get_full_path() \
+                and 'contact' not in request.get_full_path() \
+                and 'bulklicensechange' not in request.get_full_path() \
+                and request.get_full_path().startswith(settings.MEDIA_URL):
 
             user = request.user
             cache_key = "has-accepted-tos-%s" % user.id

freesound/settings.py
@@ -11,7 +11,6 @@ import logging.config
 DEBUG = False
 
 MIDDLEWARE = [
-    'freesound.middleware.PermissionDeniedHandler',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

templates/permissiondenied.html
@@ -1,2 +1,0 @@
-omg!
-permission denied!! :)

commit aa1596384a6c937311570d8cc76fb4c7daf21db0

Re-enable BulkChangeLicenseHandler and TosAcceptanceHandler middleware
Fix bug in middlewares introduced in https://github.com/MTG/freesound/commit/6965c6a254d7b4a5fc8d8e05c1a19d61bf1ff409

https://github.com/MTG/freesound/issues/1021

freesound/middleware.py
@@ -51,16 +51,16 @@ class BulkChangeLicenseHandler(object):
                 and 'bulklicensechange' not in request.get_full_path() \
                 and 'logout' not in request.get_full_path() \
                 and 'tosacceptance' not in request.get_full_path() \
-                and request.get_full_path().startswith(settings.MEDIA_URL):
+                and not request.get_full_path().startswith(settings.MEDIA_URL):
 
             user = request.user
             cache_key = "has-old-license-%s" % user.id
             cache_info = cache.get(cache_key)
 
-            if cache_info == None or 0 or not isinstance(cache_info, (list, tuple)):
+            if cache_info is None or 0 or not isinstance(cache_info, (list, tuple)):
                 has_old_license = user.profile.has_old_license
                 has_sounds = Sound.objects.filter(user=user).exists()
-                cache.set(cache_key, [has_old_license, has_sounds], 2592000) # 30 days cache
+                cache.set(cache_key, [has_old_license, has_sounds], 2592000)  # 30 days cache
                 if has_old_license and has_sounds:
                     return HttpResponseRedirect(reverse("bulk-license-change"))
             else:
@@ -101,7 +101,7 @@ class TosAcceptanceHandler(object):
                 and 'tos_web' not in request.get_full_path() \
                 and 'contact' not in request.get_full_path() \
                 and 'bulklicensechange' not in request.get_full_path() \
-                and request.get_full_path().startswith(settings.MEDIA_URL):
+                and not request.get_full_path().startswith(settings.MEDIA_URL):
 
             user = request.user
             cache_key = "has-accepted-tos-%s" % user.id
@@ -112,9 +112,10 @@ class TosAcceptanceHandler(object):
                 if not has_accepted_tos:
                     return HttpResponseRedirect(reverse("tos-acceptance"))
                 else:
-                    cache.set(cache_key, 'yes', 2592000) # 30 days cache
+                    cache.set(cache_key, 'yes', 2592000)  # 30 days cache
             else:
                 # If there is cache it means the terms has been accepted
                 pass
+
         response = self.get_response(request)
         return response

freesound/settings.py
@@ -17,6 +17,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'freesound.middleware.TosAcceptanceHandler',
+    'freesound.middleware.BulkChangeLicenseHandler',
     'freesound.middleware.OnlineUsersHandler',
     'corsheaders.middleware.CorsMiddleware',
     'freesound.middleware.FrontendPreferenceHandler',
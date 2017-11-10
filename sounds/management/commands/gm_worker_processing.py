31f6dc34caf14509b0f770ad610ea363aa8d7014
Frederic Font <frederic.font@gmail.com>
2017-11-10 12:52:13 +0100

    Use processing logger in gm_worker_processing command
    Also various code clean ups

--- sounds/management/commands/gm_worker_processing.py
+++ sounds/management/commands/gm_worker_processing.py
@@ -18,21 +18,24 @@
 #     See AUTHORS file.
 #
 
-import gearman, sys, traceback, json, time, os
-from django.core.management.base import BaseCommand
-from utils.audioprocessing.freesound_audio_processing import process
-from utils.audioprocessing.essentia_analysis import analyze
-from django.conf import settings
-from sounds.models import Sound, Pack
-from tickets.models import Ticket
-from optparse import make_option
-from psycopg2 import InterfaceError
-from django.db.utils import DatabaseError
-from django.db import connection
-from tickets import TICKET_STATUS_CLOSED
 import logging
 
-logger = logging.getLogger("gearman_worker_processing")
+import gearman
+import os
+import sys
+import traceback
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.db import connection
+from django.db.utils import DatabaseError
+from psycopg2 import InterfaceError
+
+from sounds.models import Sound
+from utils.audioprocessing.essentia_analysis import analyze
+from utils.audioprocessing.freesound_audio_processing import process
+
+logger = logging.getLogger("processing")
+
 
 class Command(BaseCommand):
     help = 'Run the sound processing worker'
@@ -44,7 +47,6 @@ class Command(BaseCommand):
             dest='queue',
             default='process_sound',
             help='Register this function (default: process_sound)')
-
 
     def write_stdout(self, msg):
         logger.info("[%d] %s" % (os.getpid(),msg))
@@ -70,48 +72,15 @@ class Command(BaseCommand):
         gm_worker.work()
         self.write_stdout('Ended work\n')
 
-
     def task_analyze_sound(self, gearman_worker, gearman_job):
         return self.task_process_x(gearman_worker, gearman_job, analyze)
-
 
     def task_process_sound(self, gearman_worker, gearman_job):
         return self.task_process_x(gearman_worker, gearman_job, process)
 
-    def task_create_pack_zip(self, gearmanworker, gearman_job):
-        # dirty hack: sleep 1 sec while the transaction finishes at the other end
-        time.sleep(1)
-        pack_id  = gearman_job.data
-        self.write_stdout("Processing pack with id %s\n" % pack_id)
-        # connection (max of 'intent' times)
-        intent = 3
-        while intent > 0:
-            try:
-                pack = Pack.objects.get(id=int(pack_id))
-                break
-            except (InterfaceError, DatabaseError):
-                try:
-                    connection.connection.close()
-                except:
-                    pass
-                    # Trick Django into creating a fresh connection on the next db use attempt
-                connection.connection = None
-                intent -= 1
-        if intent <= 0:
-            self.write_stdout("Problems while connecting to the database, could not reset the connection and will kill the worker.\n")
-            sys.exit(255)
-        try:
-            self.write_stdout("Found pack with id %d\n" % pack.id)
-            pack.create_zip()
-            self.write_stdout("Finished creating zip")
-        except Exception as e:
-            self.write_stdout("ERROR in zip creation: % \n"%str(e))
-        return 'true'
-    
     def task_process_x(self, gearman_worker, gearman_job, func):
         sound_id = gearman_job.data
         self.write_stdout("Processing sound with id %s\n" % sound_id)
-        success = True
         sound = False
         try:
             # If the database connection has become invalid, try to reset the
@@ -134,21 +103,18 @@ class Command(BaseCommand):
 
             # if we didn't succeed in resetting the connection, quit the worker
             if intent <= 0:
-                self.write_stdout("Problems while connecting to the database, could not reset the connection and will kill the worker.\n")
+                self.write_stdout("Problems while connecting to the database, could not reset the connection and "
+                                  "will kill the worker.\n")
                 sys.exit(255)
 
             result = func(sound)
             self.write_stdout("Finished, sound: %s, processing %s\n" % \
                               (sound_id, ("ok" if result else "failed")))
-            success = result
             return 'true' if result else 'false'
         except Sound.DoesNotExist:
             self.write_stdout("\t did not find sound with id: %s\n" % sound_id)
-            success = False
             return 'false'
         except Exception as e:
             self.write_stdout("\t something went terribly wrong: %s\n" % e)
             self.write_stdout("\t%s\n" % traceback.format_exc())
-            success = False
             return 'false'
-

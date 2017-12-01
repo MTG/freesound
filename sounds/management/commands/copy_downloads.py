e252f8a31f777d0ff5aaa03ade04a1522c3d3cd1
Andres Ferraro <andres.ferraro@upf.edu>
2017-12-01 18:11:31 +0100

    Change copy pack downloads script with optimizations

--- sounds/management/commands/copy_downloads.py
+++ sounds/management/commands/copy_downloads.py
@@ -23,6 +23,8 @@ import logging
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from sounds.models import Download, PackDownload, PackDownloadJson, PackDownloadSound
+from django.db import transaction
+
 
 logger = logging.getLogger("console")
 
@@ -49,26 +51,27 @@ class Command(BaseCommand):
         if last_downloads.count():
             start = last_downloads[0].created
             start = start.replace(hour=0, minute=0, second=0)
-            PackDownload.objects.filter(created__gt=start).delete()
-            PackDownloadJson.objects.filter(created__gt=start).delete()
         else:
             first_downloads = Download.objects.order_by('created')
             start = first_downloads.first().created
 
         more_downloads = True
         while start < datetime.datetime.now():
-            downloads = Download.objects.filter(pack_id__isnull=False, created__gte=start, created__lt=start+td)
+            downloads = Download.objects.filter(pack_id__isnull=False, created__gte=start, created__lt=start+td)\
+                    .prefetch_related('pack__sound_set')
             start += td
             more_downloads = downloads.count() != 0
-            for download in downloads.all():
-                # create both PackDownload and PackDownloadJson
-
-                sounds = []
-                pd = PackDownload.objects.create(user=download.user, created=download.created, pack_id=download.pack_id)
-                for sound in download.pack.sound_set.all():
-                    PackDownloadSound.objects.create(sound=sound, license=sound.license, pack_download=pd)
-                    sounds.append({'sound_id': sound.id, 'license_id': sound.license_id})
-                PackDownloadJson.objects.create(user=download.user, created=download.created, pack_id=download.pack_id, sounds=sounds)
+            with transaction.atomic():
+                for download in downloads.all():
+                    # create both PackDownload and PackDownloadJson
+                    sounds = []
+                    pd = PackDownload.objects.create(user=download.user, created=download.created, pack_id=download.pack_id)
+                    pds = []
+                    for sound in download.pack.sound_set.all():
+                        pds.append(PackDownloadSound(sound=sound, license=sound.license, pack_download=pd))
+                        sounds.append({'sound_id': sound.id, 'license_id': sound.license_id})
+                    PackDownloadSound.objects.bulk_create(pds, batch_size=1000)
+                    PackDownloadJson.objects.create(user=download.user, created=download.created, pack_id=download.pack_id, sounds=sounds)
 
             logger.info("Copy of Download for %d sounds of the date: %s " % (downloads.count(), start.strftime("%Y-%m-%d")))
         logger.info('Copy Downloads to new PackDownload finished')

fa03ee440863ecff9776e346f901a6909cc8bea1
Frederic Font <frederic.font@gmail.com>
2018-01-11 10:30:40 +0100

    Improve API clients info shown in admin and search fields

--- apiv2/admin.py
+++ apiv2/admin.py
@@ -26,8 +26,9 @@ from apiv2.models import ApiV2Client
 
 class ApiV2ClientAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)
-    search_fields = ('=user__username', )
+    search_fields = ('user__username', 'name')
     list_filter = ('status', )
-    list_display = ("user", "status", "allow_oauth_passoword_grant")
+    list_display = ("name", "url", "user", "status", "throttling_level")
+
 
 admin.site.register(ApiV2Client, ApiV2ClientAdmin)

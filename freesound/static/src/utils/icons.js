360336b1ebaf1277b2948423ced8adcbbe16c08c
giuband <giuseppe.bandiera@live.com>
2018-02-04 13:23:26 +0100

    Add util to correctly create nodes for icons

---
+++ freesound/static/src/utils/icons.js
@@ -0,0 +1,7 @@
+export const getIcon = iconName => {
+  // first get the icon plain text
+  const iconTextContent = require(`../../styles/assets/icons/${iconName}.svg`)
+  // then convert it to an svg node
+  const iconSvgNode = document.createRange().createContextualFragment(iconTextContent)
+  return iconSvgNode
+}

469890ff258c73ea120b91ed22b6beca0118ad3d
giuband <giuseppe.bandiera@live.com>
2018-02-11 18:14:21 +0100

    Improve behaviour with multiple dropdowns on page

--- freesound/static/src/components/dropdown.js
+++ freesound/static/src/components/dropdown.js
@@ -7,31 +7,35 @@ const closeOnClickAway = () => {
   if (toggle && dropdownOptions) {
     dropdownOptions.classList.remove('show');
     toggle.setAttribute('aria-expanded', 'false');
+    lastOpenedDropdown.toggle = undefined;
+    lastOpenedDropdown.dropdownOptions = undefined;
   }
   document.body.removeEventListener('click', closeOnClickAway);
 };
 
 const toggleExpandDropdown = toggle => {
   const dropdownContainer = toggle.closest('.dropdown');
-  const isDropdownExpanded = toggle.getAttribute('aria-expanded') === 'true';
+  const isDropdownExpanded = lastOpenedDropdown.toggle && lastOpenedDropdown.toggle === toggle;
+  const isSomeOtherDropdownExpanded =
+    lastOpenedDropdown.toggle && lastOpenedDropdown.toggle !== toggle;
+
+  const shouldCloseLastOpenedDropdown = isDropdownExpanded || isSomeOtherDropdownExpanded;
+  if (shouldCloseLastOpenedDropdown) {
+    closeOnClickAway();
+  }
   if (!dropdownContainer) {
     return;
   }
   const dropdownOptions = dropdownContainer.getElementsByClassName('dropdown-menu')[0];
-  if (dropdownOptions) {
-    if (isDropdownExpanded) {
-      dropdownOptions.classList.remove('show');
-      toggle.setAttribute('aria-expanded', 'false');
-    } else {
-      dropdownOptions.classList.add('show');
-      toggle.setAttribute('aria-expanded', 'true');
-      lastOpenedDropdown.toggle = toggle;
-      lastOpenedDropdown.dropdownOptions = dropdownOptions;
-      setTimeout(() => {
-        // attach the close listener asynchronously to avoid closing it immediately
-        document.body.addEventListener('click', closeOnClickAway);
-      }, 0);
-    }
+  if (dropdownOptions && !isDropdownExpanded) {
+    dropdownOptions.classList.add('show');
+    toggle.setAttribute('aria-expanded', 'true');
+    lastOpenedDropdown.toggle = toggle;
+    lastOpenedDropdown.dropdownOptions = dropdownOptions;
+    setTimeout(() => {
+      // attach the close listener asynchronously to avoid closing it immediately
+      document.body.addEventListener('click', closeOnClickAway);
+    }, 0);
   }
 };
 

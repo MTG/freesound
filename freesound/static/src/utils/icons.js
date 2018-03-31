33a77e203215926b4aff433d0dfa4161c0a09dc6
giuband <giuseppe.bandiera@live.com>
2018-03-31 18:10:02 +0200

    Improve look and behaviour of player

--- freesound/static/src/utils/icons.js
+++ freesound/static/src/utils/icons.js
@@ -1,7 +1,7 @@
 export const getIcon = iconName => {
   // first get the icon plain text
-  const iconTextContent = require(`../../styles/assets/icons/${iconName}.svg`)
+  const iconTextContent = require(`../../styles/assets/icons/${iconName}.svg`);
   // then convert it to an svg node
-  const iconSvgNode = document.createRange().createContextualFragment(iconTextContent)
-  return iconSvgNode
-}
+  const iconSvgNode = document.createRange().createContextualFragment(iconTextContent);
+  return iconSvgNode;
+};

eba62d234669dbdad321dee5a37c1767e9919dca
Frederic Font <frederic.font@gmail.com>
2018-04-16 16:19:10 +0200

    Make error message text more explicit
    https://github.com/MTG/freesound/pull/1093

--- utils/sound_upload.py
+++ utils/sound_upload.py
@@ -479,7 +479,7 @@ def bulk_describe_from_csv(csv_file_path, delete_already_existing=False, force_i
             if bulk_upload_progress_object:
                 bulk_upload_progress_object.store_progress_for_line(line['line_no'], message)
         except AlreadyExistsException:
-            message = 'l%i: The file %s is already part of freesound, not uploading it.' % (line['line_no'], dest_path,)
+            message = 'l%i: The file %s is already part of Freesound, discarding it.' % (line['line_no'], dest_path,)
             console_logger.info(message)
             if bulk_upload_progress_object:
                 bulk_upload_progress_object.store_progress_for_line(line['line_no'], message)

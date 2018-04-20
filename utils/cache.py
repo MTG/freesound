678df94206b2a4b23d022999152c0dd272ff2e98
Philip <phil.tgun@gmail.com>
2018-04-20 16:54:58 +0200

    Replace manual cache computation with django method

--- utils/cache.py
+++ utils/cache.py
@@ -19,18 +19,11 @@
 #
 
 from django.core.cache import cache
-from hashlib import md5
-from django.utils.http import urlquote
-from functional import compose
 from django.core.cache.utils import make_template_fragment_key
 
 
 def get_template_cache_key(fragment_name, *variables):
-    args = md5(u':'.join(map(compose(urlquote, unicode), variables)))
-    old = 'template.cache.%s.%s' % (fragment_name, args.hexdigest())
-    new = make_template_fragment_key(fragment_name, variables)
-    # TODO: check that it works for multiple variables
-    return old
+    return make_template_fragment_key(fragment_name, variables)
 
 
 def invalidate_template_cache(fragment_name, *variables):

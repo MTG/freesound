3a21a95baf1a288d60de1195fa563c67b845ecf7
Frederic Font <frederic.font@upf.edu>
2018-06-18 10:30:29 +0200

    Removed extra "<br>" which was accidentally committed
    https://github.com/MTG/freesound/pull/1093

--- sounds/forms.py
+++ sounds/forms.py
@@ -71,8 +71,7 @@ class SoundDescriptionForm(forms.Form):
     is_explicit = forms.BooleanField(required=False)
     tags = TagField(widget=forms.Textarea(attrs={'cols': 80, 'rows': 3}),
                     help_text="<br>Add at least 3 tags, separating them with spaces. Join multi-word tags with dashes. "
-                              "For example: <i>field-recording</i> is a popular tag."
-                              "<br>")
+                              "For example: <i>field-recording</i> is a popular tag.")
     description = HtmlCleaningCharField(widget=forms.Textarea(attrs={'cols': 80, 'rows': 10}))
 
     def __init__(self, *args, **kwargs):

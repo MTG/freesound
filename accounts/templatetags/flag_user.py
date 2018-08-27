5107b0d4e6f091f5d7aca486d8dc03b803cdbaf9
Frederic Font <frederic.font@upf.edu>
2018-08-27 11:47:56 +0200

    Add object previews in spam report emails
    Now spam email reports sent to admin will include a small preview of the reported object so that most of the times that a decision about what to do with the user can be taken from the email client without the need to inspect each reported object individually.
    
    This commit also includes some small refactorings to the user flagging code and adds unit tests.
    
    https://github.com/MTG/freesound/issues/1218

--- accounts/templatetags/flag_user.py
+++ accounts/templatetags/flag_user.py
@@ -19,11 +19,9 @@
 #
 
 from django import template
-from comments.models import Comment
-from forum.models import Post
-from messages.models import Message
 from accounts.models import UserFlag
 register = template.Library()
+
 
 @register.inclusion_tag("accounts/flag_user.html", takes_context=True)
 def flag_user(context, flag_type, username, content_id, text = None, user_sounds = None):
@@ -35,8 +33,18 @@ def flag_user(context, flag_type, username, content_id, text = None, user_sounds
         no_show = True
         flagged = []
     else:
-        flagged = UserFlag.objects.filter(user__username = username, reporting_user = context['request'].user, object_id = content_id).values('reporting_user').distinct()
+        flagged = UserFlag.objects.filter(user__username=username,
+                                          reporting_user=context['request'].user,
+                                          object_id=content_id).values('reporting_user').distinct()
         if text:
             link_text = text
 
-    return {'user_sounds':user_sounds,'done_text':"Marked as spam/offensive", 'flagged':len(flagged),'flag_type':flag_type,'username':username, 'content_obj_id':content_id, 'media_url': context['media_url'], 'link_text':link_text, 'no_show':no_show}
+    return {'user_sounds': user_sounds,
+            'done_text': "Marked as spam/offensive",
+            'flagged': len(flagged),
+            'flag_type': flag_type,
+            'username': username,
+            'content_obj_id': content_id,
+            'media_url': context['media_url'],
+            'link_text': link_text,
+            'no_show': no_show}

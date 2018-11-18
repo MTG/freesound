78754ee3d58a06fef030ce6af2cb1eb34486a26c
giuband <giuseppe.bandiera@live.com>
2018-11-18 12:54:04 +0100

    Use parcel instead of webpack

---
+++ freesound/static/src/index.js
@@ -0,0 +1,8 @@
+// polyfills and css resets
+import 'babel-polyfill'
+import 'whatwg-fetch'
+import 'classlist-polyfill'
+import 'element-closest'
+import 'normalize.css'
+
+import '../styles/index.scss'

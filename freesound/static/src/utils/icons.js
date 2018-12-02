d60be07b8774c9c67f79a98ebe2f9ff0cc270c39
giuband <giuseppe.bandiera@live.com>
2018-12-02 18:35:47 +0100

    Fully interactive front page

--- freesound/static/src/utils/icons.js
+++ freesound/static/src/utils/icons.js
@@ -1,7 +1,15 @@
 export const getIcon = iconName => {
   // first get the icon plain text
-  const iconTextContent = require(`../../styles/assets/icons/${iconName}.svg`);
+  const iconTextContent = require(`../../styles/assets/icons/${iconName}.svg`)
   // then convert it to an svg node
-  const iconSvgNode = document.createRange().createContextualFragment(iconTextContent);
-  return iconSvgNode;
-};
+  const iconSvgNode = document
+    .createRange()
+    .createContextualFragment(iconTextContent)
+  return iconSvgNode
+}
+
+export const createIconElement = iconName => {
+  const node = document.createElement('i')
+  node.classList.add(iconName)
+  return node
+}

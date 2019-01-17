5c053bd096123371efad618d028baea9f097d7e5
xavierfav <xavier.favory@gmail.com>
2019-01-17 13:22:13 +0100

    Add wrapper utility clustering

---
+++ utils/clustering_utilities.py
@@ -0,0 +1,42 @@
+import traceback, logging
+from django.conf import settings
+from django.core.cache import cache
+from clustering.client import Clustering
+from utils.encryption import create_hash
+
+
+logger = logging.getLogger('web')
+
+
+def get_clusters(sound_ids, query=None, filter=None):
+
+    cache_key = 'cluster-sounds-q-%s-f-%s' % (str(query).replace(" ", ""), str(filter).replace(" ", ""))
+    cache_key = hash_cache_key(cache_key)
+
+    # Don't use the cache when we're debugging
+    if settings.DEBUG or len(cache_key) >= 250 or in_ids:
+        returned_sounds = False
+    else:
+        result = cache.get(cache_key)
+        if result:
+            returned_sounds = result
+        else:
+            returned_sounds = False
+
+    if not returned_sounds:
+        result = Clustering.cluster_points(
+            query=query,
+            sound_ids=sound_ids,
+        )
+
+        returned_sounds = result
+        num_clusters = max(result.values()) + 1
+
+        if len(returned_sounds) > 0 and len(cache_key) < 250 and not settings.DEBUG:
+            cache.set(cache_key, result, SIMILARITY_CACHE_TIME)
+
+    return returned_sounds, num_clusters
+
+
+def hash_cache_key(key):
+    return create_hash(key, add_secret=False, limit=32)

6d637e9d2873594f71fb6e18f7ad84536b280d02
Frederic Font <frederic.font@upf.edu>
2019-03-21 19:35:28 +0100

    Remove unneeded donations_cache from front_page tvars

--- sounds/views.py
+++ sounds/views.py
@@ -187,8 +187,8 @@ def get_current_thread_ids():
 
 
 def front_page(request):
+
     rss_cache = cache.get("rss_cache_bw" if using_beastwhoosh(request) else "rss_cache", None)
-    donations_cache = cache.get("donations_cache", None)
     current_forum_threads = Thread.objects.filter(pk__in=get_current_thread_ids(),
                                                   first_post__moderation_state="OK",
                                                   last_post__moderation_state="OK") \
@@ -223,7 +223,6 @@ def front_page(request):
 
     tvars = {
         'rss_cache': rss_cache,
-        'donations_cache': donations_cache,
         'current_forum_threads': current_forum_threads,
         'latest_additions': latest_additions,
         'random_sound': random_sound,

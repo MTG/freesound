78786e6ce0c6a6c8d51b0a78e97dfaf494b7333d
Frederic Font <frederic.font@upf.edu>
2019-04-30 14:04:21 +0200

    Fix typo

--- general/management/commands/create_front_page_caches.py
+++ general/management/commands/create_front_page_caches.py
@@ -51,7 +51,7 @@ class Command(BaseCommand):
         # TODO: we still don't know how to handle multiple news entries in BW, currently only the latest will be shown
 
         # Generate popular searches cache
-        popular_searches = ['field-recording', 'ambiene', 'voice', 'loop']
+        popular_searches = ['field-recording', 'ambience', 'voice', 'loop']
         cache.set("popular_searches", popular_searches,  cache_time)
 
         # TODO: we have to decide how do we determine "trending searches" and how often these are updated. Depending on

e917b58889af92ae4fac41a869f739e9d8bfa81f
xavierfav <xavier.favory@gmail.com>
2019-09-05 20:06:50 +0200

    Download swig from github Travis CI

--- travis/install_gaia.sh
+++ travis/install_gaia.sh
@@ -3,10 +3,10 @@
 echo "> Installing swig 3.0.12..."
 # Install newer version of swig (gaia fails with swig 3.0.8)
 sudo apt-get install libpcre3-dev
-wget -O swig-3.0.12.tar.gz https://downloads.sourceforge.net/project/swig/swig/swig-3.0.12/swig-3.0.12.tar.gz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fswig%2Ffiles%2Fswig%2Fswig-3.0.12%2Fswig-3.0.12.tar.gz%2Fdownload&ts=1486782132&use_mirror=superb-sea2
+wget -O swig-3.0.12.tar.gz https://github.com/swig/swig/archive/rel-3.0.12.tar.gz
 tar xf swig-3.0.12.tar.gz
-cd swig-3.0.12
-./configure --prefix=/usr
+cd swig-rel-3.0.12
+./configure
 make -j 4
 sudo make install
 cd ..

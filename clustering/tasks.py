498cf5ebefe77453dd3b4e608d6f1ede84d4c162
xavierfav <xavier.favory@gmail.com>
2019-10-09 17:16:50 +0200

    Add docstring cluster sounds task

--- clustering/tasks.py
+++ clustering/tasks.py
@@ -10,6 +10,16 @@ logger = logging.getLogger('clustering')
 
 @task(name="cluster_sounds")
 def cluster_sounds(cache_key_hashed, sound_ids, features):
+    """ Triggers the clustering of the sounds given as argument with the specified features.
+
+    This is the task that is used for clustering the sounds of a search result asynchronously with Celery.
+    The clustering result is stored in cache using the hashed cache key built with the query parameters.
+
+    Args:
+        cache_key_hashed (str): hashed key for storing/retrieving the results in cache.
+        sound_ids (str): string containing comma-separated sound ids.
+        features (str): name of the features used for clustering the sounds (defined in the clustering settings file).
+    """
     # This ensures that the engine is imported after it is re-assigned in __init__.py
     # There should be a better way to do it to avoid multiple imports that can decrease performance
     from . import engine

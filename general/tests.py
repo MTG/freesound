12c461a195ddbe983e2e350a54028e7529d120ee
ffont <frederic.font@gmail.com>
2020-10-19 16:46:31 +0200

    Properly handle non-ascii chars in paginator GET parameter names
    Fixes #1491

--- general/tests.py
+++ general/tests.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 #
 # Freesound is (c) MUSIC TECHNOLOGY GROUP, UNIVERSITAT POMPEU FABRA
 #
@@ -19,10 +21,14 @@
 #
 
 from django.core.management import call_command
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
 
 from forum.models import Thread, Post, Forum
+from general.templatetags.paginator import show_paginator
 from ratings.models import SoundRating
+from sounds.models import Sound
+from utils.pagination import paginate
 from utils.test_helpers import create_user_and_sounds
 
 
@@ -123,3 +129,24 @@ class ReportCountStatusesManagementCommandTestCase(TestCase):
         self.assertEqual(sound.avg_rating, 4)
         self.assertEqual(sound.num_comments, 1)
         self.assertEqual(sound.num_downloads, 0)
+
+
+class PaginatorTestCase(TestCase):
+
+    def test_url_with_non_ascii_characters(self):
+        """Paginator objects are passed a request object which includes a list of request GET parameters and values.
+        The paginator uses this object to get parameters like the current page that is requested, and also to construct
+        pagination links which contain all the same GET parameters as the initial request so that whatever things
+        are determined there, will be preserved when moving to the next page. To do that the paginator iterates over
+        all GET parameters and values. This test checks that if non-ascii characters are passed as GET parameter names
+        or values, paginator does not break.
+        """
+        context = {'media_url': 'fake URL'}
+        text_with_non_ascii = u'�textèé'
+        dummy_request = RequestFactory().get(reverse('sounds'), {
+            text_with_non_ascii: '1',
+            'param_name': text_with_non_ascii,
+            'param2_name': 'ok_value',
+        })
+        paginator = paginate(dummy_request, Sound.objects.all(), 10)
+        show_paginator(context, paginator['paginator'], paginator['page'], paginator['current_page'], dummy_request)

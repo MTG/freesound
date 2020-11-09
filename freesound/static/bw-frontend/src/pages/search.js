1734677adf3f4e0cc7ac913b1afacaa8b0b9f2eb
Genis Diaz <genisdf89@gmail.com>
2020-11-09 09:36:06 +0100

    add functionality to query search

--- freesound/static/bw-frontend/src/pages/search.js
+++ freesound/static/bw-frontend/src/pages/search.js
@@ -1,1 +1,18 @@
-import './page-polyfills'
+import './page-polyfills';
+
+const searchInputBrowse = document.getElementById('search-input-browse');
+const removeSearchInputValueBrowse = document.getElementById('remove-content-search');
+
+const searchInputChange = event => {
+  if (event.target.value.length) {
+    removeSearchInputValueBrowse.style.opacity = 0.5;
+  }
+};
+
+const removeSearchQuery = () => {
+  searchInputBrowse.value = '';
+  removeSearchInputValueBrowse.style.opacity = 0;
+};
+
+searchInputBrowse.addEventListener('change', searchInputChange);
+removeSearchInputValueBrowse.addEventListener('click', removeSearchQuery);

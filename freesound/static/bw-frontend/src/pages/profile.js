d8d8d0d74d5df3ad818a0ef41352c8a407e9f3d2
Genis Diaz <genisdf89@gmail.com>
2020-11-18 16:36:11 +0100

    handle taps

--- freesound/static/bw-frontend/src/pages/profile.js
+++ freesound/static/bw-frontend/src/pages/profile.js
@@ -1,13 +1,20 @@
 const taps = [...document.querySelectorAll('[data-toggle="tap"]')];
+const tapsElements = document.getElementsByClassName('bw-profile__tap_container');
 
 const cleanActiveClass = () => {
   taps.forEach(tap => tap.classList.remove('active'));
+  tapsElements.forEach(tapElement =>
+    tapElement.classList.remove('bw-profile__tap_container__active')
+  );
 };
 
 const handleTap = tap => {
   cleanActiveClass();
-  console.log('handleTap', tap);
+
+  const tapContainer = document.getElementById(tap.dataset.target.substring(1));
+
   tap.classList.add('active');
+  tapContainer.classList.add('bw-profile__tap_container__active');
 };
 
 taps.forEach(tap => {

a20af6d6305456c2e5e11304c7a1388d67d2fdde
ffont <frederic.font@gmail.com>
2020-11-26 16:49:27 +0100

    Port BW middle-sized player
    This is the player used in search results page and in other freesound pages with sound lists

--- general/templatetags/bw_templatetags.py
+++ general/templatetags/bw_templatetags.py
@@ -19,13 +19,62 @@
 #
 
 from django import template
+from django.conf import settings
+from django.urls import reverse
 
 register = template.Library()
 
 
-@register.inclusion_tag('atoms/icons.html')
+@register.inclusion_tag('atoms/icon.html')
 def bw_icon(name, class_name=''):
     """
     Displays a Beast Whoosh icon with the given name
     """
     return {'name': name, 'class_name': class_name}
+
+
+@register.inclusion_tag('atoms/tag.html')
+def bw_tag(tag_name, size=1, class_name=""):
+    """
+    Displays a Beast Whoosh tag with the given name
+    """
+    url = reverse('tags', args=[tag_name])
+    return {'tag_name': tag_name, 'size': size, 'class_name': class_name, 'url': url}
+
+@register.inclusion_tag('atoms/stars.html', takes_context=True)
+def bw_sound_stars(context):
+    sound = context['sound']
+    if hasattr(sound, 'username'):
+        sound_user = sound.username
+    else:
+        sound_user = sound.user.username
+    request = context['request']
+    request_user = request.user.username
+    is_authenticated = request.user.is_authenticated
+
+    if sound.num_ratings >= settings.MIN_NUMBER_RATINGS:
+        sound_avg_rating = sound.avg_rating
+    else:
+        sound_avg_rating = 0
+
+    # Pre process rating values to do less work in the template
+    stars_10 = []
+    for i in range(0, 10):
+        if sound_avg_rating >= i + 1:
+            stars_10.append(True)
+        else:
+            stars_10.append(False)
+    stars_5 = []
+    for i in range(0, 10, 2):
+        if stars_10[i] and stars_10[i + 1]:
+            stars_5.append('full')
+        elif not stars_10[i] and not stars_10[i + 1]:
+            stars_5.append('empty')
+        else:
+            stars_5.append('half')
+
+    return {'sound_user': sound_user,
+            'request_user': request_user,
+            'is_authenticated': is_authenticated,
+            'sound': sound,
+            'stars_5': stars_5}

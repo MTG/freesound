e0763711e979192767bcb89ae01aca897fdb4180
ffont <frederic.font@gmail.com>
2020-11-27 10:31:29 +0100

    Refactor BW navbars
    Now there are 3 versions of the navbar as defined in the original designs:
    
    - The default version has the waveform logo and the search input visible from a start.
    
    - The front page version has the waveform+text logo visible from a start and the search input hidden.
    
    - The search page version has the waveform logo visible from a start and the search input hidden, but sets the "value" of the input to the current query

--- freesound/static/bw-frontend/src/pages/search.js
+++ freesound/static/bw-frontend/src/pages/search.js
@@ -1,5 +1,8 @@
 import './page-polyfills';
+import throttle from "lodash.throttle";
+import navbar from "../components/navbar";
 
+// Main search input box behaviour
 const searchInputBrowse = document.getElementById('search-input-browse');
 const removeSearchInputValueBrowse = document.getElementById('remove-content-search');
 
@@ -18,3 +21,20 @@ const removeSearchQuery = () => {
 
 searchInputBrowse.addEventListener('input', searchInputChange);
 removeSearchInputValueBrowse.addEventListener('click', removeSearchQuery);
+
+// Navbar search input box behaviour (shouold only appear when searchInputBrowse is not visible)
+const SCROLL_CHECK_TIMER = 100 // min interval (in ms) between consecutive calls of scroll checking function
+const checkShouldShowSearchInNavbar = throttle(() => {
+  const heroRect = searchInputBrowse.getBoundingClientRect()
+  // not all browsers support clientRect.height
+  const heroSearchPosition = heroRect.height
+    ? heroRect.y + heroRect.height
+    : heroRect.y
+  const shouldShowSearchBar = heroSearchPosition < 80
+  const isShowingSearchBar = !navbar.classList.contains('bw-nav--expanded')
+  if (shouldShowSearchBar !== isShowingSearchBar) {
+    navbar.classList.toggle('bw-nav--expanded')
+  }
+}, SCROLL_CHECK_TIMER)
+
+window.addEventListener('scroll', checkShouldShowSearchInNavbar)

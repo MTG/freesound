95e414da5068a83b4b4a762c29950e92418afc95
ffont <frederic.font@gmail.com>
2020-12-02 10:26:08 +0100

    Center search input text
    Also improve placeholder behaviour so it disappears as soon as search input element gets focus, and make "remove query" action add focus to search input

--- freesound/static/bw-frontend/src/pages/search.js
+++ freesound/static/bw-frontend/src/pages/search.js
@@ -4,23 +4,41 @@ import navbar from "../components/navbar";
 
 // Main search input box behaviour
 const searchInputBrowse = document.getElementById('search-input-browse');
+const searchInputBrowsePlaceholder = searchInputBrowse.getAttribute("placeholder");
 const removeSearchInputValueBrowse = document.getElementById('remove-content-search');
 
-const searchInputChange = event => {
-  if (event.target.value.length) {
+
+const updateRemoveSearchInputButtonVisibility = (searchInputElement) => {
+  if (searchInputElement.value.length) {
     removeSearchInputValueBrowse.style.opacity = 0.5;
   } else {
-    removeSearchQuery();
+    removeSearchInputValueBrowse.style.opacity = 0.0;
   }
-};
+}
+
+const updateSearchInput = (event) => {
+  var element = event.target;
+  if (element === document.activeElement){  // Element has focus
+    // Remove placeholder attribute so it does not show under the blinking caret when no text has been entered yet
+    element.setAttribute('placeholder', '');
+    updateRemoveSearchInputButtonVisibility(element);
+  } else {  // Element has no focus
+    // Set placeholder attribute back to the original
+    element.setAttribute('placeholder', searchInputBrowsePlaceholder);
+  }
+}
 
 const removeSearchQuery = () => {
-  searchInputBrowse.value = '';
   removeSearchInputValueBrowse.style.opacity = 0;
+  searchInputBrowse.value = '';
+  searchInputBrowse.focus();
 };
 
-searchInputBrowse.addEventListener('input', searchInputChange);
 removeSearchInputValueBrowse.addEventListener('click', removeSearchQuery);
+searchInputBrowse.addEventListener('input', updateSearchInput);
+searchInputBrowse.addEventListener('focus', updateSearchInput);
+searchInputBrowse.addEventListener('blur', updateSearchInput);
+window.addEventListener('load', function(){updateRemoveSearchInputButtonVisibility(searchInputBrowse)})
 
 // Navbar search input box behaviour (shouold only appear when searchInputBrowse is not visible)
 const SCROLL_CHECK_TIMER = 100 // min interval (in ms) between consecutive calls of scroll checking function

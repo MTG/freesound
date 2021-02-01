5d2268b4b9ddcd054ac2cebbb2cfbc2b94d8c3c7
Genis Diaz <genisdf89@gmail.com>
2021-02-01 18:08:45 +0100

    textarea counter

--- freesound/static/bw-frontend/src/pages/editProfile.js
+++ freesound/static/bw-frontend/src/pages/editProfile.js
@@ -1,7 +1,7 @@
+const textareaInputElement = document.getElementById('about-edit-profile-textarea');
 const fileInputElement = document.getElementById('file');
 
 const readURL = input => {
-  console.log('readURL', input);
   if (input.files && input.files[0]) {
     var reader = new FileReader();
 
@@ -13,4 +13,9 @@ const readURL = input => {
   }
 };
 
+const textareaCounter = value => {
+  document.getElementById('counter-number').innerText = value.length;
+};
+
 fileInputElement.addEventListener('change', e => readURL(e.target));
+textareaInputElement.addEventListener('keyup', e => textareaCounter(e.target.value));

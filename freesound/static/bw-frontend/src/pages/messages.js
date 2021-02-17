ccb35fabad992715d46bedf49ce350c7f1f876fa
Genis Diaz <genisdf89@gmail.com>
2021-02-17 17:25:37 +0100

    select all with actions handling

--- freesound/static/bw-frontend/src/pages/messages.js
+++ freesound/static/bw-frontend/src/pages/messages.js
@@ -1,11 +1,36 @@
 const CheckboxGetAllElement = document.getElementById('selectAll');
+const messageCheckboxes = document.getElementsByClassName('message-checkbox');
+const checkboxesActions = document.getElementsByClassName('checked-action');
 
 const handleAllCheckboxes = () => {
   const allCheckboxes = document.getElementsByClassName('message-checkbox');
 
-  allCheckboxes.forEach(
-    checkboxElement => (checkboxElement.checked = CheckboxGetAllElement.checked)
+  allCheckboxes.forEach(checkboxElement => {
+    checkboxElement.checked = CheckboxGetAllElement.checked;
+    handleActions(CheckboxGetAllElement.checked);
+  });
+};
+
+const handleMessageCheckboxes = checkboxElement => {
+  let isAnyCheckboxChecked = false;
+
+  messageCheckboxes.forEach(checkbox => {
+    if (checkbox.checked) {
+      isAnyCheckboxChecked = true;
+    }
+  });
+
+  handleActions(isAnyCheckboxChecked);
+};
+
+const handleActions = show => {
+  checkboxesActions.forEach(
+    checkboxesActions => (checkboxesActions.style.display = show ? 'flex' : 'none')
   );
 };
+
+messageCheckboxes.forEach(checkbox =>
+  checkbox.addEventListener('change', () => handleMessageCheckboxes(checkbox))
+);
 
 CheckboxGetAllElement.addEventListener('change', handleAllCheckboxes);

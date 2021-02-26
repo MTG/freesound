ee93c5c477c1518a50a5acbf448b6d476bdbf759
ffont <frederic.font@gmail.com>
2021-02-26 10:53:24 +0100

    Remove log statement and add comment to addVisibleCheckbox

--- freesound/static/bw-frontend/src/components/checkbox.js
+++ freesound/static/bw-frontend/src/components/checkbox.js
@@ -2,9 +2,9 @@ const addVisibleCheckbox = (checkboxEl) => {
   const parent = checkboxEl.parentNode
   const actualCheckboxIndex = [...parent.children].findIndex(el => [...el.classList].includes('bw-checkbox'))
   const insertBeforeElement = [...parent.children][actualCheckboxIndex + 1]
-  console.log(insertBeforeElement)
 
   if (!insertBeforeElement.classList.contains('bw-checkbox-container')){
+    // Only add the visible checkbox element if it has not yet been added before
     const visibleCheckboxContainer = document.createElement('span')
     visibleCheckboxContainer.className = 'bw-checkbox-container'
     parent.insertBefore(visibleCheckboxContainer, insertBeforeElement)
@@ -12,8 +12,7 @@ const addVisibleCheckbox = (checkboxEl) => {
     checkboxIcon.className = 'bw-icon-checkbox'
     visibleCheckboxContainer.append(checkboxIcon)
   }
-
-}
+};
 
 const addCheckboxVisibleElements = () => {
   const checkboxes = [...document.getElementsByClassName('bw-checkbox')];

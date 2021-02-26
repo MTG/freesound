06e7fbb527e3cf50b92095e512bcda94277cba33
ffont <frederic.font@gmail.com>
2021-02-26 14:13:29 +0100

    Don't stop execution if recaptcha script is not loaded

--- freesound/static/bw-frontend/src/components/recaptcha.js
+++ freesound/static/bw-frontend/src/components/recaptcha.js
@@ -1,12 +1,17 @@
 const initRecaptchaWidgets = () => {
     const recaptchaElements = document.getElementsByClassName('g-recaptcha');
     recaptchaElements.forEach(element => {
-        const recaptchaKey = element.dataset.sitekey;
-        // eslint-disable-next-line no-undef
-        grecaptcha.render( element, {
-            'sitekey' : recaptchaKey,
-            'theme' : 'light',
-        });
+        try {
+            // Wrap this call in try statement so that if recaptcha script has not loaded it does not stop execution
+            const recaptchaKey = element.dataset.sitekey;
+            // eslint-disable-next-line no-undef
+            grecaptcha.render( element, {
+                'sitekey' : recaptchaKey,
+                'theme' : 'light',
+            });
+        } catch (e) {
+            // Do nothing...
+        }
     });
 };
 

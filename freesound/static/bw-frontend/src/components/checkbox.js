835d13958a5df2667e6c562ac2194f343780cbe5
ffont <frederic.font@gmail.com>
2021-03-31 13:58:09 +0200

    Improve format of BW checkboxes

--- freesound/static/bw-frontend/src/components/checkbox.js
+++ freesound/static/bw-frontend/src/components/checkbox.js
@@ -1,4 +1,17 @@
 const addVisibleCheckbox = (checkboxEl) => {
+  // Check if checkbox has a label element and re-order them
+  // We need to do this because Django does not provide an easy way to order checkbox/labels differently than
+  // the default
+  const labelEl = checkboxEl.previousElementSibling;
+  if ((labelEl) && (labelEl.tagName === 'LABEL')){
+      const wrapperEl = document.createElement('div');
+      checkboxEl.parentNode.insertBefore(wrapperEl, checkboxEl);
+      wrapperEl.appendChild(checkboxEl);
+      wrapperEl.appendChild(labelEl);
+      labelEl.classList.add('input-checkbox-label-default');
+  }
+
+  // Create the visible version of checkbox element
   const parent = checkboxEl.parentNode
   const actualCheckboxIndex = [...parent.children].findIndex(el => [...el.classList].includes('bw-checkbox'))
   const insertBeforeElement = [...parent.children][actualCheckboxIndex + 1]

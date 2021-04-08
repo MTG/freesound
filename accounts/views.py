e7b6d6ed750decff7aedd96dde49b30b22033f5f
ffont <frederic.font@gmail.com>
2021-04-08 10:12:23 +0200

    Show warning message if avatar has low resolution for BW

--- accounts/views.py
+++ accounts/views.py
@@ -529,6 +529,9 @@ def edit(request):
         image_form = AvatarForm(prefix="image")
 
     has_granted_permissions = AccessToken.objects.filter(user=request.user).count()
+    has_old_avatar = not os.path.exists(profile.locations('avatar.XL.path')) \
+                     or os.path.getsize(profile.locations('avatar.XL.path')) == \
+                     os.path.getsize(profile.locations('avatar.L.path'))
 
     tvars = {
         'user': request.user,
@@ -536,6 +539,7 @@ def edit(request):
         'profile_form': profile_form,
         'image_form': image_form,
         'has_granted_permissions': has_granted_permissions,
+        'has_old_avatar': has_old_avatar,
         'activePage': 'profile',  # For BW account settings sidebar
     }
     return render(request, 'accounts/edit.html', tvars)

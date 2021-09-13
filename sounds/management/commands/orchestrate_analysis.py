e836a75354a411a596d2707d791d4e6349206b7e
gonznm <gonznm@gmail.com>
2021-09-13 19:59:01 +0200

    Updating sound analysis counts

--- sounds/management/commands/orchestrate_analysis.py
+++ sounds/management/commands/orchestrate_analysis.py
@@ -43,7 +43,7 @@ class Command(BaseCommand):
         parser.add_argument(
             '--dry_run',
             action="store_true",
-            help='Flag to print the main status of the analysis. It does not trigger any analysis.')
+            help='Flag to only print the main status of the analysis. It does not trigger any analysis.')
 
     def handle(self, *args, **options):
         # Read config json
@@ -51,33 +51,37 @@ class Command(BaseCommand):
             config_file = json.load(json_file)
 
         # Print information about the already analyzed sounds if the dry_run flag exists
-        n_analyzed = SoundAnalysis.objects.all().count()
+        ok_total = SoundAnalysis.objects.filter(analysis_status="OK").count()
+        sk_total = SoundAnalysis.objects.filter(analysis_status="SK").count()
+        fa_total = SoundAnalysis.objects.filter(analysis_status="FA").count()
         n_sounds = Sound.objects.all().count()
         n_analyzers = len(config_file['analyzers'])
-        total2analyze = n_sounds * n_analyzers
+        missing_total = (n_sounds * n_analyzers)-(ok_total+sk_total+fa_total)
         # print headers of columns
         console_logger.info("{: >44} {: >11} {: >11} {: >11} {: >11}".format(
             *['analyzer name |', '# ok |', '# failed |', '# skipped |', '# missing']))
         # print row with total numbers
         console_logger.info("{: >44} {: >11} {: >11} {: >11} {: >11}".format(
-            *['total |', '{0} |'.format(n_analyzed), '0 |', '0 |', total2analyze]))
+            *['total |', '{0} |'.format(ok_total), '{0} |'.format(sk_total), '{0} |'.format(fa_total), missing_total]))
 
         # Count number of analysis done with each analyzer
         for a in config_file['analyzers']:
             analyzer, version = a.split(":")
-            analyzed = SoundAnalysis.objects.filter(analyzer=analyzer, analyzer_version=version).count()
-            missing = n_sounds-analyzed
+            ok = SoundAnalysis.objects.filter(analyzer=analyzer, analyzer_version=version, analysis_status="OK").count()
+            sk = SoundAnalysis.objects.filter(analyzer=analyzer, analyzer_version=version, analysis_status="SK").count()
+            fa = SoundAnalysis.objects.filter(analyzer=analyzer, analyzer_version=version, analysis_status="FA").count()
+            missing = n_sounds-(ok+sk+fa)
             # print row with more info
             console_logger.info("{: >44} {: >11} {: >11} {: >11} {: >11}".format(
-                *[a+' |', '{0} |'.format(analyzed), '0 |', '0 |', missing]))
+                *[a+' |', '{0} |'.format(ok), '{0} |'.format(sk), '{0} |'.format(fa), missing]))
 
         if not options['dry_run']:
             console_logger.info("Analysis configuration file: {0}".format(config_file))
             for a in config_file['analyzers']:
                 # Check all sounds available
                 for s in Sound.objects.all():
+                    analyzer, version = a.split("_")
                     # if the combination sound-analyzer-version does not exist, trigger analysis
-                    analyzer, version = a.split(":")
                     if not SoundAnalysis.objects.filter(sound=s, analyzer=analyzer, analyzer_version=version).exists():
                         console_logger.info(
                             "Triggering analysis of sound {0} with analyzer {1}.".format(s.id, analyzer))

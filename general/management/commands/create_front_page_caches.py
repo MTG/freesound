42a0428477fee2ae7226d9a5aeb81ce8cc23327f
ffont <frederic.font@gmail.com>
2021-11-30 12:35:01 +0100

    Add caches to BW front paghe to optimize num queries

--- general/management/commands/create_front_page_caches.py
+++ general/management/commands/create_front_page_caches.py
@@ -21,12 +21,16 @@
 import json
 import logging
 import time
+from collections import defaultdict
 
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.template.loader import render_to_string
 from django.core.cache import cache
 
+from donations.models import Donation
 from sounds.models import Download, Pack, Sound
+from sounds.views import get_n_weeks_back_datetime
 from utils.management_commands import LoggingBaseCommand
 
 commands_logger = logging.getLogger("commands")
@@ -77,5 +81,20 @@ class Command(LoggingBaseCommand):
         # Add total number of sounds in Freesound to the cache
         total_num_sounds = Sound.public.all().count()
         cache.set("total_num_sounds", total_num_sounds, cache_time)
+
+        # Calculate top donor
+        last_week = get_n_weeks_back_datetime(n_weeks=1)
+        top_donor_data = defaultdict(int)
+        for username, amount in \
+                Donation.objects.filter(created__gt=last_week) \
+                        .exclude(user=None, is_anonymous=True) \
+                        .values_list('user__username', 'amount'):
+            top_donor_data[username] += amount
+        if top_donor_data:
+            top_donor_username = sorted(top_donor_data.items(), key=lambda x: x[1], reverse=True)[0][0]
+            top_donor = User.objects.get(username=top_donor_username)
+            cache.set("top_donor_user_id", top_donor.id, cache_time)
+        else:
+            cache.set("top_donor_user_id", None, cache_time)
 
         self.log_end()

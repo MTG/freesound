5e08270d9cfb4ffe12215ce439b7f3e672e3101e
ffont <frederic.font@gmail.com>
2021-12-09 19:56:52 +0100

    WIP porting wiki to BW

--- wiki/views.py
+++ wiki/views.py
@@ -19,9 +19,10 @@
 #
 
 from django.http import HttpResponseRedirect, Http404
-from django.shortcuts import render
+
 from django.urls import reverse
 
+from utils.frontend_handling import render
 from wiki.models import Content, Page
 from wiki.forms import ContentForm
 

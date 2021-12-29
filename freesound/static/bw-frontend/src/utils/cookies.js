5073820e17727420b45c79df977fedde0f65395d
ffont <frederic.font@upf.edu>
2021-12-29 11:21:56 +0100

    Port cookie conset to BW

---
+++ freesound/static/bw-frontend/src/utils/cookies.js
@@ -0,0 +1,25 @@
+export const getCookie = (name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (`${name  }=`)) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+export const setCookie = (name, value, expiresInDays) => {
+    var expires = "";
+    if (expiresInDays) {
+        var date = new Date();
+        date.setTime(date.getTime() + (expiresInDays*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
--- freesound/static/bw-frontend/src/utils/postRequest.js
+++ freesound/static/bw-frontend/src/utils/postRequest.js
@@ -1,20 +1,4 @@
-
-
-const getCookie = (name) => {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (`${name  }=`)) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
+import { getCookie } from "./cookies";
 
 const makePostRequest = (url, data, successCallback, errorCallback) => {
     const csrftoken = getCookie('csrftoken');

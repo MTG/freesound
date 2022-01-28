3c0a0d05302554904c4e901ba8ba2aa262492bd7
ffont <frederic.font@gmail.com>
2022-01-28 16:03:03 +0100

    Make tests work again after search refactor

--- search/tests.py
+++ search/tests.py
@@ -21,70 +21,25 @@
 from django.test import TestCase
 from django.urls import reverse
 from sounds.models import Sound
-from utils.search.search_general import search_process_filter
-# Below we import SolrResponseInterpreter and SolrResponseInterpreterPaginator because we're using them in the 
-# tests to process data returned from Solr. Ideally we should make this test agnostic of search backend and 
-# prepare fake search data as it would be afeter being processed by SolrResponseInterpreter and SolrResponseInterpreterPaginator
-from utils.search.backends.solr451custom import SolrResponseInterpreter, SolrResponseInterpreterPaginator
+from utils.search import SearchResults, SearchResultsPaginator
 from utils.test_helpers import create_user_and_sounds
 import mock
-import copy
 
 
-solr_select_returned_data = {
-    'facet_counts': {
-        'facet_dates': {},
-        'facet_fields': {
-            'bitdepth': ['16', 390, '24', 221, '0', 105, '32', 19, '4', 1],
-            'bitrate': ['1411', 153, '1378', 151, '2250', 79, '1500', 32, '1536', 23],
-            'channels': ['2', 630, '1', 91, '6', 12, '4', 4],
-        },
-        'facet_queries': {},
-        'facet_ranges': {}
-    },
-    'grouped': {
-        'grouping_pack': {
-            'groups': [],  # Actual sounds to be returned are filled dynamically in tests
-            'matches': 737,
-            'ngroups': 548
-        }
-    },
-    'responseHeader': {
-        'QTime': 17,
-        'params': {
-            'f.grouping_pack.facet.limit': '10',
-            'f.license.facet.limit': '10',
-            'f.tag.facet.limit': '30',
-            'f.username.facet.limit': '30',
-            'facet': 'true',
-            'facet.field': ['bitrate', 'bitdepth', 'channels'],
-            'facet.limit': '5',
-            'facet.mincount': '1',
-            'facet.missing': 'false',
-            'facet.sort': 'true',
-            'fl': 'id',
-            'group': 'true',
-            'group.cache.percent': '0',
-            'group.field': 'grouping_pack',
-            'group.format': 'grouped',
-            'group.limit': '1',
-            'group.main': 'false',
-            'group.ngroups': 'true',
-            'group.offset': '0',
-            'group.rows': '10',
-            'group.start': '0',
-            'group.truncate': 'false',
-            'q': 'dogs',
-            'qf': 'id^4 tag^4 description^3 username^1 pack_tokenized^2 original_filename^2',
-            'qt': 'dismax',
-            'rows': '15',
-            'sort': 'score desc',
-            'start': '0',
-            'wt': 'json'
-        },
-        'status': 0
-    }
-}
+def create_fake_search_engine_results():
+    return SearchResults(
+        docs=[], # Actual sounds to be returned are filled dynamically in tests
+        num_found=548,
+        start=0,
+        num_rows=0,
+        non_grouped_number_of_results=737,
+        facets={
+            'bitdepth': [('16', 390), ('24', 221), ('0', 105), ('32', 19), ('4', 1)],
+            'bitrate': [('1411', 153), ('1378', 151), ('2250', 79), ('1500', 32), ('1536', 23)],
+            'channels': [('2', 630), ('1', 91), ('6', 12), ('4', 4)]},
+        highlighting={},
+        q_time=17
+    )
 
 
 def return_successful_clustering_results(sound_id_1, sound_id_2, sound_id_3, sound_id_4):
@@ -160,20 +115,16 @@ class SearchPageTests(TestCase):
         self.NUM_RESULTS = 15
         sound_ids = list(Sound.objects.filter(
             moderation_state="OK", processing_state="OK").values_list('id', flat=True)[:self.NUM_RESULTS])
-        solr_select_returned_data['grouped']['grouping_pack']['groups'] = [
-            {'doclist': {'docs': [{'id': sound_id}], 'numFound': 1, 'start': 0}, 'groupValue': str(sound_id)}
-            for sound_id in sound_ids
+        results = create_fake_search_engine_results()
+        results.docs = [
+            {'group_docs': [{'id': sound_id}],
+             'id': sound_id,
+             'n_more_in_group': 0,
+             'group_name': str(sound_id)} for sound_id in sound_ids
         ]
-        
-        results = SolrResponseInterpreter(copy.deepcopy(solr_select_returned_data))
-        # NOTE: in the line above, we need to deepcopy the dictionary of results because SolrResponseInterpreter changes
-        # it and makes it break when run a second time. Ideally SolrResponseInterpreter should be fixed so that it does
-        # not change its input parameter.
-
-        paginator = SolrResponseInterpreterPaginator(results, self.NUM_RESULTS)
+        paginator = SearchResultsPaginator(results, self.NUM_RESULTS)
         page = paginator.page(1)  # Get first page
-        self.perform_search_engine_query_response = \
-            (results.non_grouped_number_of_matches, results.facets, paginator, page, results.docs)
+        self.perform_search_engine_query_response = (results, paginator)
 
     @mock.patch('search.views.perform_search_engine_query')
     def test_search_page_response_ok(self, perform_search_engine_query):
@@ -211,24 +162,6 @@ class SearchPageTests(TestCase):
         # Which means they are going to be displayed
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(len(resp.context['filter_query_split']), 2)
-
-
-class SearchProcessFilter(TestCase):
-
-    def test_search_process_filter(self):
-
-        filter_query = search_process_filter('ac_loudness: ac_dynamic_range: ac_temporal_centroid: ac_log_attack_time: '
-                                             'ac_single_event: ac_tonality: ac_tonality_confidence: ac_loop: ac_tempo: '
-                                             'ac_tempo_confidence: ac_note_midi: ac_note_name: ac_note_frequency: '
-                                             'ac_note_confidence: ac_brightness: ac_depth: ac_hardness: ac_roughness: '
-                                             'ac_boominess: ac_reverb: ac_warmth: ac_sharpness: another_field:')
-        self.assertEqual(filter_query, 'ac_loudness_d: ac_dynamic_range_d: ac_temporal_centroid_d: '
-                                       'ac_log_attack_time_d: ac_single_event_b: ac_tonality_s: '
-                                       'ac_tonality_confidence_d: ac_loop_b: ac_tempo_i: ac_tempo_confidence_d: '
-                                       'ac_note_midi_i: ac_note_name_s: ac_note_frequency_d: ac_note_confidence_d: '
-                                       'ac_brightness_d: ac_depth_d: ac_hardness_d: ac_roughness_d: ac_boominess_d: '
-                                       'ac_reverb_b: ac_warmth_d: ac_sharpness_d: another_field:')
-
 
 
 class SearchResultClustering(TestCase):

f2e405526c1b5af59306d23a2c91fb6b6be83588
ffont <frederic.font@gmail.com>
2022-02-09 12:44:59 +0100

    Fix celery queues info in monitor

--- freesound/celery.py
+++ freesound/celery.py
@@ -23,27 +23,28 @@ app.autodiscover_tasks()
 
 def get_queues_task_counts():
     try:
-        raw_data = requests.get('http://{}:{}/api/queues'.format(settings.RABBITMQ_HOST, settings.RABBITMQ_API_PORT),
+        raw_data = requests.get('http://{}:{}/rabbitmq-admin/api/queues'.format(settings.RABBITMQ_HOST, settings.RABBITMQ_API_PORT),
                                 auth=(settings.RABBITMQ_USER, settings.RABBITMQ_PASS)).json()
     except Exception as e:
         raw_data = []
         print e
 
     data = []
-    for queue_data in raw_data:
-        queue_name = queue_data['name']
-        if 'celery' in queue_name:
-            continue
-        try:
-            message_rate = queue_data['message_stats']['ack_details']['rate']
-        except KeyError:
-            message_rate = -1
-        data.append((queue_name,
-                     queue_data['messages_ready'],
-                     queue_data['messages_unacknowledged'],
-                     queue_data['consumers'],
-                     message_rate
-                     ))
+    if 'error' not in raw_data:
+        for queue_data in raw_data:
+            queue_name = queue_data['name']
+            if 'celery' in queue_name:
+                continue
+            try:
+                message_rate = queue_data['message_stats']['ack_details']['rate']
+            except KeyError:
+                message_rate = -1
+            data.append((queue_name,
+                         queue_data['messages_ready'],
+                         queue_data['messages_unacknowledged'],
+                         queue_data['consumers'],
+                         message_rate
+                         ))
 
     data = sorted(data, key=lambda x: x[0])
     return data

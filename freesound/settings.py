9c8d5a97a7d6d72e0d7765a0943844c21616bf8a
ffont <frederic.font@upf.edu>
2022-02-10 15:47:54 +0100

    Move celery settings before local_settings import

--- freesound/settings.py
+++ freesound/settings.py
@@ -794,6 +794,21 @@ LAST_RESTART_DATE = datetime.datetime.now().strftime("%d%m")
 PLAUSIBLE_AGGREGATE_PAGEVIEWS = True
 PLAUSIBLE_SEPARATE_FRONTENDS = True
 
+# -------------------------------------------------------------------------------
+# Celery
+RABBITMQ_USER = "guest"
+RABBITMQ_PASS = "guest"
+RABBITMQ_HOST = 'rabbit'
+RABBITMQ_PORT = '5672'
+RABBITMQ_API_PORT = '15672'
+
+CELERY_BROKER_URL = 'amqp://{}:{}@{}:{}//'.format(RABBITMQ_USER, RABBITMQ_PASS, RABBITMQ_HOST, RABBITMQ_PORT)
+CELERY_RESULT_BACKEND = 'redis://{}:{}/{}'.format(REDIS_HOST, REDIS_PORT, CELERY_BROKER_REDIS_STORE_ID)
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ASYNC_TASKS_QUEUE_NAME = 'async_tasks_queue'
+
 
 # -------------------------------------------------------------------------------
 # Import local settings
@@ -811,22 +826,6 @@ if SENTRY_DSN:
         integrations=[DjangoIntegration()],
         send_default_pii=True
     )
-
-
-# -------------------------------------------------------------------------------
-# Celery
-RABBITMQ_USER = "guest"
-RABBITMQ_PASS = "guest"
-RABBITMQ_HOST = 'rabbit'
-RABBITMQ_PORT = '5672'
-RABBITMQ_API_PORT = '15672'
-
-CELERY_BROKER_URL = 'amqp://{}:{}@{}:{}//'.format(RABBITMQ_USER, RABBITMQ_PASS, RABBITMQ_HOST, RABBITMQ_PORT)
-CELERY_RESULT_BACKEND = 'redis://{}:{}/{}'.format(REDIS_HOST, REDIS_PORT, CELERY_BROKER_REDIS_STORE_ID)
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_ASYNC_TASKS_QUEUE_NAME = 'async_tasks_queue'
 
 
 # -------------------------------------------------------------------------------

5eeeb2d3ef4e1476d770842e6309a231c9508eff
Bomme <13520622+Bomme@users.noreply.github.com>
2023-01-23 23:07:24 +0100

    mark mock content as bytestring

--- monitor/tests.py
+++ monitor/tests.py
@@ -1,11 +1,7 @@
-import json
-
-from django.contrib.auth.models import User
+import mock
+import requests
 from django.test import TestCase, override_settings
 from django.urls import reverse
-import requests
-
-import mock
 
 
 class QueryStatsAjaxTestCase(TestCase):
@@ -28,7 +24,7 @@ class QueryStatsAjaxTestCase(TestCase):
         """The endpoint returns HTTP500 if the graylog endpoint returns 200, but the data is not JSON"""
         mock_get.return_value = requests.Response()
         mock_get.return_value.status_code = 200
-        mock_get.return_value._content = '<html>this is definitely not json</html>'
+        mock_get.return_value._content = b'<html>this is definitely not json</html>'
         resp = self.client.get(reverse('monitor-queries-stats-ajax'))
 
         self.assertEqual(resp.status_code, 500)
@@ -40,10 +36,10 @@ class QueryStatsAjaxTestCase(TestCase):
         """The endpoint returns valid data if graylog returns valid data"""
         mock_get.return_value = requests.Response()
         mock_get.return_value.status_code = 200
-        mock_get.return_value._content = '{"response": "ok"}'
+        mock_get.return_value._content = b'{"response": "ok"}'
 
         resp = self.client.get(reverse('monitor-queries-stats-ajax'))
 
         self.assertEqual(resp.status_code, 200)
-        self.assertEqual(json.loads(resp.content), {'response': 'ok'})
+        self.assertJSONEqual(resp.content, {'response': 'ok'})
         mock_get.assert_called_with('http://graylog/graylog/api/search/universal/relative/terms', auth=mock.ANY, params=mock.ANY)

ac9a7580eb3051eedb7d59ef8923a72d867beda7
ffont <frederic.font@upf.edu>
2023-02-07 14:10:24 +0100

    Avoid connecting to real solr in unit tests

--- freesound/test_settings.py
+++ freesound/test_settings.py
@@ -35,8 +35,8 @@ CACHES = {
 from .logger import LOGGING
 LOGGING['handlers']['stdout']['class'] = 'logging.NullHandler'
 
-SOLR_URL = "http://fakehost:8080/fs2/"  # Avoid making accidental queries to "real" search server if running
-SOLR_FORUM_URL = "http://fakehost:8080/forum/"  # Avoid making accidental requests to "real" search server if running
+SOLR5_SOUNDS_URL = "http://fakehost:8080/fs2/"  # Avoid making accidental queries to "real" search server if running
+SOLR5_FORUM_URL = "http://fakehost:8080/forum/"  # Avoid making accidental requests to "real" search server if running
 SEARCH_ENGINE_BACKEND_CLASS = 'utils.search.backends.solr555pysolr.Solr555PySolrSearchEngine' # Test with our own custom search engine functions
 SIMILARITY_ADDRESS = 'fakehost' # Avoid making accidental requests to "real" similarity server if running
 TAGRECOMMENDATION_ADDRESS = 'fakehost'  # Avoid making accidental requests to "real" tag rec server if running

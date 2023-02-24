5b9fdf75fb29f91393f1d8e94c4bc84ba6aa0ce7
ffont <frederic.font@upf.edu>
2023-02-24 13:55:10 +0100

    Avoid re-adding event listeners

--- freesound/static/bw-frontend/src/components/geotagFormField.js
+++ freesound/static/bw-frontend/src/components/geotagFormField.js
@@ -1,9 +1,13 @@
 import {makeGeotagEditMap} from '../components/mapsMapbox';
 import debounce from 'lodash.debounce';
 
-const setMapPosition = (map, lat, lon, zoom) => {
-    map.setZoom(parseInt(zoom, 10) - 1, {'triggeredFromForm': true});
-    map.setCenter([parseFloat(lon), parseFloat(lat)], {'triggeredFromForm': true});
+const setMapPosition = (map, lat, lon, zoom, updateFormFields) => {
+    let eventData = {}
+    if (updateFormFields !== undefined && updateFormFields == false){
+        eventData = {'triggeredFromForm': true};
+    }
+    map.setZoom(parseInt(zoom, 10) - 1, eventData);
+    map.setCenter([parseFloat(lon), parseFloat(lat)], eventData);
 };
 
 const showGeotagField = geotagFieldElement => {
@@ -28,33 +32,39 @@ const showGeotagField = geotagFieldElement => {
         zoom = parseInt(zoomInputElement.value);
     }
 
-    // Add map
-    const map = makeGeotagEditMap(mapContainer.id, onBoundsChange, center_lat, center_lon, zoom);
-
-    // Add event listeners to update map with form fields
-    const debouncedSetMapPosition = debounce(setMapPosition);
-    [latInputElement, lonInputElement, zoomInputElement].forEach(element => {
-        element.addEventListener('input', evt => {
-            debouncedSetMapPosition(map, latInputElement.value, lonInputElement.value, zoomInputElement.value);
-        });
-    })
-
-    // Handle button for removing geolocation information
     const removeGeotagCheckbox = document.getElementsByName(`${mapIdx}-remove_geotag`)[0];
     removeGeotagCheckbox.value = false; 
-    const removeGeolocationButton = geotagFieldElement.getElementsByClassName('remove-geolocation-button')[0];
-    removeGeolocationButton.addEventListener('click', evt => {
-        evt.preventDefault(); // Don't submit form
-        const geotagFieldWrapper = geotagFieldElement.getElementsByClassName("geotag-field-wrapper")[0];
-        geotagFieldWrapper.classList.add('display-none');
-        const buttonElement = geotagFieldElement.getElementsByTagName('button')[0];
-        buttonElement.classList.remove('display-none');
-        const removeGeotagCheckbox = document.getElementsByName(`${mapIdx}-remove_geotag`)[0];
-        removeGeotagCheckbox.value = true;
-        latInputElement.value = "";
-        lonInputElement.value = "";
-        zoomInputElement.value = "";
-    });
+
+    if (geotagFieldElement.map == undefined){
+        // Create map    
+        geotagFieldElement.map = makeGeotagEditMap(mapContainer.id, onBoundsChange, center_lat, center_lon, zoom);
+
+        // Add event listeners to update map with form fields
+        const debouncedSetMapPosition = debounce(setMapPosition);
+        [latInputElement, lonInputElement, zoomInputElement].forEach(element => {
+            element.addEventListener('input', evt => {
+                debouncedSetMapPosition(geotagFieldElement.map, latInputElement.value, lonInputElement.value, zoomInputElement.value, false);
+            });
+        })
+
+        // Handle button for removing geolocation information
+        const removeGeolocationButton = geotagFieldElement.getElementsByClassName('remove-geolocation-button')[0];
+        removeGeolocationButton.addEventListener('click', evt => {
+            evt.preventDefault(); // Don't submit form
+            const geotagFieldWrapper = geotagFieldElement.getElementsByClassName("geotag-field-wrapper")[0];
+            geotagFieldWrapper.classList.add('display-none');
+            const buttonElement = geotagFieldElement.getElementsByTagName('button')[0];
+            buttonElement.classList.remove('display-none');
+            const removeGeotagCheckbox = document.getElementsByName(`${mapIdx}-remove_geotag`)[0];
+            removeGeotagCheckbox.value = true;
+            latInputElement.value = "";
+            lonInputElement.value = "";
+            zoomInputElement.value = "";
+        });
+    } else {
+        // If map was already created, only center it to the current values of the form (if any)
+        setMapPosition(geotagFieldElement.map, center_lat, center_lon, zoom, true);
+    }
 }
 
 const onBoundsChange = (map_container_id, lat, lon, zoom, box_bl_lat, box_bl_lon, box_tr_lat, box_tr_lon) => {

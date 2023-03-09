a11f31c40341f4e95c4d58402737e6c92c7323f7
ffont <frederic.font@upf.edu>
2023-03-09 11:02:11 +0100

    Uncomment accidentally commented lines

--- freesound/static/bw-frontend/src/index.js
+++ freesound/static/bw-frontend/src/index.js
@@ -1,11 +1,11 @@
 // polyfills and css resets
-//import 'core-js/stable'
-//import 'regenerator-runtime/runtime'
-//import 'whatwg-fetch'
-//import 'classlist-polyfill'
-//import 'element-closest'
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+import 'whatwg-fetch'
+import 'classlist-polyfill'
+import 'element-closest'
 import 'normalize.css'
 
 import '../styles/index.scss'
 
-//import './components'
+import './components'

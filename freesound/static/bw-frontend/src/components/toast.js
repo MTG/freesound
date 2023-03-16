ce45881c4c207232d0a721639b28abfe681f2188
ffont <frederic.font@upf.edu>
2023-03-16 10:11:14 +0100

    Wrap toast messages in <ul>

--- freesound/static/bw-frontend/src/components/toast.js
+++ freesound/static/bw-frontend/src/components/toast.js
@@ -1,10 +1,18 @@
 let hideToastTimeout;
 
+const wrapTextInUl = (text) => {
+  // We wrap toast messages in ul/li so that they are formatted the same when returned from Django and when directly triggered in javascript
+  if (text.indexOf('<ul>') === -1){
+    return '<ul><li><span class="h-spacing-1">·</span>' + text + '</li></ul>';
+  }
+  return text;
+}
+
 export const showToast = text => {
   clearTimeout(hideToastTimeout);
   const toastElement = document.querySelector('[role="alert"]');
   toastElement.style.display = 'block';
-  toastElement.children[0].innerHTML = text;
+  toastElement.children[0].innerHTML = wrapTextInUl(text);
   hideToastTimeout = setTimeout(() => {
     toastElement.style.display = 'none';
   }, 5000);
@@ -14,7 +22,7 @@ export const showToastNoTimeout = text => {
   clearTimeout(hideToastTimeout);
   const toastElement = document.querySelector('[role="alert"]');
   toastElement.style.display = 'block';
-  toastElement.children[0].innerHTML = text;
+  toastElement.children[0].innerHTML = wrapTextInUl(text);
 };
 
 export const dismissToast = () => {

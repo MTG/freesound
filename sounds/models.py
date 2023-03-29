74952fd805d83bd6c28f1d8e61cf5b8d5283eeb4
ffont <frederic.font@upf.edu>
2023-03-29 12:47:34 +0200

    Fix bug with badly encoded pack names in urls

--- sounds/models.py
+++ sounds/models.py
@@ -44,6 +44,7 @@ from django.dispatch import receiver
 from django.template.loader import render_to_string
 from django.urls import reverse
 from django.utils.encoding import smart_str
+from django.utils.http import urlquote
 from django.utils.functional import cached_property
 from django.utils.text import Truncator, slugify
 
@@ -1620,8 +1621,11 @@ class Pack(SocialModel):
         except Exception as e:
             return False
 
+    def pack_filter_value(self):
+        return f"\"{self.id}_{urlquote(self.name)}\""
+
     def browse_pack_tag_url(self, tag):
-        return reverse('tags', args=[tag]) + f'?pack_flt="{self.id}_{self.name}"'
+        return reverse('tags', args=[tag]) + f'?pack_flt={self.pack_filter_value()}'
 
     def get_pack_tags_bw(self):
         try:

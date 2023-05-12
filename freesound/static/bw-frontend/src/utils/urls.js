92381ca5dca5ba77d979af0a0ea9946eccc676f6
ffont <frederic.font@upf.edu>
2023-05-12 13:50:08 +0200

    Make profile "latest packs" tap linkable with hash

---
+++ freesound/static/bw-frontend/src/utils/urls.js
@@ -0,0 +1,10 @@
+export const setURLHash = (newHash) => {
+    location.hash =  newHash;
+}
+
+export const hashEquals = (hash) => {
+    if (!hash.startsWith("#")) {
+        hash = '#' + hash;
+    }
+    return location.hash == hash;
+}

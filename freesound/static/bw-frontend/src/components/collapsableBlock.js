e4ea743528e4971947fc112964cd0a03e8683d65
ffont <frederic.font@upf.edu>
2023-10-24 16:57:06 +0200

    Add more options to collapsable block

--- freesound/static/bw-frontend/src/components/collapsableBlock.js
+++ freesound/static/bw-frontend/src/components/collapsableBlock.js
@@ -2,10 +2,21 @@ const collapsableToggles = document.getElementsByClassName('collapsable-toggle')
 
 const toggleCollapse = (toggleElement) => {
   const collapsableContainer = document.getElementById(toggleElement.dataset.target);
-  collapsableContainer.classList.toggle('collapsable-block-close');
+  let closeClass =  'collapsable-block-close';
+  if (toggleElement.dataset.maxHeightWhenClosed !== undefined) {
+    closeClass =  'collapsable-block-close-gradient';
+    if (collapsableContainer.classList.contains('collapsable-block-close-gradient')) {
+      // If the block contains the closed gradient class, it means we will expand the collapsable block now so we 
+      // unset the height that we manually set from the dataset property
+      collapsableContainer.style.maxHeight = 'unset';
+    } else {
+      collapsableContainer.style.maxHeight = toggleElement.dataset.maxHeightWhenClosed + 'px';
+    }
+  }
+  collapsableContainer.classList.toggle(closeClass);
   const showText = toggleElement.dataset.showText;
   const hideText = toggleElement.dataset.hideText;
-  toggleElement.textContent = collapsableContainer.classList.contains('collapsable-block-close')
+  toggleElement.textContent = collapsableContainer.classList.contains(closeClass)
     ? showText
     : hideText;
 }
@@ -15,6 +26,17 @@ const handleCollapsable = (e) => {
 };
 
 collapsableToggles.forEach(element => {
+  if (element.dataset.maxHeightWhenClosed !== undefined) {
+    // If a max height is set, then the element will be partially visible when closed,
+    // but if the element's height is actually less than the max height, then no "colappsable" 
+    // actions will take place and we hide the toggle element
+    const collapsableContainer = document.getElementById(element.dataset.target);
+    if (element.dataset.maxHeightWhenClosed >= collapsableContainer.clientHeight) {
+      element.classList.add('display-none');  // Hide controls
+      return; // continue to next toggle element as this will not implement collapsable behaviour
+    }
+  }
+  
   if (element.dataset.hideOnLoad !== undefined){
     toggleCollapse(element);
   } else {

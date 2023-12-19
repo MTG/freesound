6e762264aa9b8ead8ca2075ccacecffd4d1a49e5
Bomme <13520622+Bomme@users.noreply.github.com>
2023-12-19 11:22:36 +0100

    enable password validation
    the settings are taken from the default Django settings.py

--- freesound/settings.py
+++ freesound/settings.py
@@ -242,6 +242,23 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.SHA1PasswordHasher',
 ]
 
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 # -------------------------------------------------------------------------------
 # Email settings

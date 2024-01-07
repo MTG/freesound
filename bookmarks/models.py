c3d1cfc3dea799f24dccb280a47320107d4dd5c4
Bomme <13520622+Bomme@users.noreply.github.com>
2024-01-07 21:19:00 +0100

    fix AttributeError on Bookmark.__str__
    fixes FREESOUND-WEB-1V5 and FREESOUND-WEB-1V4

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -40,7 +40,7 @@ class Bookmark(models.Model):
     created = models.DateTimeField(db_index=True, auto_now_add=True)
     
     def __str__(self):
-        return f"Bookmark: {self.name}"
+        return f"Bookmark: {self.sound} by {self.user}"
 
     @property
     def category_name_or_uncategorized(self):

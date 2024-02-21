3719876a2253d6f9d8de400869c17b6125be80b5
ffont <frederic.font@upf.edu>
2024-02-21 16:05:24 +0100

    Working facets with SearchQueryProcessor

--- search/templatetags/search.py
+++ search/templatetags/search.py
@@ -29,6 +29,98 @@ from utils.tags import annotate_tags
 register = template.Library()
 
 
+@register.inclusion_tag('search/facet2.html', takes_context=True)
+def display_facet2(context, facet_name):
+    sqp = context['sqp']
+    facets = context['facets']
+    facet_type = {'tag': 'cloud', 'username': 'cloud'}.get(facet_name, 'list')
+    facet_title = {
+        'tag': 'Related tags',
+        'username': 'Related users',
+        'grouping_pack': 'Packs',
+        'license': 'Licenses'
+    }.get(facet_name, facet_name.capitalize())
+    facet = annotate_tags([dict(value=f[0], count=f[1]) for f in facets[facet_name] if f[0] != "0"],
+                          sort="value", small_size=0.7, large_size=2.0)
+    
+    # If the filter is grouping_pack and there are elements which do not contain the character "_" means that
+    # these sounds do not belong to any pack (as grouping pack values should by "packId_packName" if there is a pack
+    # or "soundId" if there is no pack assigned. We did this to be able to filter properly in the facets, as pack names
+    # are not unique!. What we do then is filter out the facet elements where, only for the case of grouping_pack,
+    # the element name is a single number that does not contain the character "_"
+
+    # We add the extra Free Cultural Works license facet
+    if facet_name == 'license':
+        fcw_count = 0
+        only_fcw_in_facet = True
+        for element in facet:
+            if element['value'].lower() == 'attribution' or element['value'].lower() == 'creative commons 0':
+                fcw_count += element['count']
+            else:
+                only_fcw_in_facet = False
+        if fcw_count and not only_fcw_in_facet:
+            facet.append({
+                    'value': settings.FCW_FILTER_VALUE,
+                    'count': fcw_count,
+                    'size': 1.0,
+                })
+
+    filtered_facet = []
+    for element in facet:
+        if facet_name == "grouping_pack":
+            if element['value'].count("_") > 0:
+                # We also modify the display name to remove the id
+                element['display_value'] = element['value'][element['value'].find("_")+1:]
+            else:
+                # If facet element belongs to "grouping pack" filter but does not have the "_" character in it, it
+                # means this corresponds to the "no pack" grouping which we don't want to show as a facet element.
+                continue
+        elif element['value'] == settings.FCW_FILTER_VALUE:
+            element['display_value'] = "Approved for Free Cultural Works"
+        elif facet_name == 'license':
+            # License field in solr is case insensitive and will return facet names in lowercase. 
+            # We need to properly capitalize them to use official CC license names.
+            element['display_value'] = element['value'].title().replace('Noncommercial', 'NonCommercial')
+        else:
+            # In all other cases, use the value as is for display purposes
+            element['display_value'] = element['value']
+        
+        #if element['name'] == settings.FCW_FILTER_VALUE:
+        #    # If adding the FCW filter (which has more complex logic) don't wrap the filter in " as it breaks the syntax parsing    
+        #    element['params'] = f"{filter_query} {facet_name}:{quote_plus(element['name'])}"
+        #else:
+        #    element['params'] = f"{filter_query} {facet_name}:\"{quote_plus(element['name'])}\""
+
+        
+        
+        if element["value"] == settings.FCW_FILTER_VALUE:
+            filter_str = f'{facet_name}:{element["value"]}'
+        elif ' ' in element["value"]:
+            filter_str = f'{facet_name}:"{element["value"]}"'
+        else:
+            filter_str = f'{facet_name}:{element["value"]}'
+        element['add_filter_url'] = sqp.get_url(add_filters=[filter_str])
+        
+        filtered_facet.append(element)
+
+    # We sort the facets by count. Also, we apply an opacity filter on "could" type facets
+    if filtered_facet:
+        filtered_facet = sorted(filtered_facet, key=lambda x: x['count'], reverse=True)
+        max_count = max([element['count'] for element in filtered_facet])
+        for element in filtered_facet:
+            element['weight'] = element['count'] / max_count
+
+    # We also add icons to license facets
+    if facet_name == 'license':
+        for element in filtered_facet:
+            if element['value'] != settings.FCW_FILTER_VALUE:
+                element['icon'] = License.bw_cc_icon_name_from_license_name(element['display_value'])
+            else:
+                element['icon'] = 'fcw'
+
+    return {'type': facet_type, 'title': facet_title, 'facet': filtered_facet}
+
+
 @register.inclusion_tag('search/facet.html', takes_context=True)
 def display_facet(context, flt, facet, facet_type, title=""):
     facet = annotate_tags([dict(name=f[0], count=f[1]) for f in facet if f[0] != "0"],

6ef3a884b5c4b96cedb359a3716455489977152f
ffont <frederic.font@upf.edu>
2024-04-15 12:27:34 +0200

    Add further ac facets

--- freesound/settings.py
+++ freesound/settings.py
@@ -629,6 +629,10 @@ SEARCH_SOUNDS_DEFAULT_FACETS = {
 SEARCH_SOUNDS_BETA_FACETS = {
     'fsdsinet_detected_class': {'limit': 30},
     'ac_brightness': {'type': 'range', 'start': 0, 'end': 100, 'gap': 20},
+    'ac_depth': {'type': 'range', 'start': 0, 'end': 100, 'gap': 20},
+    'ac_warmth': {'type': 'range', 'start': 0, 'end': 100, 'gap': 20},
+    'ac_hardness': {'type': 'range', 'start': 0, 'end': 100, 'gap': 20},
+    'ac_boominess': {'type': 'range', 'start': 0, 'end': 100, 'gap': 20},
 }
 
 SEARCH_FORUM_SORT_OPTION_THREAD_DATE_FIRST = "Thread creation (newest first)"

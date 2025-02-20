fecac3f719a435bede1fe194649693a16d1b7ad6
ffont <frederic.font@upf.edu>
2025-02-20 12:26:26 +0100

    Disable sitemaps app

--- freesound/settings.py
+++ freesound/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.sitemaps',
+    #'django.contrib.sitemaps',
     'django.contrib.admin',
     'django.contrib.messages',
     'django.contrib.staticfiles',

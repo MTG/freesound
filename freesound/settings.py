6a01919c9023efc44e6f7285e063456f698a739b
Alastair Porter <alastair.porter@upf.edu>
2025-03-10 12:21:17 +0100

    Remove deprecated toolbar panel

--- freesound/settings.py
+++ freesound/settings.py
@@ -917,7 +917,6 @@ if DEBUG and DISPLAY_DEBUG_TOOLBAR:
         'debug_toolbar.panels.templates.TemplatesPanel',
         'debug_toolbar.panels.cache.CachePanel',
         'debug_toolbar.panels.signals.SignalsPanel',
-        'debug_toolbar.panels.logging.LoggingPanel',
         'debug_toolbar.panels.redirects.RedirectsPanel',
     ]
 

2a66256d158a5e47b08d48f96f55ab8d6d159d3b
Alastair Porter <alastair.porter@upf.edu>
2025-05-21 14:12:57 +0200

    Use pytest to run search engine backend tests

--- conftest.py
+++ conftest.py
@@ -1,5 +1,8 @@
 import pytest
 
+def pytest_addoption(parser):
+    """Used in the search enging backend tests to select a backend engine"""
+    parser.addoption("--search-engine-backend", action="store", default="utils.search.backends.solr9pysolr.Solr9PySolrSearchEngine")
 
 @pytest.fixture
 def use_dummy_cache_backend(settings):

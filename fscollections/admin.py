e57338ac70365dc4ab10c04a65cad1b19d673359
quimmrc <quim.mrc@gmail.com>
2025-06-05 10:32:58 +0200

    basic admin page for collections

--- fscollections/admin.py
+++ fscollections/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Collection, CollectionSound
 
 # Register your models here.
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+
+    fields = ["user","name", "num_sounds", "public","get_sounds"]
+    filter_horizontal =  ["sounds"]
+    list_display = ("name", "user", "num_sounds", "public","get_sounds")
+    readonly_fields = ["created"]
+    actions = ["make_public", "make_private"]
+
+    def has_delete_permission(self, request, obj=None):
+        if obj and obj.sounds.count() > 0:
+            return False  
+        return True
+
+    def get_sounds(self, obj):
+        return ", ".join(str(sound.id) for sound in obj.sounds.all())
+    get_sounds.short_description = "Sounds"
+
+    @admin.action(description="Make selected collections public")
+    def make_public(self, request, queryset):
+        queryset.update(public=True)
+    
+    @admin.action(description="Make selected collection private")
+    def make_private(self, request, queryset):
+        queryset.update(public=False)

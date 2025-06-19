7a8412f039005723e9c5f07713371bc282c8ef2c
Alastair Porter <alastair.porter@upf.edu>
2025-06-19 16:06:48 +0200

    Send user id to celery task instead of object
    Rename parameter to make its role more clear (tickets also have a
    sender, which is distinct)

--- tickets/views.py
+++ tickets/views.py
@@ -161,7 +161,7 @@ def ticket(request, ticket_key):
                     notification = ticket.NOTIFICATION_APPROVED
 
                 elif sound_action == 'Whitelist':
-                    whitelist_user_task.delay(sender=request.user, ticket_ids=[ticket.id])  # async job should take care of whitelisting
+                    whitelist_user_task.delay(annotation_sender_id=request.user.id, ticket_ids=[ticket.id])
                     comment += f'whitelisted all sounds from user {ticket.sender}'
                     notification = ticket.NOTIFICATION_WHITELISTED
 
@@ -559,7 +559,7 @@ def moderation_assigned(request, user_id):
 
             elif action == "Whitelist":
                 ticket_ids = list(tickets.values_list('id',flat=True))
-                whitelist_user_task.delay(sender=request.user, ticket_ids=ticket_ids)  # async job should take care of whitelisting
+                whitelist_user_task.delay(annotation_sender_id=request.user.id, ticket_ids=ticket_ids)  # async job should take care of whitelisting
                 notification = Ticket.NOTIFICATION_WHITELISTED
 
                 users = set(tickets.values_list('sender__username', flat=True))
@@ -729,7 +729,7 @@ def whitelist_user(request, user_id):
                              """The user you are trying to whitelist does not exist""")
         return HttpResponseRedirect(reverse('tickets-moderation-home'))
 
-    whitelist_user_task.delay(sender=request.user, user_id=user_id)
+    whitelist_user_task.delay(annotation_sender_id=request.user.id, user_id=user_id)
 
     messages.add_message(request, messages.INFO,
         f"""User {user.username} has been whitelisted. Note that some of tickets might

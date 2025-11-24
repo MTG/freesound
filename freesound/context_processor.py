bad486901d75caf9ffd74fa562620521635b31ff
ffont <frederic.font@upf.edu>
2025-11-24 10:00:34 +0100

    Remove max_sounds_per_collection from context as it is passed in tvars

--- freesound/context_processor.py
+++ freesound/context_processor.py
@@ -77,8 +77,7 @@ def context_extra(request):
             'login_form': FsAuthenticationForm(),
             'problems_logging_in_form': ProblemsLoggingInForm(),
             'system_prefers_dark_theme': request.COOKIES.get('systemPrefersDarkTheme', 'no') == 'yes',  # Determine the user's system preference for dark/light theme (for non authenticated users, always use light theme)
-            'enable_collections': settings.ENABLE_COLLECTIONS,
-            'max_sounds_per_collection': settings.MAX_SOUNDS_PER_COLLECTION,
+            'enable_collections': settings.ENABLE_COLLECTIONS
         })
     
     return tvars

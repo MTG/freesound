050af6a49cee883fe87440f6092361692e33ea3e
ffont <frederic.font@upf.edu>
2025-11-26 14:54:12 +0100

    Check serializer fields

--- apiv2/tests.py
+++ apiv2/tests.py
@@ -389,7 +389,12 @@ class TestSoundSerializer(TestCase):
                 include_remix_subqueries=True,
             )[0]
             dummy_request = self.factory.get(reverse("apiv2-sound-instance", args=[self.sound_id]) + "?fields=*")
-            _ = SoundSerializer(sound, context={"request": dummy_request}).data
+            serialized_sound = SoundSerializer(sound, context={"request": dummy_request}).data
+            # Check that at least all fields in the serializer Meta are present in the serialized data
+            self.assertTrue(
+                len(set(serialized_sound.keys()).intersection(set(SoundSerializer.Meta.fields)))
+                == len(SoundSerializer.Meta.fields)
+            )
 
 
 class TestApiV2Client(TestCase):

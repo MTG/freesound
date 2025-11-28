95e9c6c65b5de521b2ae53174298b1c07f3e713b
Domonkos Kostyal <kostyal.domonkos@gmail.com>
2025-11-28 17:04:10 +0100

    Buttons functionality changed to raw forms

--- messages/views.py
+++ messages/views.py
@@ -44,7 +44,6 @@ def messages_change_state(request):
     if request.method == "POST":
         choice = request.POST.get("choice", False)
         message_ids = [int(mid) for mid in request.POST.get("ids", "").split(',')]
-        next_url = request.POST.get("next", reverse("messages"))
         if choice and message_ids:
             fs_messages = Message.objects.filter(Q(user_to=request.user, is_sent=False) |
                                               Q(user_from=request.user, is_sent=True)).filter(id__in=message_ids)
@@ -54,15 +53,12 @@ def messages_change_state(request):
                     message.save()
             elif choice == "d":
                 fs_messages.delete()
-            elif choice == "dm":
-                fs_messages.delete()
-                next_url = reverse("messages")
             elif choice == "r":
                 for message in fs_messages:
                     message.is_read = not message.is_read
                     message.save()
             invalidate_user_template_caches(request.user.id)
-    return HttpResponseRedirect(next_url)
+    return HttpResponseRedirect(request.POST.get("next", reverse("messages")))
 
 
 # base query object

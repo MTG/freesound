9fec01a22be2e82c2262aee6d4d395d36a2c785b
Domonkos Kostyal <kostyal.domonkos@gmail.com>
2026-03-23 09:31:23 +0100

    removed redundant code

--- freesound/static/bw-frontend/src/components/addSoundsModal.js
+++ freesound/static/bw-frontend/src/components/addSoundsModal.js
@@ -5,72 +5,6 @@ import {
 } from '../components/objectSelector';
 import { serializedIdListToIntList, combineIdsLists } from '../utils/data';
 
-const handleAddSoundsModal = (
-  modalId,
-  modalUrl,
-  selectedSoundsDestinationElement,
-  onSoundsSelectedCallback
-) => {
-  handleGenericModal(
-    modalUrl,
-    modalContainer => {
-      const inputElement = modalContainer.getElementsByTagName('input')[0];
-      inputElement.addEventListener('keypress', function (event) {
-        if (event.key === 'Enter') {
-          event.preventDefault();
-          const baseUrl = modalUrl.split('?')[0];
-          const soundIdsToExclude = combineIdsLists(
-            serializedIdListToIntList(
-              selectedSoundsDestinationElement.dataset.selectedIds
-            ),
-            serializedIdListToIntList(
-              selectedSoundsDestinationElement.dataset.unselectedIds
-            )
-          ).join(',');
-          handleAddSoundsModal(
-            modalId,
-            `${baseUrl}?q=${inputElement.value}&exclude=${soundIdsToExclude}`,
-            selectedSoundsDestinationElement,
-            onSoundsSelectedCallback
-          );
-        }
-      });
-
-      const objectSelectorElement = modalContainer.getElementsByClassName(
-        'bw-object-selector-container'
-      )[0];
-      initializeObjectSelector(objectSelectorElement, element => {
-        addSelectedSoundsButton.disabled = element.dataset.selectedIds == '';
-      });
-
-      const addSelectedSoundsButton =
-        modalContainer.getElementsByTagName('button')[0];
-      addSelectedSoundsButton.disabled = true;
-      addSelectedSoundsButton.addEventListener('click', evt => {
-        const selectableSoundElements = [
-          ...modalContainer.getElementsByClassName('bw-selectable-object'),
-        ];
-        selectableSoundElements.forEach(element => {
-          const checkbox = element.querySelectorAll('input.bw-checkbox')[0];
-          if (checkbox.checked) {
-            const clonedCheckbox = checkbox.cloneNode(); // Cloning the node will remove the event listeners which refer to the "old" sound selector
-            delete clonedCheckbox.dataset.initialized; // This will force re-initialize the element when added to the new sounds selector
-            clonedCheckbox.checked = false;
-            checkbox.parentNode.replaceChild(clonedCheckbox, checkbox);
-            element.classList.remove('selected');
-            selectedSoundsDestinationElement.appendChild(element.parentNode);
-          }
-        });
-        onSoundsSelectedCallback(objectSelectorElement.dataset.selectedIds);
-        dismissModal(modalId);
-      });
-    },
-    undefined,
-    true,
-    true
-  );
-};
-
 const prepareAddSoundsModalAndFields = container => {
   const addSoundsButtons = [
     ...container.querySelectorAll(`[data-toggle^="add-sounds-modal"]`),
@@ -153,18 +87,42 @@ const prepareAddSoundsModalAndFields = container => {
 
     addSoundsButton.addEventListener('click', evt => {
       evt.preventDefault();
-      handleAddSoundsModal(
+      const getExcludeIds = () => combineIdsLists(
+        serializedIdListToIntList(selectedSoundsDestinationElement.dataset.selectedIds),
+        serializedIdListToIntList(selectedSoundsDestinationElement.dataset.unselectedIds)
+      ).join(',');
+      openAddSoundsModal(
         'addSoundsModal',
         addSoundsButton.dataset.modalUrl,
-        selectedSoundsDestinationElement,
-        selectedSoundIds => {
+        addSoundsButton.dataset.modalUrl,
+        getExcludeIds,
+        modalContainer => {
+          const objectSelectorElement = modalContainer.getElementsByClassName(
+            'bw-object-selector-container'
+          )[0];
+          const selectableSoundElements = [
+            ...modalContainer.getElementsByClassName('bw-selectable-object'),
+          ];
+          selectableSoundElements.forEach(element => {
+            const checkbox = element.querySelectorAll('input.bw-checkbox')[0];
+            if (checkbox.checked) {
+              const clonedCheckbox = checkbox.cloneNode();
+              delete clonedCheckbox.dataset.initialized;
+              clonedCheckbox.checked = false;
+              checkbox.parentNode.replaceChild(clonedCheckbox, checkbox);
+              element.classList.remove('selected');
+              selectedSoundsDestinationElement.appendChild(element.parentNode);
+            }
+          });
           const selectedSoundsHiddenInput = document.getElementById(
             addSoundsButton.dataset.selectedSoundsHiddenInputId
           );
           const currentSoundIds = serializedIdListToIntList(
             selectedSoundsHiddenInput.value
           );
-          const newSoundIds = serializedIdListToIntList(selectedSoundIds);
+          const newSoundIds = serializedIdListToIntList(
+            objectSelectorElement.dataset.selectedIds
+          );
           const combinedIds = combineIdsLists(currentSoundIds, newSoundIds);
           selectedSoundsHiddenInput.value = combinedIds.join(',');
           if (

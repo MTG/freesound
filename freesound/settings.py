bc5bdabc5bfc478b3d2daf71940c66a450298216
Domonkos Kostyal <domonkos.kostyal01@estudiant.upf.edu>
2026-06-27 10:14:47 +0200

    ENABLE_COLLECTIONS state flip

--- freesound/settings.py
+++ freesound/settings.py
@@ -1286,7 +1286,7 @@ RABBITMQ_API_PORT = "5673"
 
 # -------------------------------------------------------------------------------
 # Collections
-ENABLE_COLLECTIONS = True
+ENABLE_COLLECTIONS = False
 MAX_SOUNDS_PER_COLLECTION = 250
 MAX_FEATURED_SOUNDS_PER_COLLECTION = 6
 COLLECTION_SORT_OPTIONS = {

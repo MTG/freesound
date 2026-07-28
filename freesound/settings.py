64b062605b75be1ac8ba98286bf67ace7d54d3c9
ffont <frederic.font@upf.edu>
2026-07-28 16:46:35 +0200

    Add command to generate full datapack files

--- freesound/settings.py
+++ freesound/settings.py
@@ -995,7 +995,7 @@ ANALYSIS_PATH = os.path.join(DATA_PATH, "analysis/")
 FILE_UPLOAD_TEMP_DIR = os.path.join(DATA_PATH, "tmp_uploads/")
 PROCESSING_TEMP_DIR: str = os.path.join(DATA_PATH, "tmp_processing/")
 PROCESSING_BEFORE_DESCRIPTION_DIR = os.path.join(DATA_PATH, "processing_before_description/")
-DATA_PACKS_EXPORT_PATH = os.path.join(DATA_PATH, "data_packs_export/")
+DATA_PACKS_PATH = os.path.join(DATA_PATH, "data_packs/")
 
 # URLs (depend on DATA_URL potentially re-defined in local_settings.py)
 AVATARS_URL = DATA_URL + "avatars/"
--- sounds/tests/test_export_metadata_for_data_packs.py
+++ sounds/tests/test_export_metadata_for_data_packs.py
@@ -4,16 +4,15 @@ from django.core.management import call_command
 from django.test import TestCase, override_settings
 
 
-class ExportMetadataForDataPacksCommandTests(TestCase):
+class CreateDataPackCommandTests(TestCase):
     fixtures = ["licenses", "sounds"]
 
     def test_limit_zero_runs_with_single_query(self):
         with tempfile.TemporaryDirectory() as temp_dir:
-            with override_settings(DATA_PACKS_EXPORT_PATH=temp_dir):
-                # In sounds fixture, max_id is less than 1000, therefore the command will only create one CSV file
-                # The command should run with 3 queries:
-                # 1. Get the max sound ID
-                # 2. Get the sound IDs in the range 0-1000
-                # 3. Get the sound objects for those IDs
-                with self.assertNumQueries(3):
-                    call_command("export_metadata_for_data_packs")
+            with override_settings(DATA_PACKS_PATH=temp_dir):
+                # In sounds fixture, max sound ID is less than 1000, therefore the command will only need one chunk
+                # The command should run with 2 queries:
+                # 1. Get all sound IDs
+                # 2. Get the sound objects for those IDs
+                with self.assertNumQueries(2):
+                    call_command("create_data_pack")

96bbee9ae255819f441297737323a90af96f0df5
ffont <frederic.font@upf.edu>
2026-07-29 15:37:49 +0200

    Update experiment admin objects permissions

--- user_feedback/admin.py
+++ user_feedback/admin.py
@@ -11,19 +11,20 @@ class UserFeedbackAdmin(admin.ModelAdmin):
     search_fields = ("=user__username", "experiment_id")
     readonly_fields = ("experiment_id", "user", "ip", "data", "created")
 
-    # Feedback is collected from users -- treat the admin as a read-only log.
     def has_add_permission(self, request):
         return False
 
     def has_change_permission(self, request, obj=None):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
 
-
-# Add FeedbackOptOut model to admin
 @admin.register(FeedbackOptOut)
 class FeedbackOptOutAdmin(admin.ModelAdmin):
     raw_id_fields = ("user",)
     list_display = ("user", "experiment_id")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
